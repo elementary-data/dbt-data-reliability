654545048ca7a22a70f8b4e3c48007b0f9c52f94
Maayan-s <maayansalom@gmail.com>
2022-01-13 16:30:16 +0200

    data for tests runs on its own schema

--- integration_tests/macros/do_schema_changes.sql
+++ integration_tests/macros/do_schema_changes.sql
@@ -1,27 +1,27 @@
 {% macro do_schema_changes() %}
 
     {% set schema_changes_query -%}
-    alter table {{ target.database ~"."~ target.schema }}.GROUPS
+    alter table {{ target.database ~"."~ target.schema }}_data.GROUPS
     drop column GROUP_A;
 
-    alter table {{ target.database ~"."~ target.schema }}.STATS_PLAYERS
+    alter table {{ target.database ~"."~ target.schema }}_data.STATS_PLAYERS
     add RED_CARDS varchar(100);
 
-    alter table {{ target.database ~"."~ target.schema }}.STATS_PLAYERS
+    alter table {{ target.database ~"."~ target.schema }}_data.STATS_PLAYERS
     add KEY_CROSSES varchar(100);
 
-    alter table {{ target.database ~"."~ target.schema }}.STATS_PLAYERS
+    alter table {{ target.database ~"."~ target.schema }}_data.STATS_PLAYERS
     drop column OFFSIDES;
 
-    alter table {{ target.database ~"."~ target.schema }}.GROUPS
+    alter table {{ target.database ~"."~ target.schema }}_data.GROUPS
     drop column GROUP_B;
 
-    alter table {{ target.database ~"."~ target.schema }}.GROUPS
+    alter table {{ target.database ~"."~ target.schema }}_data.GROUPS
     add GROUP_B integer;
 
-    drop table {{ target.database ~"."~ target.schema }}.STATS_TEAM;
+    drop table {{ target.database ~"."~ target.schema }}_data.STATS_TEAM;
 
-    create table {{ target.database ~"."~ target.schema }}.stadiums (
+    create table {{ target.database ~"."~ target.schema }}_data.stadiums (
     stadium_name varchar,
     location varchar,
     capacity integer
