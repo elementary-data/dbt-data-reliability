289e11eecf9b8c8bcc7804edfa664222e1ca8463
Maayan-s <maayansalom@gmail.com>
2022-02-05 19:36:37 +0200

    cosmetics

--- models/account_usage/stg_access_history.sql
+++ models/account_usage/stg_access_history.sql
@@ -1,6 +1,3 @@
--- TODO: add dbs filter
--- TODO: change  global time limit to var
-
 {{
   config(
     materialized='incremental',
@@ -21,30 +18,30 @@ with source as (
 access_history as (
 
      select
-             src.query_id,
-             src.query_start_time,
-             src.user_name,
-             src.direct_objects_accessed,
-             src.base_objects_accessed,
-             src.objects_modified,
-             {{ low_no_quotes('direct.value:"objectName"::varchar') }} as direct_access_table_name,
-             direct.value:"objectDomain"::varchar as direct_access_table_type,
-             direct.value:"columns"::varchar as direct_access_columns,
-             {{ low_no_quotes('base.value:"objectName"::varchar') }} as base_access_table_name,
-             base.value:"objectDomain"::varchar as base_access_table_type,
-             base.value:"columns"::varchar as base_access_columns,
-             {{ low_no_quotes('modified.value:"objectName"::varchar') }} as modified_table_name,
-             modified.value:"objectDomain"::varchar as modified_table_type,
-             modified.value:"columns"::varchar as modified_columns
+         src.query_id,
+         src.query_start_time,
+         src.user_name,
+         src.direct_objects_accessed,
+         src.base_objects_accessed,
+         src.objects_modified,
+         {{ low_no_quotes('direct.value:"objectName"::varchar') }} as direct_access_table_name,
+         direct.value:"objectDomain"::varchar as direct_access_table_type,
+         direct.value:"columns"::varchar as direct_access_columns,
+         {{ low_no_quotes('base.value:"objectName"::varchar') }} as base_access_table_name,
+         base.value:"objectDomain"::varchar as base_access_table_type,
+         base.value:"columns"::varchar as base_access_columns,
+         {{ low_no_quotes('modified.value:"objectName"::varchar') }} as modified_table_name,
+         modified.value:"objectDomain"::varchar as modified_table_type,
+         modified.value:"columns"::varchar as modified_columns
 
-         from source as src,
-            lateral flatten(input => src.direct_objects_accessed) as direct,
-            lateral flatten(input => src.base_objects_accessed) as base,
-            lateral flatten(input => src.objects_modified) as modified
-         where direct.value:"objectId" is not null
-             {% if is_incremental() %}
-                 and query_start_time > (select max(query_start_time)  from {{ this }})
-             {% endif %}
+     from source as src,
+         lateral flatten(input => src.direct_objects_accessed) as direct,
+         lateral flatten(input => src.base_objects_accessed) as base,
+         lateral flatten(input => src.objects_modified) as modified
+     where direct.value:"objectId" is not null
+         {% if is_incremental() %}
+             and query_start_time > (select max(query_start_time)  from {{ this }})
+         {% endif %}
 )
 
 select * from access_history
