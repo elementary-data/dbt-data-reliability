8577f1326b443840966fd9dfd5eb792de357b6ee
Maayan-s <maayansalom@gmail.com>
2022-03-02 13:55:37 +0200

    fixed bug

--- models/elementary/metadata_store/filtered_information_schema_columns.sql
+++ models/elementary/metadata_store/filtered_information_schema_columns.sql
@@ -2,12 +2,12 @@
 
 with filtered_information_schema_columns as (
 
-    {% if configured_schemas | length > 0 %}
-        {% set columns_from_info_schema_macro = context['elementary']['get_columns_from_information_schema'] %}
-        {{ elementary.run_query_macro_on_list(columns_from_info_schema_macro, configured_schemas) }}
-    {% else %}
+    {%- if configured_schemas | length > 0 -%}
+        {%- set columns_from_info_schema_macro = context['elementary']['get_columns_from_information_schema'] %}
+        {{ elementary.run_query_macro_on_list(configured_schemas, columns_from_info_schema_macro) }}
+    {%- else %}
         {{ elementary.empty_table([('full_table_name', 'string'), ('database_name', 'string'), ('schema_name', 'string'), ('table_name', 'string'), ('column_name', 'string'), ('data_type', 'string')]) }}
-    {% endif %}
+    {%- endif %}
 
 )
 
--- models/elementary/metadata_store/filtered_information_schema_tables.sql
+++ models/elementary/metadata_store/filtered_information_schema_tables.sql
@@ -2,12 +2,12 @@
 
 with filtered_information_schema_tables as (
 
-    {% if configured_schemas | length > 0 %}
-        {% set tables_from_info_schema_macro = context['elementary']['get_tables_from_information_schema'] %}
-        {{ elementary.run_query_macro_on_list(tables_from_info_schema_macro, configured_schemas) }}
-    {% else %}
+    {%- if configured_schemas | length > 0 -%}
+        {%- set tables_from_info_schema_macro = context['elementary']['get_tables_from_information_schema'] -%}
+        {{ elementary.run_query_macro_on_list(configured_schemas, tables_from_info_schema_macro) }}
+    {%- else %}
         {{ elementary.empty_table([('full_table_name', 'string'), ('database_name', 'string'), ('schema_name', 'string'), ('table_name', 'string')]) }}
-    {% endif %}
+    {%- endif %}
 
 )
 
