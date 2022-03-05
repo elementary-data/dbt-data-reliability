de1b609621c50a126f54fcad9186e77937f1e04c
Maayan-s <maayansalom@gmail.com>
2022-03-05 11:51:06 +0200

    joined models for faster run

--- models/edr/data_monitoring/anomaly_detection/metrics_for_anomalies.sql
+++ models/edr/data_monitoring/anomaly_detection/metrics_for_anomalies.sql
@@ -4,7 +4,7 @@ with metrics as (
 
 ),
 
-metrics_in_timeframe_for_aggregation as (
+metrics_in_timeframe_for_training as (
 
      select *
      from metrics
@@ -15,7 +15,7 @@ metrics_in_timeframe_for_aggregation as (
 
 ),
 
-metrics_aggregated_stats as (
+metrics_training_set as (
 
     select
         full_table_name,
@@ -32,12 +32,12 @@ metrics_aggregated_stats as (
             when max(timeframe_start) is not null then max(timeframe_start)
             else max(updated_at)
         end as timeframe_end
-    from metrics_in_timeframe_for_aggregation
+    from metrics_in_timeframe_for_training
     group by full_table_name, column_name, metric_name
 
 ),
 
-latest_metrics_times as (
+metrics_times_for_validation as (
 
      select
          full_table_name,
@@ -50,7 +50,7 @@ latest_metrics_times as (
 
 ),
 
-latest_metrics as (
+metrics_validation_set as (
 
      select
          m.id,
@@ -62,11 +62,11 @@ latest_metrics as (
          m.timeframe_end,
          m.timeframe_duration_hours,
          m.updated_at
-     from metrics as m join latest_metrics_times as lm
-        on (m.full_table_name = lm.full_table_name
-            and m.column_name = lm.column_name
-            and m.metric_name = lm.metric_name
-            and (m.timeframe_end = lm.latest_timeframe_end or m.timeframe_end is null and m.updated_at = lm.latest_updated_at))
+     from metrics as m join metrics_times_for_validation as tv
+        on (m.full_table_name = tv.full_table_name
+            and m.column_name = tv.column_name
+            and m.metric_name = tv.metric_name
+            and (m.timeframe_end = tv.latest_timeframe_end or m.timeframe_end is null and m.updated_at = tv.latest_updated_at))
      where metric_value is not null
 
 ),
@@ -74,32 +74,32 @@ latest_metrics as (
 final as (
 
     select
-        lm.id,
-        lm.full_table_name,
-        lm.column_name,
-        lm.metric_name,
+        vl.id,
+        vl.full_table_name,
+        vl.column_name,
+        vl.metric_name,
         case
-           when agg.metric_stddev = 0 then 0
-           else (lm.metric_value - agg.metric_avg) / (agg.metric_stddev)
+           when tr.metric_stddev = 0 then 0
+           else (vl.metric_value - tr.metric_avg) / (tr.metric_stddev)
         end as z_score,
-        lm.metric_value as latest_value,
-        lm.updated_at as value_updated_at,
-        agg.metric_avg,
-        agg.metric_stddev,
-        agg.timeframe_start as stats_timeframe_start,
-        agg.timeframe_end as stats_timeframe_end,
-        agg.values_in_timeframe,
-        max(lm.updated_at) as updated_at
-    from latest_metrics as lm
-        join metrics_aggregated_stats as agg
-        on (agg.full_table_name = lm.full_table_name and
-            agg.column_name = lm.column_name and
-            agg.metric_name = lm.metric_name)
+        vl.metric_value as latest_value,
+        vl.updated_at as value_updated_at,
+        tr.metric_avg,
+        tr.metric_stddev,
+        tr.timeframe_start as training_timeframe_start,
+        tr.timeframe_end as training_timeframe_end,
+        tr.values_in_timeframe,
+        max(vl.updated_at) as updated_at
+    from metrics_validation_set as vl
+        join metrics_training_set  as tr
+        on (tr.full_table_name = vl.full_table_name and
+            tr.column_name = vl.column_name and
+            tr.metric_name = vl.metric_name)
         where
-            lm.metric_value is not null
-            and agg.metric_avg is not null
-            and agg.metric_stddev is not null
-            and agg.values_in_timeframe >= {{ var('days_back') - 1 }}
+            vl.metric_value is not null
+            and tr.metric_avg is not null
+            and tr.metric_stddev is not null
+            and tr.values_in_timeframe >= {{ var('days_back') - 1 }}
     group by 1,2,3,4,5,6,7,8,9,10,11,12
 
 )
