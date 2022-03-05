2129b01b03f35ec55508c7ea530db6211712c926
Maayan-s <maayansalom@gmail.com>
2022-03-05 10:51:49 +0200

    joined models for faster run

--- models/edr/data_monitoring/anomaly_detection/latest_metrics.sql
+++ models/edr/data_monitoring/anomaly_detection/latest_metrics.sql
@@ -4,51 +4,20 @@ with metrics as (
 
 ),
 
-latest_metrics_times as (
+ latest_metrics_times as (
 
-    select
-        full_table_name,
-        column_name,
-        metric_name,
-        max(timeframe_end) over (partition by full_table_name, column_name, metric_name) as latest_timeframe_end
-    from metrics
-    where timeframe_end is not null
-
-),
-
-latest_metrics_no_times as (
-
-    select
-        full_table_name,
-        column_name,
-        metric_name,
-        max(updated_at) over (partition by full_table_name, column_name, metric_name) as latest_updated_at
-    from metrics
-    where timeframe_end is null
+     select
+         full_table_name,
+         column_name,
+         metric_name,
+         max(timeframe_end) as latest_timeframe_end,
+         max(updated_at) as latest_updated_at
+     from metrics
+     group by 1,2,3
 
 ),
 
-latest_time_based_metrics as (
-
-    select
-        m.id,
-        m.full_table_name,
-        m.column_name,
-        m.metric_name,
-        m.metric_value,
-        m.timeframe_start,
-        m.timeframe_end,
-        m.timeframe_duration_hours,
-        updated_at
-    from metrics as m join latest_metrics_times as lm
-        on (m.full_table_name = lm.full_table_name
-            and m.column_name = lm.column_name
-            and m.metric_name = lm.metric_name
-            and m.timeframe_end = lm.latest_timeframe_end)
-    where metric_value is not null
-),
-
-latest_no_time_metrics as (
+latest_metrics as (
 
      select
          m.id,
@@ -59,19 +28,13 @@ latest_no_time_metrics as (
          m.timeframe_start,
          m.timeframe_end,
          m.timeframe_duration_hours,
-         updated_at
-     from metrics as m join latest_metrics_no_times as lm
+         m.updated_at
+     from metrics as m join latest_metrics_times as lm
         on (m.full_table_name = lm.full_table_name
             and m.column_name = lm.column_name
             and m.metric_name = lm.metric_name
-            and m.updated_at = lm.latest_updated_at)
-),
-
-latest_metrics as (
-
-    select * from latest_time_based_metrics
-    union all
-    select * from latest_no_time_metrics
+            and (m.timeframe_end = lm.latest_timeframe_end or m.timeframe_end is null and m.updated_at = lm.latest_updated_at))
+     where metric_value is not null
 
 )
 
