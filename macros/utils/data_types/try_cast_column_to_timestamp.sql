640e0bda4d06340f101e807ebaa6771a8f17347b
Maayan-s <maayansalom@gmail.com>
2022-04-14 20:20:28 +0300

    changed safe cast for redshift

--- macros/utils/data_types/try_cast_column_to_timestamp.sql
+++ macros/utils/data_types/try_cast_column_to_timestamp.sql
@@ -1,17 +1,19 @@
 {% macro try_cast_column_to_timestamp(table_relation, timestamp_column) %}
 
-    {%- set query %}
-        select {{ dbt_utils.safe_cast(timestamp_column, dbt_utils.type_timestamp()) }} as timestamp_column
-        from {{ table_relation }}
-        where {{ timestamp_column }} is not null
-        limit 1
-    {%- endset %}
+    {# We only try casting for snowflake and bigquery, as these support safe cast and the query will not fail if the cast fails #}
+    {%- if target.type in ['snowflake','bigquery'] %}
+        {%- set query %}
+            select {{ dbt_utils.safe_cast(timestamp_column, dbt_utils.type_timestamp()) }} as timestamp_column
+            from {{ table_relation }}
+            where {{ timestamp_column }} is not null
+            limit 1
+        {%- endset %}
 
-    {%- set result = elementary.result_value(query) %}
-    {%- if result is not none %}
-        {{ return(true) }}
-    {%- else %}
-        {{ return(false) }}
+        {%- set result = elementary.result_value(query) %}
+        {%- if result is not none %}
+            {{ return(true) }}
+        {%- endif %}
     {%- endif %}
+    {{ return(false) }}
 
 {% endmacro %}
