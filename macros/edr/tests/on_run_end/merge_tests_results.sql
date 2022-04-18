1487f032abdb1e4c33458aee8263a8e7876c4b3f
oravi <oravi90@gmail.com>
2022-04-18 12:35:21 +0300

    Fixed CR comments

--- macros/edr/tests/on_run_end/merge_tests_results.sql
+++ macros/edr/tests/on_run_end/merge_tests_results.sql
@@ -1,48 +1,62 @@
 {% macro merge_test_results() %}
+    -- depends_on: {{ ref('data_monitoring_metrics') }}
+    -- depends_on: {{ ref('alerts_data_monitoring') }}
+    -- depends_on: {{ ref('alerts_schema_changes') }}
     {% if execute and flags.WHICH == 'test' %}
         {% set database_name, schema_name = elementary.get_package_database_and_schema('elementary') %}
         {% set temp_metrics_tables, temp_anomalies_tables, temp_schema_changes_tables = elementary.get_temp_tables_from_graph(database_name, schema_name) %}
-        {{ elementary.merge_temp_tables_data_to_target(database_name, schema_name, temp_metrics_tables, 'metrics', 'id') }}
-        {{ elementary.merge_temp_tables_data_to_target(database_name, schema_name, temp_anomalies_tables, 'anomalies', 'alert_id') }}
-        {{ elementary.merge_temp_tables_data_to_target(database_name, schema_name, temp_schema_changes_tables, 'schema_changes', 'alert_id') }}
+        {{ elementary.merge_test_tables_to_target(database_name, schema_name, temp_metrics_tables, ref('data_monitoring_metrics')) }}
+        {{ elementary.merge_test_tables_to_target(database_name, schema_name, temp_anomalies_tables, ref('alerts_data_monitoring')) }}
+        {{ elementary.merge_test_tables_to_target(database_name, schema_name, temp_schema_changes_tables, ref('alerts_schema_changes')) }}
     {% endif %}
     {{ return('') }}
 {% endmacro %}
 
 
-{% macro get_merge_target_and_union_query(temp_data_type, temp_tables_list) %}
-    {%- if temp_data_type == 'metrics' %}
-        {%- set temp_tables_union_query = elementary.union_metrics_query(temp_tables_list) %}
-        {{ return(('data_monitoring_metrics', temp_tables_union_query)) }}
-    {%- elif temp_data_type == 'anomalies' %}
-        {%- set temp_tables_union_query = elementary.union_anomalies_alerts_query(temp_tables_list) %}
-        {{ return(('alerts_data_monitoring', temp_tables_union_query)) }}
-    {%- elif temp_data_type == 'schema_changes' %}
-        {%- set temp_tables_union_query = elementary.union_schema_changes_query(temp_tables_list) %}
-        {{ return(('alerts_schema_changes',temp_tables_union_query)) }}
+{% macro get_test_tables_union_query(test_tables_list, target_name) %}
+    {% set test_tables_union_query = none %}
+    {%- if target_name == 'data_monitoring_metrics' %}
+        {%- set test_tables_union_query = elementary.union_metrics_query(test_tables_list) %}
+    {%- elif target_name == 'alerts_data_monitoring' %}
+        {%- set test_tables_union_query = elementary.union_anomalies_alerts_query(test_tables_list) %}
+    {%- elif target_name == 'alerts_schema_changes' %}
+        {%- set test_tables_union_query = elementary.union_schema_changes_query(test_tables_list) %}
     {%- else %}
-        {% do elementary.edr_log('Error: could not find merge target table') %}
-        {{ return('') }}
+        {% do elementary.edr_log('Error: invalid target name') %}
     {%- endif %}
+    {{ return(test_tables_union_query) }}
 {% endmacro %}
 
+{% macro get_target_unique_id(target_name) %}
+    {% set target_unique_id = none %}
+    {%- if target_name == 'data_monitoring_metrics' %}
+        {%- set target_unique_id = 'id' %}
+    {%- elif target_name == 'alerts_data_monitoring' %}
+        {%- set target_unique_id = 'alert_id' %}
+    {%- elif target_name == 'alerts_schema_changes' %}
+        {%- set target_unique_id = 'alert_id' %}
+    {%- else %}
+        {% do elementary.edr_log('Error: invalid target name') %}
+    {%- endif %}
+    {{ return(target_unique_id) }}
+{% endmacro %}
 
-{% macro merge_temp_tables_data_to_target(database_name, schema_name, temp_tables_list, temp_data_type, unique_id) %}
+{% macro merge_test_tables_to_target(database_name, schema_name, test_tables_list, target_relation) %}
 
-    {%- if temp_tables_list | length >0 %}
-        {%- set target_table_name, temp_tables_union_query = elementary.get_merge_target_and_union_query(temp_data_type, temp_tables_list) %}
-        {%- set target_relation = adapter.get_relation(database=database_name, schema=schema_name, identifier=target_table_name) %}
+    {%- if test_tables_list | length > 0 %}
+        {%- set test_tables_union_query = elementary.get_test_tables_union_query(test_tables_list, target_relation.identifier) %}
         {%- set temp_relation = dbt.make_temp_relation(target_relation) -%}
 
-        {%- if temp_tables_union_query %}
-            {{ elementary.debug_log('pulling ' ~ temp_data_type ~ ' from temp tables to: ' ~ database_name ~ '.' ~ schema_name ~ '.' ~ temp_relation.identifier) }}
-            {%- do run_query(dbt.create_table_as(True, temp_relation, temp_tables_union_query)) %}
+        {%- if test_tables_union_query %}
+            {{ elementary.debug_log('running union query from test tables to ' ~ temp_relation.database ~ '.' ~ temp_relation.schema ~ '.' ~ temp_relation.identifier) }}
+            {%- do run_query(dbt.create_table_as(True, temp_relation, test_tables_union_query)) %}
             {% set dest_columns = adapter.get_columns_in_relation(target_relation) %}
-            {{ elementary.debug_log('merging ' ~ temp_data_type ~ ' to: ' ~ database_name ~ '.' ~ schema_name ~ '.' ~ target_table_name) }}
-            {% set merge_sql = elementary.merge_sql(target_relation, temp_relation, unique_id, dest_columns) %}
+            {{ elementary.debug_log('merging ' ~ temp_relation.database ~ '.' ~ temp_relation.schema ~ '.' ~ temp_relation.identifier ~ ' to ' ~ target_relation.database ~ '.' ~ target_relation.schema ~ '.' ~ target_relation.identifier) }}
+            {% set target_unique_id = elementary.get_target_unique_id(target_relation.identifier) %}
+            {% set merge_sql = elementary.merge_sql(target_relation, temp_relation, target_unique_id, dest_columns) %}
             {% do run_query(merge_sql) %}
             {%- do adapter.commit() -%}
-            {{ elementary.debug_log('finished merging ' ~ temp_data_type) }}
+            {{ elementary.debug_log('finished merging') }}
         {%- endif %}
 
     {%- endif %}
