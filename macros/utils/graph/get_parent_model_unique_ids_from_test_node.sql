46e531f5b640f3716ca9ae48d72b718be3558f7a
oravi <oravi90@gmail.com>
2022-05-09 22:21:44 +0300

    Fixed typo

--- macros/utils/graph/get_parent_model_unique_ids_from_test_node.sql
+++ macros/utils/graph/get_parent_model_unique_ids_from_test_node.sql
@@ -1,5 +1,5 @@
 {% macro get_parent_model_unique_ids_from_test_node(test_node) %}
-    {% set nodes_in_current_pacakgae = [] %}
+    {% set nodes_in_current_package = [] %}
     {% set test_depends_on = test_node.get('depends_on') %}
     {% if test_depends_on %}
         {% set depends_on_nodes = test_depends_on.get('nodes') %}
@@ -9,11 +9,11 @@
                 {% set current_package_name = '.' ~ current_package_name ~ '.' %}
                 {% for node in depends_on_nodes %}
                     {% if current_package_name in node %}
-                        {% do nodes_in_current_pacakgae.append(node) %}
+                        {% do nodes_in_current_package.append(node) %}
                     {% endif %}
                 {% endfor %}
             {% endif %}
         {% endif %}
     {% endif %}
-    {{ return(nodes_in_current_pacakgae) }}
+    {{ return(nodes_in_current_package) }}
 {% endmacro %}
