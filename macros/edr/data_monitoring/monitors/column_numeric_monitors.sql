a60b85dd3f30714f239d52d07dd21bf700f8a9d0
Maayan-s <maayansalom@gmail.com>
2022-05-24 23:42:20 +0300

    Changed metric values to float

--- macros/edr/data_monitoring/monitors/column_numeric_monitors.sql
+++ macros/edr/data_monitoring/monitors/column_numeric_monitors.sql
@@ -1,17 +1,17 @@
 {% macro max(column_name) -%}
-    max(cast({{ column_name }} as {{ dbt_utils.type_int() }}))
+    max(cast({{ column_name }} as {{ dbt_utils.type_float() }}))
 {%- endmacro %}
 
 {% macro min(column_name) -%}
-    min(cast({{ column_name }} as {{ dbt_utils.type_int() }}))
+    min(cast({{ column_name }} as {{ dbt_utils.type_float() }}))
 {%- endmacro %}
 
 {% macro average(column_name) -%}
-    avg(cast({{ column_name }} as {{ dbt_utils.type_int() }}))
+    avg(cast({{ column_name }} as {{ dbt_utils.type_float() }}))
 {%- endmacro %}
 
 {% macro zero_count(column_name) %}
-    coalesce(sum(case when {{ column_name }} is null then 1 when cast({{ column_name }} as {{ dbt_utils.type_int() }}) = 0 then 1 else 0 end), 0)
+    coalesce(sum(case when {{ column_name }} is null then 1 when cast({{ column_name }} as {{ dbt_utils.type_float() }}) = 0 then 1 else 0 end), 0)
 {% endmacro %}
 
 {% macro zero_percent(column_name) %}
@@ -19,9 +19,9 @@
 {% endmacro %}
 
 {% macro standard_deviation(column_name) -%}
-    stddev(cast({{ column_name }} as {{ dbt_utils.type_int() }}))
+    stddev(cast({{ column_name }} as {{ dbt_utils.type_float() }}))
 {%- endmacro %}
 
 {% macro variance(column_name) -%}
-    variance(cast({{ column_name }} as {{ dbt_utils.type_int() }}))
+    variance(cast({{ column_name }} as {{ dbt_utils.type_float() }}))
 {%- endmacro %}
