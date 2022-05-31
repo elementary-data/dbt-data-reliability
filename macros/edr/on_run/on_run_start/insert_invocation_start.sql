8a6a9eb99dd4659d964930dc79d6a21795e4e197
Maayan-s <maayansalom@gmail.com>
2022-05-31 21:42:21 +0300

    Update insert_invocation_start.sql

--- macros/edr/on_run/on_run_start/insert_invocation_start.sql
+++ macros/edr/on_run/on_run_start/insert_invocation_start.sql
@@ -1,11 +1,8 @@
 {% macro insert_invocation_start() %}
-    {%- set invocation_times_table = elementary.get_invocations_table() %}
-    {%- if invocation_times_table %}
-        {%- set insert_invocation_query %}
-            insert into {{ invocation_times_table }} values
-            ('{{ invocation_id }}', {{ elementary.run_start_column() }}, {{ elementary.null_timestamp() }})
-        {%- endset %}
-        {% do run_query(insert_invocation_query) %}
+    {%- set invocation_times_relation = elementary.get_invocations_table() %}
+    {%- if invocation_times_relation %}
+        {%- set invocation_time_dict = [{'invocation_id': invocation_id, 'invocation_started_at': run_started_at.strftime("%Y-%m-%d %H:%M:%S") }] %}
+        {{ elementary.insert_dicts(invocation_times_relation, invocation_time_dict) }}
     {%- endif %}
     {{ return('') }}
 {% endmacro %}
