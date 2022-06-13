035bbf5ff7f95e34a74962545aa996f62421021d
Maayan-s <maayansalom@gmail.com>
2022-06-13 15:13:58 +0300

    Changes string and long string length, added casting in places where there was danger before

--- macros/utils/data_types/cast_column.sql
+++ macros/utils/data_types/cast_column.sql
@@ -10,6 +10,14 @@
     cast({{ column }} as {{ elementary.type_string() }})
 {%- endmacro -%}
 
+{%- macro cast_as_long_string(column) -%}
+    cast({{ column }} as {{ elementary.type_long_string() }})
+{%- endmacro -%}
+
+{%- macro cast_as_bool(column) -%}
+    cast({{ column }} as {{ elementary.type_bool() }})
+{%- endmacro -%}
+
 {%- macro const_as_string(string) -%}
     cast('{{ string }}' as {{ elementary.type_string() }})
 {%- endmacro -%}
--- models/edr/data_monitoring/schema_changes/column_changes.sql
+++ models/edr/data_monitoring/schema_changes/column_changes.sql
@@ -108,4 +108,13 @@ column_changes_desc as (
 
 )
 
-select * from column_changes_desc
+select
+    {{ elementary.cast_as_string('change_id') }} as change_id,
+    {{ elementary.cast_as_string('database_name') }} as database_name,
+    {{ elementary.cast_as_string('schema_name') }} as schema_name,
+    {{ elementary.cast_as_string('table_name') }} as table_name,
+    {{ elementary.cast_as_string('column_name') }} as column_name,
+    {{ elementary.cast_as_timestamp('detected_at') }} as detected_at,
+    {{ elementary.cast_as_string('change') }} as change,
+    {{ elementary.cast_as_long_string('change_description') }} as change_description
+from column_changes_desc
