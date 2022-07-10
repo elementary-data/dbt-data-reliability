4752d7a1466ad6705f6d8fc077d288530c2ecc0d
Elon Gliksberg <elongliks@gmail.com>
2022-07-10 14:08:23 +0300

    Alerts don't strive to be generic, added model alerts.

---
+++ models/edr/alerts/alerts_dbt_models.sql
@@ -0,0 +1,29 @@
+{{
+  config(
+    materialized = 'view',
+    bind=False
+  )
+}}
+
+with model_run_results as (
+    select * from {{ ref('model_run_results') }}
+),
+
+model_alerts as (
+    select model_execution_id as alert_id,
+           unique_id,
+           generated_at as detected_at,
+           database_name,
+           schema_name,
+           message,
+           owner as owners,
+           tags,
+           alias,
+           status,
+           full_refresh
+    from model_run_results
+    where lower(status) != 'success'
+)
+
+select * from model_alerts
+{{ dbt_utils.group_by(11) }}
