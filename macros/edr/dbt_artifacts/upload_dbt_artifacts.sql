e9719671782c0eb46a3b91b69261996d7e611ffe
Elon Gliksberg <elongliks@gmail.com>
2022-07-11 12:05:32 +0300

    Made the deprecation warning more clear.

--- macros/edr/dbt_artifacts/upload_dbt_artifacts.sql
+++ macros/edr/dbt_artifacts/upload_dbt_artifacts.sql
@@ -1,5 +1,5 @@
 {% macro upload_dbt_artifacts(results) %}
-        {% do elementary.edr_log("Deprecated - Please remove the call to elementary.upload_dbt_artifacts() on your on-run-end hook.") %}
+        {% do elementary.edr_log("Deprecated - Please remove the call to elementary.upload_dbt_artifacts() on your on-run-end hook as it happens automatically now.") %}
 {% endmacro %}
 
 
