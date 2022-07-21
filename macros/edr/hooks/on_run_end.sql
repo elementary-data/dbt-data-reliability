b14f0163c7a8782db9c87e2da1b75ff1983221ff
Elon Gliksberg <elongliks@gmail.com>
2022-07-21 17:13:47 +0300

    Changed the order of the on-run-end hook macros.

--- macros/edr/hooks/on_run_end.sql
+++ macros/edr/hooks/on_run_end.sql
@@ -1,4 +1,4 @@
 {% macro on_run_end() %}
-  {{ elementary.upload_run_results(results) }}
   {{ elementary.handle_test_results(results) if flags.WHICH in ['test', 'build'] }}
+  {{ elementary.upload_run_results(results) }}
 {% endmacro %}
