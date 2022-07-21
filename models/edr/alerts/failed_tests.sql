4052ad568bf262026de0c9a7cd85ebf82407b22c
Elon Gliksberg <elongliks@gmail.com>
2022-07-21 13:01:57 +0300

    Moved variable declaration outside of block.

--- models/edr/alerts/failed_tests.sql
+++ models/edr/alerts/failed_tests.sql
@@ -5,14 +5,14 @@
   )
 }}
 
+{% set anomaly_detection_relation = adapter.get_relation(this.database, this.schema, 'alerts_anomaly_detection') %}
+// Backwards compatibility support for a renamed model.
+{% set data_monitoring_relation = adapter.get_relation(this.database, this.schema, 'alerts_data_monitoring') %}
+
 with failed_tests as (
      select * from {{ ref('elementary', 'alerts_schema_changes') }}
      union all
      select * from {{ ref('elementary', 'alerts_dbt_tests') }}
-
-    {% set anomaly_detection_relation = adapter.get_relation(this.database, this.schema, 'alerts_anomaly_detection') %}
-    // Backwards compatibility support for a renamed model.
-    {% set data_monitoring_relation = adapter.get_relation(this.database, this.schema, 'alerts_data_monitoring') %}
 
     {% if anomaly_detection_relation %}
         union all
