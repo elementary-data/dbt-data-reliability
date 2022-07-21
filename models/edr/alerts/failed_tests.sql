ce60a32ef01ec825be7a0faabaab7f94d85a8a4f
Elon Gliksberg <elongliks@gmail.com>
2022-07-21 11:46:04 +0300

    Changed a model to support renamed model.

--- models/edr/alerts/failed_tests.sql
+++ models/edr/alerts/failed_tests.sql
@@ -9,19 +9,18 @@ with failed_tests as (
      select * from {{ ref('elementary', 'alerts_schema_changes') }}
      union all
      select * from {{ ref('elementary', 'alerts_dbt_tests') }}
-     union all
 
     {% set anomaly_detection_relation = adapter.get_relation(this.database, this.schema, 'alerts_anomaly_detection') %}
     // Backwards compatibility support for a renamed model.
     {% set data_monitoring_relation = adapter.get_relation(this.database, this.schema, 'alerts_data_monitoring') %}
 
     {% if anomaly_detection_relation %}
+        union all
         select * from {{ anomaly_detection_relation }}
     {% endif %}
-    {% if anomaly_detection_relation or data_monitoring_relation %}
-        union all
-    {% endif %}
+
     {% if data_monitoring_relation %}
+        union all
         select * from {{ data_monitoring_relation }}
     {% endif %}
 )
