b281c92ec7333e2a07be43e148f099f5e0a89491
civitaspo <civitaspo@gmail.com>
2022-07-27 00:14:39 +0900

    Revert "Support alias"
    This reverts commit e016c04db95fc1a08941807979404f1348a29a37.

--- macros/edr/tests/test_utils/get_model_graph_node.sql
+++ macros/edr/tests/test_utils/get_model_graph_node.sql
@@ -4,11 +4,11 @@
         {% set test_graph_node = model %}
         {% set test_depends_on_unique_ids = test_graph_node.depends_on.nodes %}
         {# model relation is the relation object of the model where the test is defined #}
-        {% set model_node = elementary.get_node_by_relation(model_relation) %}
+        {% set model_name = model_relation.name | lower %}
         {% set depends_on_nodes = elementary.get_nodes_by_unique_ids(test_depends_on_unique_ids) %}
         {% if depends_on_nodes %}
             {% for node in depends_on_nodes %}
-                {% if node.unique_id == model_node.unique_id %}
+                {% if node.name | lower == model_name %}
                     {{ return(node) }}
                 {% endif %}
             {% endfor %}
