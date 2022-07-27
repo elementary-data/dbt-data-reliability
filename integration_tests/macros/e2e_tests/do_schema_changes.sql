c53ab1bab080011156d7ffe926cbf4fcc92cebc3
oravi <oravi90@gmail.com>
2022-07-27 14:50:37 +0300

    Fixed schema changes validation

--- integration_tests/macros/e2e_tests/do_schema_changes.sql
+++ integration_tests/macros/e2e_tests/do_schema_changes.sql
@@ -1,35 +1,17 @@
 {% macro do_schema_changes() %}
+    {{- return(adapter.dispatch('do_schema_changes')()) -}}
+{% endmacro %}
+
+{% macro default__do_schema_changes() %}
 
     {% set schema_changes_query -%}
 
-    {# column_removed #}
-    {# Dropping the column group_a from the table groups. #}
-    alter table {{ ref('groups') }}
-        drop column group_a;
-
-    {# column_removed #}
-    {# Dropping the column offsides from the stats_players table. #}
-    alter table {{ ref('stats_players') }}
-        drop column offsides;
-
-    {# column_added #}
-    {# Adding a column called red_cards to the stats_players table. #}
-    alter table {{ ref('stats_players') }}
-        add column red_cards {{ elementary.type_string() }};
-
-    {# column_added #}
-    {# Adding a column called key_crosses to the stats_players table. #}
-    alter table {{ ref('stats_players') }}
-        add column key_crosses {{ elementary.type_string() }};
-
-    {# column_type_change #}
-    {# Dropping the column goals from the stats_team table and then adding it back in with a different type. #}
-    alter table {{ ref('stats_team') }}
-        drop column goals;
-    alter table {{ ref('stats_team') }}
-        add column goals {{ dbt_utils.type_string() }};
-
-
+    drop table {{ ref('groups') }};
+    drop table {{ ref('stats_players') }};
+    drop table {{ ref('stats_team') }};
+    alter table {{ ref('groups_validation') }} rename to {{ ref('groups').identifier }};
+    alter table {{ ref('stats_players_validation') }} rename to {{ ref('stats_players').identifier }};
+    alter table {{ ref('stats_team_validation') }} rename to {{ ref('stats_team').identifier }};
 
     {%- endset %}
     {% do run_query(schema_changes_query) %}
@@ -37,3 +19,19 @@
 
 {% endmacro %}
 
+{% macro bigquery__do_schema_changes() %}
+
+    {% set schema_changes_query -%}
+
+    drop table {{ ref('groups') }};
+    drop table {{ ref('stats_players') }};
+    drop table {{ ref('stats_team') }};
+    alter table {{ ref('groups_validation') }} rename to {{ elementary.quote_column(ref('groups').identifier) }};
+    alter table {{ ref('stats_players_validation') }} rename to {{ elementary.quote_column(ref('stats_players').identifier) }};
+    alter table {{ ref('stats_team_validation') }} rename to {{ elementary.quote_column(ref('stats_team').identifier) }};
+
+    {%- endset %}
+    {% do run_query(schema_changes_query) %}
+    {% do elementary.edr_log("schema changes executed successfully") %}
+
+{% endmacro %}
