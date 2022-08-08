0c766a73c0a3da74e3dec368eba15c2b655eeac9
IDoneShaveIt <idanshavit31@gmail.com>
2022-08-08 12:58:51 +0300

    fixed concat list with separator

---
+++ macros/utils/sql_utils/list_concat_with_separator.sql
@@ -0,0 +1,14 @@
+{% macro list_concat_with_separator(item_list, separator, handle_nulls = true) %}
+    {% set new_list = [] %}
+    {% for item in item_list %}
+        {% set new_item = item %}
+        {% if handle_nulls %}
+            {% set new_item = "case when " ~ elementary.cast_as_string(item) ~ " is null then 'NULL' else " ~ elementary.cast_as_string(item) ~ " end" %}
+        {% endif %}
+        {% do new_list.append(new_item) %}
+        {% if not loop.last %}
+            {% do new_list.append("'" ~ separator ~ "'") %}
+        {% endif %}
+    {% endfor %}
+    {{ return(elementary.join_list(new_list, " || ")) }}
+{% endmacro %}
