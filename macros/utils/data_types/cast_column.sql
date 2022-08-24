58507976e3ce74e6dd3452534d19fcab9fcc81dd
IDoneShaveIt <idanshavit31@gmail.com>
2022-08-24 09:28:08 +0300

    Fixed the use of current_timestamp macro

--- macros/utils/data_types/cast_column.sql
+++ macros/utils/data_types/cast_column.sql
@@ -1,5 +1,5 @@
 {%- macro cast_as_timestamp(timestamp_field) -%}
-    cast({{ timestamp_field }} as {{- elementary.type_timestamp() -}})
+    cast({{ timestamp_field }} as {{ elementary.type_timestamp() }})
 {%- endmacro -%}
 
 {%- macro cast_as_float(column) -%}
