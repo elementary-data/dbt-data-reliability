e63f580b910a1a74ec260ea8ef28ad919c1ecbbb
IDoneShaveIt <idanshavit31@gmail.com>
2022-08-24 09:11:48 +0300

    fixed typo

--- macros/utils/cross_db_utils/type_biginit.sql
+++
@@ -1,7 +0,0 @@
-{% macro type_biginit() %}
-    {% if dbt_version >= '1.2.0' %}
-        {{ return(dbt.type_biginit()) }}
-    {% else %}
-        {{ return(dbt_utils.type_biginit()) }}
-    {% endif %}
-{% endmacro %}
