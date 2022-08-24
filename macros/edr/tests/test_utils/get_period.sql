2e9255d51b07428aaad4afca04743ba072a1e0e2
Rodrigo Loredo <rloredo@fleyx.com>
2022-08-24 16:56:54 +0200

    add macro to get period from config

---
+++ macros/edr/tests/test_utils/get_period.sql
@@ -0,0 +1,16 @@
+{% macro get_period(period, model_node) %}
+    {% set supported_values = ['hour', 'day', 'week', 'month'] %}
+    {% if period is none %}
+        {% set elementary_config = elementary.get_elementary_config_from_node(model_node) %}
+        {% if elementary_config and elementary_config is mapping %}
+            {% set period = elementary_config.get('period').lower() %}
+        {% endif %}
+    {% endif %}
+    {% if period in supported_values %}
+        {{ return(period) }}
+    {% else %}
+        {{- elementary.edr_log('Specified period not supported. Using default: day') }}
+        {{ return('day') }}
+    {% endif %}
+    {{- return(none) -}}
+{% endmacro %}
