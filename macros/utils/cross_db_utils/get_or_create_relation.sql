1e0185a450445784472919a7fe37781cb2973a71
Elon Gliksberg <elongliks@gmail.com>
2022-08-28 19:47:14 +0300

    Added the 'make_match_kwargs' macro.

--- macros/utils/cross_db_utils/get_or_create_relation.sql
+++ macros/utils/cross_db_utils/get_or_create_relation.sql
@@ -1,4 +1,5 @@
 {% macro get_or_create_relation(database, schema, identifier, type) %}
+  {% set database, schema, identifier = elementary.make_match_kwargs(database, schema, identifier) %}
   {% set relation_existed, target_relation = dbt.get_or_create_relation(database, schema, identifier, type) %}
   {% if not relation_existed %}
     {{ adapter.cache_added(target_relation) }}
