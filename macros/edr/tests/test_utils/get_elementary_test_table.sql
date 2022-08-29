2f1a6bf5450bc49977df7b3639b63f0f3e110c7c
Elon Gliksberg <elongliks@gmail.com>
2022-08-30 01:53:31 +0300

    Simplified the dispatched macros.

--- macros/edr/tests/test_utils/get_elementary_test_table.sql
+++ macros/edr/tests/test_utils/get_elementary_test_table.sql
@@ -1,23 +1,32 @@
 {% macro get_elementary_test_table(test_relations, test_name, suffix) %}
     {% set test_table_name = elementary.table_name_with_suffix(test_name, suffix) %}
-    {{ return(adapter.dispatch('get_elementary_test_table', 'elementary')(test_relations, test_table_name )) }}
-{% endmacro %}
-
-
-{% macro default__get_elementary_test_table(test_relations, test_table_name) %}
+    {% set relation_keys = adapter.dispatch('get_relation_keys', 'elementary')() %}
     {% for test_relation in test_relations %}
         {% if test_relation['name'].lower() == test_table_name.lower() %}
-            {{ return(api.Relation.create(database=test_relation['database'], schema=test_relation['schema'], identifier=test_relation['name'])) }}
+            {{ return(api.Relation.create(
+                database=test_relation[relation_keys['database']],
+                schema=test_relation[relation_keys['schema']],
+                identifier=test_relation[relation_keys['identifier']]
+                ))
+            }}
         {% endif %}
     {% endfor %}
     {{ return(none) }}
 {% endmacro %}
 
-{% macro snowflake__get_elementary_test_table(test_relations, test_table_name) %}
-    {% for test_relation in test_relations %}
-        {% if test_relation['name'].lower() == test_table_name.lower() %}
-            {{ return(api.Relation.create(database=test_relation['database_name'], schema=test_relation['schema_name'], identifier=test_relation['name'])) }}
-        {% endif %}
-    {% endfor %}
-    {{ return(none) }}
+
+{% macro default__get_relation_keys() %}
+    {{ return({
+        'database': 'database',
+        'schema': 'schema',
+        'identifier': 'name'
+    }) }}
+{% endmacro %}
+
+{% macro snowflake__get_relation_keys() %}
+    {{ return({
+        'database': 'database_name',
+        'schema': 'schema_name',
+        'identifier': 'name'
+    }) }}
 {% endmacro %}
