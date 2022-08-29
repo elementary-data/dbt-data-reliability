2a90fdb1057ab2c0cd3e9fd1ec1ad0954e81f7ff
Elon Gliksberg <elongliks@gmail.com>
2022-08-29 23:45:15 +0300

    Using .lower() when comparing the test names.

--- macros/edr/tests/on_run_end/handle_tests_results.sql
+++ macros/edr/tests/on_run_end/handle_tests_results.sql
@@ -375,10 +375,11 @@
 {% macro get_elementary_test_table(test_relations, test_name, suffix) %}
     {% set test_table_name = elementary.table_name_with_suffix(test_name, suffix) %}
     {% for test_relation in test_relations %}
-        {% if test_relation['name'] == test_table_name %}
+        {% if test_relation['name'].lower() == test_table_name.lower() %}
             {{ return(api.Relation.create(database=test_relation['database_name'], schema=test_relation['schema_name'], identifier=test_relation['name'])) }}
         {% endif %}
     {% endfor %}
+    {{ exceptions.raise_compiler_error('Could not find ' ~ test_table_name) }}
 {% endmacro %}
 
 
