f43936972cdb1e5c3a75b51ea33cc00303ecfadc
IDoneShaveIt <idanshavit31@gmail.com>
2022-09-05 11:23:38 +0300

    make sure everything uses current timestamp

--- macros/edr/system/system_utils/timestamp_column.sql
+++ macros/edr/system/system_utils/timestamp_column.sql
@@ -5,7 +5,3 @@
 {% macro current_timestamp_column() %}
     cast ({{elementary.current_timestamp_in_utc()}} as {{ elementary.type_timestamp() }})
 {% endmacro %}
-
-{% macro current_timestamp_utc_now_column() %}
-    cast ('{{ modules.datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") }}' as {{ elementary.type_timestamp() }})
-{% endmacro %}
