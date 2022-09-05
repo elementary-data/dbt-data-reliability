1f923d1e3368f19a11b8f68d9ba98ef0f502bd2c
IDoneShaveIt <idanshavit31@gmail.com>
2022-09-05 15:23:42 +0300

    Fix schema tests timing problem

--- macros/edr/system/system_utils/timestamp_column.sql
+++ macros/edr/system/system_utils/timestamp_column.sql
@@ -5,3 +5,7 @@
 {% macro current_timestamp_column() %}
     cast ({{elementary.current_timestamp_in_utc()}} as {{ elementary.type_timestamp() }})
 {% endmacro %}
+
+{% macro formatted_current_timestamp_column() %}
+    cast ('{{ modules.datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") }}' as {{ elementary.type_timestamp() }})
+{% endmacro %}
