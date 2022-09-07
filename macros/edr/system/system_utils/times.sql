4ff5d25ff9d7b87ecdff87fe580c3150730739ac
IDoneShaveIt <idanshavit31@gmail.com>
2022-09-07 13:00:41 +0300

    fixed current_timestamp_in_utc_as_string

--- macros/edr/system/system_utils/times.sql
+++ macros/edr/system/system_utils/times.sql
@@ -1,3 +1,4 @@
 {% macro current_timestamp_utc_as_string(time_format=elementary.get_config_var('time_format')) %}
-    modules.datetime.datetime.utcnow().strftime(time_format)
+    {% set current_timestamp_as_string = modules.datetime.datetime.utcnow().strftime(time_format) %}
+    {{ return(current_timestamp_as_string) }}
 {% endmacro %}
