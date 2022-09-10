5c752e95541579548f81a23e65f6421822a55a07
Maayan-s <maayansalom@gmail.com>
2022-09-10 12:38:12 +0300

    period bucket CTEs working for day and hour on 4 platforms

--- macros/edr/system/system_utils/period_buckets_cte.sql
+++ macros/edr/system/system_utils/period_buckets_cte.sql
@@ -1,21 +1,22 @@
 {% macro period_buckets_cte(period) %}
-    {%- set max_bucket_end = elementary.get_max_bucket_end(period) %}
-    {%- set min_bucket_end = elementary.get_min_bucket_end(period) %}
+    {%- set max_bucket_end = elementary.cast_as_timestamp(elementary.get_max_bucket_end(period)) %}
+    {%- set min_bucket_end = elementary.cast_as_timestamp(elementary.get_min_bucket_end(period)) %}
     {{ adapter.dispatch('period_buckets_cte','elementary')(period, max_bucket_end, min_bucket_end) }}
 {% endmacro %}
 
 
 {% macro default__period_buckets_cte(period, max_bucket_end, min_bucket_end) -%}
     {%- set period_buckets_cte %}
-        with dates as (
-            select {{ elementary.cast_as_timestamp(min_bucket_end) }} as date
+        with time_frames as (
+            select {{ min_bucket_end }} as time_frame
         union all
-        select {{ elementary.timeadd(period, '1', 'date') }}
-        from dates
-        where {{ elementary.timeadd(period, '1', 'date') }} <= {{ elementary.cast_as_timestamp(max_bucket_end) }}
+        select {{ elementary.timeadd(period, '1', 'time_frame') }}
+        from time_frames
+        where {{ elementary.timeadd(period, '1', 'time_frame') }} <= {{ max_bucket_end }}
             )
-        select date as edr_period_bucket
-        from dates
+        select time_frame as edr_period_bucket
+        from time_frames
+        order by edr_period_bucket desc
     {%- endset %}
     {{ return(period_buckets_cte) }}
 {% endmacro %}
@@ -24,24 +25,29 @@
 {% macro bigquery__period_buckets_cte(period, max_bucket_end, min_bucket_end) %}
     {%- set period_buckets_cte %}
         select edr_period_bucket
-        from unnest(generate_timestamp_array({{ elementary.cast_as_timestamp(min_bucket_end) }}, {{ elementary.cast_as_timestamp(max_bucket_end) }}, interval 1 {{ period }})) as edr_period_bucket
+        from unnest(generate_timestamp_array({{ min_bucket_end }}, {{ max_bucket_end }}, interval 1 {{ period }})) as edr_period_bucket
+        order by edr_period_bucket desc
     {%- endset %}
     {{ return(period_buckets_cte) }}
 {% endmacro %}
+
 
 {% macro redshift__period_buckets_cte(period, max_bucket_end, min_bucket_end) %}
-    {# TODO redshift implementation of this CTE#}
-    {%- set days_back = elementary.get_config_var('days_back') %}
-
     {%- set period_buckets_cte %}
-        {%- for i in range(0, days_back+1) %}
-            {%- set daily_bucket = "'"~ (elementary.get_run_started_at() - modules.datetime.timedelta(i)).strftime("%Y-%m-%d 00:00:00") ~"'" %}
-            select {{ elementary.cast_as_timestamp(daily_bucket) }} as edr_daily_bucket
-            {%- if not loop.last %} union all {%- endif %}
-        {%- endfor %}
+        with recursive time_frames(time_frame) as
+           (
+            select {{ min_bucket_end }} as time_frame
+                union all
+            select dateadd({{ period }}, 1, cast(time_frame as TIMESTAMP))
+            from time_frames tf
+            where tf.time_frame < {{ max_bucket_end }}
+           )
+        select time_frame as edr_period_bucket from time_frames
+        order by edr_period_bucket desc;
     {%- endset %}
     {{ return(period_buckets_cte) }}
 {% endmacro %}
+
 
 {% macro databricks__period_buckets_cte(period, max_bucket_end, min_bucket_end) %}
     select edr_period_bucket
