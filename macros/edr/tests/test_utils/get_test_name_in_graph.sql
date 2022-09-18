29eac8084ba15d7c7f80b151418d88f799135eec
IDoneShaveIt <idanshavit31@gmail.com>
2022-09-18 19:47:16 +0300

    use unique key instead of custom test name for elementary tests table to prevent the use of the same metrics for 2 different tests with the same name

--- macros/edr/tests/test_utils/get_test_name_in_graph.sql
+++ macros/edr/tests/test_utils/get_test_name_in_graph.sql
@@ -1,4 +1,9 @@
 {% macro get_test_name_in_graph() %}
-    {% set test_name_in_graph = model.name %}
+    {% set test_name = model.name %}
+    {% set test_alias = model.alias %}
+    {% set test_unique_id = model.unique_id %}
+    {# Currently dbt allows multiple tests with the same custom name. #}
+    {# In that case we want to use the unique id of the test instead so we won't use the same tests tables for different tests. #}
+    {% set test_name_in_graph = test_name if test_name != test_alias else test_unique_id | replace(".", "_") %}
     {{ return(test_name_in_graph) }}
 {% endmacro %}
