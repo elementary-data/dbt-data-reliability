44df8884cf610334ce18594a0f3952982367d97b
Elon Gliksberg <elongliks@gmail.com>
2022-09-22 21:48:04 +0300

    Fixed the singular no-ref test.

--- integration_tests/tests/singular_test_with_no_ref.sql
+++ integration_tests/tests/singular_test_with_no_ref.sql
@@ -1,1 +1,3 @@
-select min from {{ elementary.target_database() ~ '.' ~ target.schema ~ '.' ~ 'numeric_column_anomalies' }} where min < 100
+{% set target_relation = api.Relation.create(database=elementary.target_database(), schema=target.schema, identifier='numeric_column_anomalies') %}
+
+select min from {{ target_relation }} where min < 100
