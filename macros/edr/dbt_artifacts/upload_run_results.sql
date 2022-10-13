0606c0d15b1370daf1f3b31fefe545c46af714d1
Elon Gliksberg <elongliks@gmail.com>
2022-10-13 12:54:22 +0300

    Added 'get_elementary_relation()' macro.

--- macros/edr/dbt_artifacts/upload_run_results.sql
+++ macros/edr/dbt_artifacts/upload_run_results.sql
@@ -6,10 +6,7 @@
             {{ return('') }}
         {% endif %}
         {{ elementary.debug_log("Uploading run results.") }}
-        {% set database_name, schema_name = elementary.get_model_database_and_schema('elementary', 'dbt_run_results') %}
-        {%- set dbt_run_results_relation = adapter.get_relation(database=database_name,
-                                                                schema=schema_name,
-                                                                identifier='dbt_run_results') -%}
+        {%- set dbt_run_results_relation = elementary.get_elementary_relation('dbt_run_results') -%}
         {%- if dbt_run_results_relation -%}
             {% do elementary.upload_artifacts_to_table(dbt_run_results_relation, results, elementary.get_flatten_run_result_callback(),
                                                        should_commit=True) %}
