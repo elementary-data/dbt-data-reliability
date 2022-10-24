8863491b054b5793628a21370316e2b64f763456
Elon Gliksberg <elongliks@gmail.com>
2022-10-24 18:50:51 +0300

    Added 'dbt_version' and 'full_refresh' columns.

--- macros/edr/dbt_artifacts/upload_dbt_invocation.sql
+++ macros/edr/dbt_artifacts/upload_dbt_invocation.sql
@@ -17,8 +17,10 @@
       'run_completed_at': now_str,
       'generated_at': now_str,
       'command': flags.WHICH,
-      'selected_resources': selected_resources,
-      'vars': invocation_vars
+      'dbt_version': dbt_version,
+      'full_refresh': flags.FULL_REFRESH,
+      'vars': invocation_vars,
+      'selected_resources': selected_resources
   } %}
 
   {% do elementary.insert_rows(relation, [dbt_invocation], should_commit=true) %}
@@ -31,6 +33,8 @@
       ('run_completed_at', 'string'),
       ('generated_at', 'string'),
       ('command', 'string'),
+      ('dbt_version', 'string'),
+      ('full_refresh', 'string'),
       ('vars', 'long_string'),
       ('selected_resources', 'long_string'),
     ])) }}
