d559958c74c41ae23c8fcf1960e679c03765926c
Elon Gliksberg <elongliks@gmail.com>
2022-10-24 13:41:39 +0300

    Calculating the data only if about to insert.

--- macros/edr/system/system_utils/upload_information.sql
+++ macros/edr/system/system_utils/upload_information.sql
@@ -5,19 +5,16 @@
     {{ return('') }}
   {% endif %}
 
-  {% set data = [
-    {'key': 'dbt_version', 'value': dbt_version},
-    {'key': 'elementary_version', 'value': elementary.get_elementary_package_version()},
-  ] %}
-
   {% if results %}
     {% if elementary.get_result_node(identifier) %}
       {{ return('') }}
     {% else %}
       {% do dbt.truncate_relation(relation) %}
-      {% do elementary.insert_rows(relation, data) %}
-    {% endif %}
-  {% else %}
-    {% do elementary.insert_rows(relation, data) %}
   {% endif %}
+
+  {% set data = [
+    {'key': 'dbt_version', 'value': dbt_version},
+    {'key': 'elementary_version', 'value': elementary.get_elementary_package_version()},
+  ] %}
+  {% do elementary.insert_rows(relation, data) %}
 {% endmacro %}
