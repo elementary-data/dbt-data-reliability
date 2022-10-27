1c8b0ef69785721fd22842499fff75c3065c72a2
Elon Gliksberg <elongliks@gmail.com>
2022-10-27 11:30:05 +0300

    Updated surrogate_key.

--- macros/utils/cross_db_utils/current_timestamp.sql
+++ macros/utils/cross_db_utils/current_timestamp.sql
@@ -1,7 +1,7 @@
 {% macro current_timestamp() %}
   {% set macro = dbt.current_timestamp_backcompat or dbt_utils.current_timestamp %}
   {% if not macro %}
-    {{ exceptions.raise_compiler_error("Did not find a current_timestamp macro.") }}
+    {{ exceptions.raise_compiler_error("Did not find a `current_timestamp` macro.") }}
   {% endif %}
   {{ return(macro) }}
 {% endmacro %}
@@ -10,7 +10,7 @@
 {% macro current_timestamp_in_utc() %}
   {% set macro = dbt.current_timestamp_in_utc_backcompat or dbt_utils.current_timestamp_in_utc %}
   {% if not macro %}
-    {{ exceptions.raise_compiler_error("Did not find a current_timestamp_in_utc macro.") }}
+    {{ exceptions.raise_compiler_error("Did not find a `current_timestamp_in_utc` macro.") }}
   {% endif %}
   {{ return(macro) }}
 {% endmacro %}
--- macros/utils/cross_db_utils/surrogate_key.sql
+++ macros/utils/cross_db_utils/surrogate_key.sql
@@ -1,7 +1,7 @@
-{% macro surrogate_key() %}
+{% macro surrogate_key(fields) %}
   {% set macro = dbt_utils.generate_surrogate_key or dbt_utils.surrogate_key %}
   {% if not macro %}
-    {{ exceptions.raise_compiler_error("Did not find a surrogate_key macro.") }}
+    {{ exceptions.raise_compiler_error("Did not find a `surrogate_key` macro.") }}
   {% endif %}
-  {{ return(macro) }}
+  {{ return(macro(fields)) }}
 {% endmacro %}
