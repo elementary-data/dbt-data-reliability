b2242413908ec8261d89dae9e566ccf4805eb221
Elon Gliksberg <elongliks@gmail.com>
2022-10-27 17:20:46 +0300

    Changed the quoting of macro definition.

--- macros/utils/cross_db_utils/current_timestamp.sql
+++ macros/utils/cross_db_utils/current_timestamp.sql
@@ -1,16 +1,16 @@
-{% macro current_timestamp() %}
+{% macro current_timestamp() -%}
   {% set macro = dbt.current_timestamp_backcompat or dbt_utils.current_timestamp %}
   {% if not macro %}
     {{ exceptions.raise_compiler_error("Did not find a `current_timestamp` macro.") }}
   {% endif %}
   {{ return(macro()) }}
-{% endmacro %}
+{%- endmacro %}
 
 
-{% macro current_timestamp_in_utc() %}
+{% macro current_timestamp_in_utc() -%}
   {% set macro = dbt.current_timestamp_in_utc_backcompat or dbt_utils.current_timestamp_in_utc %}
   {% if not macro %}
     {{ exceptions.raise_compiler_error("Did not find a `current_timestamp_in_utc` macro.") }}
   {% endif %}
   {{ return(macro()) }}
-{% endmacro %}
+{%- endmacro %}
