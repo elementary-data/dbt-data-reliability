9a1370a5f0c72464bb9f897e1168c6f9a6c7f132
Elon Gliksberg <elongliks@gmail.com>
2022-10-27 10:42:15 +0300

    Simplified the macro.

--- macros/utils/cross_db_utils/current_timestamp.sql
+++ macros/utils/cross_db_utils/current_timestamp.sql
@@ -1,15 +1,13 @@
-{% macro current_timestamp() -%}
-  {% if dbt.current_timestamp_backcompat %}
-    {{ return(dbt.current_timestamp_backcompat()) }}
-  {% elif dbt_utils.current_timestamp %}
-    {{ return(dbt_utils.current_timestamp()) }}
-  {% else %}
+{% macro current_timestamp() %}
+  {% set macro = dbt.current_timestamp_backcompat or dbt_utils.current_timestamp %}
+  {% if not macro %}
     {{ exceptions.raise_compiler_error("Did not find a current_timestamp macro.") }}
   {% endif %}
-{%- endmacro %}
+  {{ return(macro) }}
+{% endmacro %}
 
 
-{% macro current_timestamp_in_utc() -%}
+{% macro current_timestamp_in_utc() %}
   {% if dbt_version >= '1.2.0' %}
         {# This macro is depricated from dbt_utils version 0.9.0, but still hasn't got an equivalent macro at dbt-core #}
         {# Should be replaced to the equivalent macro once it released #}
@@ -17,4 +15,4 @@
     {% else %}
         {{ return(dbt_utils.current_timestamp_in_utc()) }}
     {% endif %}
-{%- endmacro %}
+{% endmacro %}
