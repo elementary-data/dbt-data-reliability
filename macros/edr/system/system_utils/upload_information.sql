b8f7c745f04f2e051f44ad9436190be52f98574f
Elon Gliksberg <elongliks@gmail.com>
2022-10-30 11:53:32 +0200

    Uploading the information table is only done from on-run-end.

--- macros/edr/system/system_utils/upload_information.sql
+++ macros/edr/system/system_utils/upload_information.sql
@@ -10,17 +10,10 @@
     {{ return('') }}
   {% endif %}
 
-  {% if results %}
-    {% if elementary.get_result_node(identifier) %}
-      {{ return('') }}
-    {% else %}
-      {% do dbt.truncate_relation(relation) %}
-    {% endif %}
-  {% endif %}
-
   {% set data = [
     {'key': 'dbt_version', 'value': dbt_version},
     {'key': 'elementary_version', 'value': elementary.get_elementary_package_version()},
   ] %}
+  {% do dbt.truncate_relation(relation) %}
   {% do elementary.insert_rows(relation, data) %}
 {% endmacro %}
