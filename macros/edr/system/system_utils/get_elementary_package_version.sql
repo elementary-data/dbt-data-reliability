e0ff7c8f0ea206ed06e6a563f89f821e6194df45
Elon Gliksberg <elongliks@gmail.com>
2022-10-30 11:49:10 +0200

    Updated a comment.

--- macros/edr/system/system_utils/get_elementary_package_version.sql
+++ macros/edr/system/system_utils/get_elementary_package_version.sql
@@ -1,5 +1,4 @@
-{# Only works if package is pulled from the registry. #}
-
+{# The macro only works if using `package` in `packages.yml`, rather than `git`, `local`, etc. #}
 {% macro get_elementary_package_version() %}
   {% for pkg in ref.config.packages.packages %}
     {% if pkg.package == 'elementary-data/elementary' %}
