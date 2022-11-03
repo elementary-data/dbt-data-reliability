ef2c585761e095024d48d814257a73a051c7f853
Elon Gliksberg <elongliks@gmail.com>
2022-11-03 14:06:27 +0200

    Renamed the identifier.

--- macros/edr/materializations/tests/test.sql
+++ macros/edr/materializations/tests/test.sql
@@ -1,6 +1,6 @@
 {%- materialization test, default -%}
     {% set ele_db, ele_schema = elementary.get_package_database_and_schema() %}
-    {% set relation = api.Relation.create(database=ele_db, schema=ele_schema, identifier=model.alias, type='table') -%} %}
+    {% set relation = api.Relation.create(database=ele_db, schema=ele_schema, identifier='test_result__{}'.format(model.alias), type='table') -%} %}
     {% do elementary.create_or_replace(false, relation, sql) %}
     {% do context.update({'sql': 'select * from {}'.format(relation)}) %}
     {{ return(dbt.materialization_test_default()) }}
