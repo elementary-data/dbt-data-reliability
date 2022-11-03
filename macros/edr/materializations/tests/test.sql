876c625047f96201b0c49cd8737df127511d6e64
Elon Gliksberg <elongliks@gmail.com>
2022-11-03 13:46:34 +0200

    create_or_replace already executes the query.

--- macros/edr/materializations/tests/test.sql
+++ macros/edr/materializations/tests/test.sql
@@ -1,7 +1,7 @@
 {%- materialization test, default -%}
     {% set ele_db, ele_schema = elementary.get_package_database_and_schema() %}
     {% set relation = api.Relation.create(database=ele_db, schema=ele_schema, identifier=model.alias, type='table') -%} %}
-    {% do dbt.run_query(elementary.create_or_replace(false, relation, sql)) %}
+    {% do elementary.create_or_replace(false, relation, sql) %}
     {% do context.update({'sql': 'select * from {}'.format(relation)}) %}
     {{ return(dbt.materialization_test_default()) }}
 {%- endmaterialization -%}
