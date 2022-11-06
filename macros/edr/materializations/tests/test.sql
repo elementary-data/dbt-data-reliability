ff5f8f6b5caebec19c6e932b0bad8cbe344791aa
Elon Gliksberg <elongliks@gmail.com>
2022-11-06 13:26:21 +0200

    Query is 'sql'.

--- macros/edr/materializations/tests/test.sql
+++ macros/edr/materializations/tests/test.sql
@@ -1,5 +1,5 @@
 {% materialization test, default %}
-    {% set test_result = dbt.run_query(get_test_result_sql) %}
+    {% set test_result = dbt.run_query(sql) %}
     {% do graph["elementary"]["test_results"].update({model.unique_id: test_result}) %}
     {{ return(dbt.materialization_test_default()) }}
 {% endmaterialization %}
