44420b1132439d13ea68fbcd80ddc6e4edf7b3e4
Elon Gliksberg <elongliks@gmail.com>
2022-11-06 19:04:52 +0200

    Overriding Snowflake's test materialization.

--- macros/edr/materializations/tests/test.sql
+++ macros/edr/materializations/tests/test.sql
@@ -1,8 +1,17 @@
-{% materialization test, default %}
+{% macro store_test_sample() %}
     {% set test_sample_sql %}
         select * from ({{ sql }}) limit {{ elementary.get_config_var('test_sample_row_count') }}
     {% endset %}
     {% set test_sample = dbt.run_query(test_sample_sql) %}
     {% do graph["elementary"]["test_samples"].update({model.unique_id: test_sample}) %}
+{% endmacro %}
+
+{% materialization test, default %}
+    {% do elementary.store_test_sample() %}
     {{ return(dbt.materialization_test_default()) }}
+{% endmaterialization %}
+
+{% materialization test, adapter='snowflake' %}
+    {% do elementary.store_test_sample() %}
+    {{ return(dbt.materialization_test_snowflake()) }}
 {% endmaterialization %}
