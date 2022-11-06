a8386fad0090fcaaf64c620bc867df7afa6190ee
Elon Gliksberg <elongliks@gmail.com>
2022-11-07 00:09:10 +0200

    Limiting also on query.

--- macros/edr/materializations/tests/test.sql
+++ macros/edr/materializations/tests/test.sql
@@ -1,6 +1,9 @@
 {% macro store_test_sample() %}
     {% set test_sample_sql %}
         select * from ({{ sql }})
+        {% if not model.package_name == 'elementary' %}
+          limit {{ elementary.get_config_var('test_sample_row_count') }}
+        {% endif %}
     {% endset %}
     {% set test_sample = dbt.run_query(test_sample_sql) %}
     {% do graph["elementary"]["test_samples"].update({model.unique_id: test_sample}) %}
