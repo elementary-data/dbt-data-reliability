65248d5995ec128ff1972df13fe3f614566a2bc6
Elon Gliksberg <elongliks@gmail.com>
2022-11-07 12:43:42 +0200

    Increasing long string size.

--- macros/edr/system/system_utils/get_config_var.sql
+++ macros/edr/system/system_utils/get_config_var.sql
@@ -31,7 +31,7 @@
       'column_numeric': ['min', 'max', 'zero_count', 'zero_percent', 'average', 'standard_deviation', 'variance']
     },
     'time_format': '%Y-%m-%d %H:%M:%S',
-    'long_string_size': 16384,
+    'long_string_size': 65535,
     'collect_model_sql': true,
     'model_sql_max_size': 10240,
     'query_max_size': 1000000,
