dd4c201f85aeef7ad3a8438816fec6678adf15c7
kkprab <97446187+kkprab@users.noreply.github.com>
2022-11-07 16:54:36 +0000

    Added query_id

--- macros/edr/dbt_artifacts/upload_run_results.sql
+++ macros/edr/dbt_artifacts/upload_run_results.sql
@@ -53,7 +53,12 @@
         'full_refresh': flags.FULL_REFRESH,
         'compiled_code': elementary.get_compiled_model_code_text(node),
         'failures': run_result_dict.get('failures'),
-        'query_id': run_result_dict.get('adapter_response', {}).get('query_id')
+        {% if run_result_dict.get('adapter_response', {}).get('query_id') == [] %}
+          'query_id': none
+        {% else %}  
+          'query_id': run_result_dict.get('adapter_response', {}).get('query_id')
+        {% endif %}  
+        
     }%}
 
     {% set timings = elementary.safe_get_with_default(run_result_dict, 'timing', []) %}
