1285b5a2d8218c4bf0657c348a888940238adf79
Itamar Hartstein <haritamar@gmail.com>
2022-11-09 11:09:07 +0200

    get / create elementary test table - add if execute

--- macros/edr/tests/test_utils/create_elementary_test_table.sql
+++ macros/edr/tests/test_utils/create_elementary_test_table.sql
@@ -1,18 +1,21 @@
 {% macro create_elementary_test_table(database_name, schema_name, test_name, table_type, sql_query) %}
-    {% set temp_table_name = elementary.table_name_with_suffix(test_name, "__" ~ table_type) %}
-    {{ elementary.debug_log(table_type ~ ' table: ' ~ database_name ~ '.' ~ schema_name ~ '.' ~ temp_table_name) }}
+    {% if execute %}
+        {% set temp_table_name = elementary.table_name_with_suffix(test_name, "__" ~ table_type) %}
+        {{ elementary.debug_log(table_type ~ ' table: ' ~ database_name ~ '.' ~ schema_name ~ '.' ~ temp_table_name) }}
 
-    {% set _, temp_table_relation = dbt.get_or_create_relation(database=database_name,
-                                                                               schema=schema_name,
-                                                                               identifier=temp_table_name,
-                                                                               type='table') -%}
+        {% set _, temp_table_relation = dbt.get_or_create_relation(database=database_name,
+                                                                                   schema=schema_name,
+                                                                                   identifier=temp_table_name,
+                                                                                   type='table') -%}
 
-    {# Save the test table to the graph for easy access later #}
-    {% do graph.setdefault("elementary_test_tables", {}) %}
-    {% do graph["elementary_test_tables"].update({(test_name, table_type): temp_table_relation}) %}
+        {# Save the test table to the graph for easy access later #}
+        {% do graph.setdefault("elementary_test_tables", {}) %}
+        {% do graph["elementary_test_tables"].update({(test_name, table_type): temp_table_relation}) %}
 
-    {# Create the table if it doesn't exist #}
-    {%- do elementary.create_or_replace(False, temp_table_relation, sql_query) %}
+        {# Create the table if it doesn't exist #}
+        {%- do elementary.create_or_replace(False, temp_table_relation, sql_query) %}
 
-    {{ return(temp_table_relation) }}
+        {{ return(temp_table_relation) }}
+    {% endif %}
+    {{ return(none) }}
 {% endmacro %}
--- macros/edr/tests/test_utils/get_elementary_test_table.sql
+++ macros/edr/tests/test_utils/get_elementary_test_table.sql
@@ -1,3 +1,6 @@
 {% macro get_elementary_test_table(test_name, table_type) %}
-    {{ return(graph["elementary_test_tables"].get((test_name, table_type))) }}
+    {% if execute %}
+        {{ return(graph["elementary_test_tables"].get((test_name, table_type))) }}
+    {% endif %}
+    {{ return(none) }}
 {% endmacro %}
