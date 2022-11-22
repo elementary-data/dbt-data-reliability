9ebf30f07a56831371112a7aeb20e95d78374712
Elon Gliksberg <elongliks@gmail.com>
2022-11-22 12:57:54 +0200

    Using 'get_runtime_config'.

--- macros/edr/system/system_utils/get_target_path.sql
+++ macros/edr/system/system_utils/get_target_path.sql
@@ -1,5 +1,6 @@
 {% macro get_target_path(identifier=none) %}
-  {% set elementary_target_path = flags.Path(ref.config.target_path) / 'elementary' %}
+  {% set runtime_conf = elementary.get_runtime_config() %}
+  {% set elementary_target_path = flags.Path(runtime_conf.target_path) / 'elementary' %}
   {% if identifier %}
     {{ return(elementary_target_path / identifier) }}
   {% else %}
