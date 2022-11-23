47f29c296c4f3a0cb215b07571e67be3817e3c22
Elon Gliksberg <elongliks@gmail.com>
2022-11-23 17:48:22 +0200

    Added result_rows to the alerts models.

--- models/edr/alerts/alerts_schema_changes.sql
+++ models/edr/alerts/alerts_schema_changes.sql
@@ -32,7 +32,8 @@ alerts_schema_changes as (
            test_name,
            test_params,
            severity,
-           status
+           status,
+           result_rows
         from elementary_test_results
         where {{ not elementary.get_config_var('disable_test_alerts') }} and lower(status) != 'pass' {%- if elementary.get_config_var('disable_warn_alerts') -%} and lower(status) != 'warn' {%- endif -%} {%- if elementary.get_config_var('disable_skipped_test_alerts') -%} and lower(status) != 'skipped' {%- endif -%} and test_type = 'schema_change'
 )
