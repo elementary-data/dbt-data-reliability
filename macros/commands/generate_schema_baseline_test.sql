8a05e5de86b695006352e5cec45896e7c65babef
Itamar Hartstein <haritamar@gmail.com>
2022-12-04 13:39:20 +0200

    add macro for generating a schema changes from baseline test on either a specific source or a model, or all nodes

--- macros/commands/generate_schema_baseline_test.sql
+++ macros/commands/generate_schema_baseline_test.sql
@@ -1,8 +1,33 @@
-{% macro generate_schema_baseline_test(model_name) %}
-  {% set node = get_node_by_name(model_name) %}
-  {% if not node %}
-    {% do print("Could not find any model or source by the name '" ~ model_name ~ "'!") %}
-    {% do return(none) %}
+{% macro generate_schema_baseline_test(name=none, include_sources=True, include_models=True) %}
+  {% if name %}
+    {{ generate_schema_baseline_test_for_node(name) }}
+  {% else %}
+    {{ generate_schema_baseline_test_for_all_nodes(include_sources=include_sources, include_models=include_models) }}
+  {% endif %}
+{% endmacro %}
+
+{% macro generate_schema_baseline_test_for_all_nodes(include_sources=True, include_models=True) %}
+  {% set nodes = get_nodes_from_graph() %}
+  {% for node in nodes %}
+    {% if node.package_name != 'elementary' and
+          ((include_sources and node.resource_type == 'source') or
+           (include_models and node.resource_type == 'model')) %}
+      {% do print("Generating schema changes from baseline test for " ~ node.resource_type ~ " '" ~ node.name ~ "'..") %}
+      {{ generate_schema_baseline_test_for_node(node) }}
+      {% do print('----------------------------------') %}
+    {% endif %}
+  {% endfor %}
+{% endmacro %}
+
+{% macro generate_schema_baseline_test_for_node(node) %}
+  {% if node is string %}
+    {% set node_name = node %}
+    {% set node = get_node_by_name(node_name) %}
+
+    {% if not node %}
+      {% do print("Could not find any model or source by the name '" ~ model_name ~ "'!") %}
+      {% do return(none) %}
+    {% endif %}
   {% endif %}
 
   {% if node.resource_type not in ["source", "model"] %}
@@ -10,7 +35,7 @@
     {% do return(none) %}
   {% endif %}
 
-  {% set node_relation = adapter.get_relation(database=node.database, schema=node.schema, identifier=node.identifier) %}
+  {% set node_relation = get_relation_from_node(node) %}
   {% if not node_relation %}
     {% do print("Table not found in the DB! Cannot create schema test.") %}
     {% do return(none) %}
