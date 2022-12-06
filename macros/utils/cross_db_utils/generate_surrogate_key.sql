6ae3a5dc55adc4faf58e29dca50166ece2fd703c
pei <peeeei0804@gmail.com>
2022-12-06 20:09:07 +0900

    add generate surrogate key macro

---
+++ macros/utils/cross_db_utils/generate_surrogate_key.sql
@@ -0,0 +1,8 @@
+{% macro generate_surrogate_key(field_list) -%}
+  {% if dbt_version >= '1.3.0' %}
+        {{ return(dbt.generate_surrogate_key(field_list)) }}
+    {% else %}
+        {# This macro is abolition from dbt_utils version 1.0.0 #}
+        {{ return(dbt_utils.surrogate_key(field_list)) }}
+    {% endif %}
+{%- endmacro %}
