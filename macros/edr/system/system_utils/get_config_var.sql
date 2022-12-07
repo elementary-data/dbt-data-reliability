ce1de4ba4486692bf46d620d3ee5ac09c9d96f53
Elon Gliksberg <elongliks@gmail.com>
2022-12-07 13:00:14 +0200

    training_set_size is decoupled from days_back.

--- macros/edr/system/system_utils/get_config_var.sql
+++ macros/edr/system/system_utils/get_config_var.sql
@@ -22,6 +22,7 @@
     'days_back': 14,
     'anomaly_sensitivity': 3,
     'backfill_days': 2,
+    'training_set_size': 14,
     'tests_schema_name': '',
     'debug_logs': false,
     'disable_warn_alerts': false,
