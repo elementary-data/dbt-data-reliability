e1de682ced7a5c980b8361a2669b10f52f724d21
Elon Gliksberg <elongliks@gmail.com>
2022-12-12 13:29:00 +0200

    If there's only one parent model, using it.

--- macros/edr/dbt_artifacts/upload_dbt_tests.sql
+++ macros/edr/dbt_artifacts/upload_dbt_tests.sql
@@ -88,20 +88,25 @@
     {% set primary_test_model_database, primary_test_model_schema = elementary.get_model_database_and_schema_from_test_node(node_dict) %}
 
     {% set primary_test_model_id = namespace(data=none) %}
-    {% set test_model_jinja = test_kwargs.get('model') %}
-    {% if test_model_jinja %}
-      {% set test_model_name_matches = modules.re.findall("ref\(['\"](\w+)['\"]\)", test_model_jinja) %}
-      {% if test_model_name_matches | length == 1 %}
-        {% set test_model_name = test_model_name_matches[0] %}
-        {% for test_model_unique_id in test_model_unique_ids %}
-            {% set split_test_model_unique_id = test_model_unique_id.split('.') %}
-            {% if split_test_model_unique_id and split_test_model_unique_id | length > 0 %}
-                {% set test_node_model_name = split_test_model_unique_id[-1] %}
-                {% if test_node_model_name == test_model_name %}
-                  {% set primary_test_model_id.data = test_model_unique_id %}
-                {% endif %}
-            {% endif %}
-        {% endfor %}
+    {% if test_model_unique_ids | length == 1 %}
+        {# if only one parent model for this test, simply use this model #}
+        {% set primary_test_model_id.data = test_model_unique_ids[0] %}
+    {% else %}
+      {% set test_model_jinja = test_kwargs.get('model') %}
+      {% if test_model_jinja %}
+        {% set test_model_name_matches = modules.re.findall("ref\(['\"](\w+)['\"]\)", test_model_jinja) %}
+        {% if test_model_name_matches | length == 1 %}
+          {% set test_model_name = test_model_name_matches[0] %}
+          {% for test_model_unique_id in test_model_unique_ids %}
+              {% set split_test_model_unique_id = test_model_unique_id.split('.') %}
+              {% if split_test_model_unique_id and split_test_model_unique_id | length > 0 %}
+                  {% set test_node_model_name = split_test_model_unique_id[-1] %}
+                  {% if test_node_model_name == test_model_name %}
+                    {% set primary_test_model_id.data = test_model_unique_id %}
+                  {% endif %}
+              {% endif %}
+          {% endfor %}
+        {% endif %}
       {% endif %}
     {% endif %}
     {% set original_file_path = node_dict.get('original_file_path') %}
