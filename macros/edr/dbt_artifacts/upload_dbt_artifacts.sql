322392977602ce02bcf6123187ff9e380613d9f9
Elon Gliksberg <elongliks@gmail.com>
2022-12-12 15:24:44 +0200

    Revert "Uploading artifacts according to the command that was ran."

--- macros/edr/dbt_artifacts/upload_dbt_artifacts.sql
+++ macros/edr/dbt_artifacts/upload_dbt_artifacts.sql
@@ -1,23 +1,23 @@
 {% macro upload_dbt_artifacts() %}
   {% if execute and results %}
-    {% set artifacts = [
-      {"model": "dbt_models", "which": "run", "handler": elementary.upload_dbt_models},
-      {"model": "dbt_tests", "which": "test", "handler": elementary.upload_dbt_tests},
-      {"model": "dbt_snapshots", "which": "snapshot", "handler": elementary.upload_dbt_snapshots},
-      {"model": "dbt_sources", "which": none, "handler": elementary.upload_dbt_sources},
-      {"model": "dbt_metrics", "which": none, "handler": elementary.upload_dbt_metrics},
-      {"model": "dbt_exposures", "which": none, "handler": elementary.upload_dbt_exposures},
-    ] %}
+    {% set model_upload_func_map = {
+      "dbt_models": elementary.upload_dbt_models,
+      "dbt_tests": elementary.upload_dbt_tests,
+      "dbt_sources": elementary.upload_dbt_sources,
+      "dbt_snapshots": elementary.upload_dbt_snapshots,
+      "dbt_metrics": elementary.upload_dbt_metrics,
+      "dbt_exposures": elementary.upload_dbt_exposures,
+      }
+    %}
     {% do elementary.debug_log("Uploading dbt artifacts.") %}
-    {% for artifact in artifacts %}
-      {% if flags.WHICH == "build" or flags.WHICH == artifact.which %}
-        {% if not elementary.get_result_node(artifact.model) %}
-          {% if elementary.get_elementary_relation(artifact.model) %}
-            {% do artifact.handler(should_commit=true, cache=elementary.get_config_var('cache_artifacts')) %}
-          {% endif %}
-        {% else %}
-          {% do elementary.debug_log('[{}] Artifacts already ran.'.format(artifact.model)) %}
+    {% for artifacts_model, upload_artifacts_func in model_upload_func_map.items() %}
+      {% if not elementary.get_result_node(artifacts_model) %}
+        {% set relation = elementary.get_elementary_relation(artifacts_model) %}
+        {% if relation %}
+          {% do upload_artifacts_func(should_commit=true, cache=elementary.get_config_var('cache_artifacts')) %}
         {% endif %}
+      {% else %}
+        {% do elementary.debug_log('[{}] Artifacts already ran.'.format(artifacts_model)) %}
       {% endif %}
     {% endfor %}
     {% do elementary.debug_log("Uploaded dbt artifacts successfully.") %}
