01f847aa87f56ece7f0d2a4ec8f5744a5b5abf21
Elon Gliksberg <elongliks@gmail.com>
2022-12-12 15:26:05 +0200

    Removed auto upload of less important artifacts.

--- macros/edr/dbt_artifacts/upload_dbt_artifacts.sql
+++ macros/edr/dbt_artifacts/upload_dbt_artifacts.sql
@@ -4,16 +4,12 @@
       "dbt_models": elementary.upload_dbt_models,
       "dbt_tests": elementary.upload_dbt_tests,
       "dbt_sources": elementary.upload_dbt_sources,
-      "dbt_snapshots": elementary.upload_dbt_snapshots,
-      "dbt_metrics": elementary.upload_dbt_metrics,
-      "dbt_exposures": elementary.upload_dbt_exposures,
       }
     %}
     {% do elementary.debug_log("Uploading dbt artifacts.") %}
     {% for artifacts_model, upload_artifacts_func in model_upload_func_map.items() %}
       {% if not elementary.get_result_node(artifacts_model) %}
-        {% set relation = elementary.get_elementary_relation(artifacts_model) %}
-        {% if relation %}
+        {% if elementary.get_elementary_relation(artifacts_model) %}
           {% do upload_artifacts_func(should_commit=true, cache=elementary.get_config_var('cache_artifacts')) %}
         {% endif %}
       {% else %}
