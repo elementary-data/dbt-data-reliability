ccf7c4ca95e24e68b0e621226e941a2fbb025ceb
Itamar Hartstein <haritamar@gmail.com>
2022-12-19 22:51:23 +0200

    table_monitoring_query: refactored a bit - so every metric is implemented in its own function

--- macros/edr/data_monitoring/monitors_query/table_monitoring_query.sql
+++ macros/edr/data_monitoring/monitors_query/table_monitoring_query.sql
@@ -2,6 +2,10 @@
 
     {% set full_table_name_str = elementary.quote(elementary.relation_to_full_name(monitored_table_relation)) %}
 
+    {% set metric_args = {
+        "freshness_column": freshness_column
+    } %}
+
     with monitored_table as (
         select * from {{ monitored_table_relation }}
         {% if where_expression %}
@@ -24,61 +28,10 @@
                 and {{ elementary.cast_as_timestamp(timestamp_column) }} < (select max(edr_bucket_end) from buckets)
         ),
 
-        {%- if 'row_count' in table_monitors %}
-
-        row_count_values as (
-            select edr_bucket_start,
-                   edr_bucket_end,
-                   start_bucket_in_data,
-                   case when start_bucket_in_data is null then
-                       0
-                   else {{ elementary.cast_as_float(elementary.row_count()) }} end as row_count_value
-            from buckets left join time_filtered_monitored_table on (edr_bucket_start = start_bucket_in_data)
-            group by 1,2,3
-        ),
-
-        row_count as (
-            select edr_bucket_start,
-                   edr_bucket_end,
-                   {{ elementary.const_as_string('row_count') }} as metric_name,
-                   {{ elementary.null_string() }} as source_value,
-                   row_count_value as metric_value
-            from row_count_values
-        ),
-
-        {%- else %}
-
-        row_count as (
-            {{ elementary.empty_table([('edr_bucket_start','timestamp'),('edr_bucket_end','timestamp'),('metric_name','string'),('source_value','string'),('metric_value','int')]) }}
-        ),
-
-        {%- endif %}
-
-        table_freshness as (
-        {%- if 'freshness' in table_monitors %}
-            {%- if freshness_column is undefined or freshness_column is none %}
-                {%- set freshness_column = timestamp_column %}
-            {%- endif %}
-            select
-                edr_bucket_start,
-                edr_bucket_end,
-                {{ elementary.const_as_string('freshness') }} as metric_name,
-                {{ elementary.cast_as_string('max('~freshness_column~')') }} as source_value,
-                {{ elementary.timediff('second', elementary.cast_as_timestamp('max('~freshness_column~')'), "edr_bucket_end") }} as metric_value
-            from buckets, monitored_table
-            where {{ elementary.cast_as_timestamp(timestamp_column) }} < edr_bucket_end
-            group by 1,2,3
-        {%- else %}
-            {{ elementary.empty_table([('edr_bucket_start','timestamp'),('edr_bucket_end','timestamp'),('metric_name','string'),('source_value','string'),('metric_value','int')]) }}
-        {%- endif %}
-        ),
-
-        union_metrics as (
-
-        select * from row_count
-        union all
-        select * from table_freshness
-
+        metrics as (
+            {{ elementary.get_unified_metrics_query(metrics=table_monitors,
+                                                    metric_args=metric_args,
+                                                    timestamp_column=timestamp_column) }}
         ),
 
         metrics_final as (
@@ -95,21 +48,13 @@
             {{ elementary.null_string() }} as dimension,
             {{ elementary.null_string() }} as dimension_value
         from
-            union_metrics
+            metrics
         where (metric_value is not null and cast(metric_value as {{ elementary.type_int() }}) < {{ elementary.get_config_var('max_int') }}) or
             metric_value is null
         )
     {% else %}
-        row_count as (
-            {%- if 'row_count' in table_monitors %}
-                select
-                    {{ elementary.const_as_string('row_count') }} as metric_name,
-                    {{ elementary.row_count() }} as metric_value
-                from monitored_table
-                group by 1
-            {%- else %}
-                {{ elementary.empty_table([('metric_name','string'),('metric_value','int')]) }}
-            {%- endif %}
+        metrics as (
+            {{ elementary.get_unified_metrics_query(metrics=table_monitors, metric_args=metric_args) }}
         ),
 
         metrics_final as (
@@ -125,7 +70,7 @@
             {{ elementary.null_int() }} as bucket_duration_hours,
             {{ elementary.null_string() }} as dimension,
             {{ elementary.null_string() }} as dimension_value
-        from row_count
+        from metrics
 
         )
     {% endif %}
@@ -150,4 +95,103 @@
         dimension_value
     from metrics_final
 
+{% endmacro %}
+
+{% macro get_unified_metrics_query(metrics, metric_args, timestamp_column=none) %}
+    {%- set included_monitors = {} %}
+    {%- for metric_name in metrics %}
+        {%- set metric_query = elementary.get_metric_query(metric_name, metric_args, timestamp_column=timestamp_column) %}
+        {%- if metric_query %}
+            {% do included_monitors.update({metric_name: metric_query}) %}
+        {%- endif %}
+    {%- endfor %}
+
+    {% if not included_monitors %}
+        {% if timestamp_column %}
+            {{ do return(elementary.empty_table([('edr_bucket_start','timestamp'),('edr_bucket_end','timestamp'),('metric_name','string'),('source_value','string'),('metric_value','int')])) }}
+        {% else %}
+            {% do return(elementary.empty_table([('metric_name','string'),('metric_value','int')])) %}
+        {% endif %}
+    {% endif %}
+
+    with
+    {%- for metric_name, metric_query in included_monitors.items() %}
+        {{ metric_name }} as (
+            {{ metric_query }}
+        ){% if not loop.last %},{% endif %}
+    {%- endfor %}
+
+    {%- for metric_name in included_monitors %}
+    select * from {{ metric_name }}
+    {% if not loop.last %} union all {% endif %}
+    {%- endfor %}
+{% endmacro %}
+
+{% macro get_metric_query(metric_name, metric_args, timestamp_column) %}
+    {%- set metrics_macro_mapping = {
+        "row_count": elementary.row_count_metric_query,
+        "freshness": elementary.freshness_metric_query
+    } %}
+
+    {%- set metric_macro = metrics_macro_mapping.get(metric_name) %}
+    {%- if not metric_macro %}
+        {%- do return(none) %}
+    {%- endif %}
+
+    {%- set metric_query = metric_macro(metric_args, timestamp_column=timestamp_column) %}
+    {%- if not metric_query %}
+        {%- do return(none) %}
+    {%- endif %}
+
+    {{ metric_query }}
+{% endmacro %}
+
+{% macro row_count_metric_query(metric_args, timestamp_column=none) %}
+{% if timestamp_column %}
+    row_count_values as (
+        select edr_bucket_start,
+               edr_bucket_end,
+               start_bucket_in_data,
+               case when start_bucket_in_data is null then
+                   0
+               else {{ elementary.cast_as_float(elementary.row_count()) }} end as row_count_value
+        from buckets left join filtered_monitored_table on (edr_bucket_start = start_bucket_in_data)
+        group by 1,2,3
+    ),
+
+    select edr_bucket_start,
+           edr_bucket_end,
+           {{ elementary.const_as_string('row_count') }} as metric_name,
+           {{ elementary.null_string() }} as source_value,
+           row_count_value as metric_value
+    from row_count_values
+{% else %}
+    select
+        {{ elementary.const_as_string('row_count') }} as metric_name,
+        {{ elementary.row_count() }} as metric_value
+    from monitored_table
+    group by 1
+{% endif %}
+{% endmacro %}
+
+{% macro freshness_metric_query(metric_args, timestamp_column=none) %}
+{% if timestamp_column %}
+    {%- set freshness_column = metric_args.freshness_column %}
+    {%- if freshness_column is undefined or freshness_column is none %}
+        {%- set freshness_column = timestamp_column %}
+    {%- endif %}
+
+    select
+        edr_bucket_start,
+        edr_bucket_end,
+        {{ elementary.const_as_string('freshness') }} as metric_name,
+        {{ elementary.cast_as_string('max('~freshness_column~')') }} as source_value,
+        {{ elementary.timediff('second', elementary.cast_as_timestamp('max('~freshness_column~')'), "edr_bucket_end") }} as metric_value
+    from buckets, monitored_table
+    where {{ elementary.cast_as_timestamp(timestamp_column) }} < edr_bucket_end
+    group by 1,2,3
+{% else %}
+    {# Freshness test not supported when timestamp column is not provided #}
+    {% do return(none) %}
+{% endif %}
 {% endmacro %}
