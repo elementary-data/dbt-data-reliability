fd7d1d6c97077dbf46a93949d9444ad4339cc857
Elon Gliksberg <elongliks@gmail.com>
2022-12-19 11:27:22 +0200

    All test data point to the same schema.

--- integration_tests/macros/system/generate_schema_name.sql
+++ integration_tests/macros/system/generate_schema_name.sql
@@ -1,8 +1,12 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
-    {%- else -%}
-        {{ custom_schema_name }}
-    {%- endif -%}
+    {% if not custom_schema_name %}
+      {% do return(default_schema) %}
+    {% endif %}
+
+    {% if node.resource_type == "seed" %}
+      {% do return(custom_schema_name) %}
+    {% endif %}
+
+    {% do return("{}_{}".format(default_schema, custom_schema_name)) %}
 {%- endmacro %}
