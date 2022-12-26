4bb435e75366455a0c8882794407d2de55a69161
Itamar Hartstein <haritamar@gmail.com>
2022-12-26 17:40:32 +0200

    test_monitoring_anomalies: WIP - add initial implementation for new freshness test + make freshness_anomalies reference that test

--- macros/edr/tests/test_freshness_anomalies.sql
+++ macros/edr/tests/test_freshness_anomalies.sql
@@ -1,7 +1,7 @@
-{% test freshness_anomalies(model, timestamp_column, sensitivity, backfill_days, where_expression, time_bucket) %}
+{% test freshness_anomalies(model, data_timestamp_column, insertion_timestamp_column, sensitivity, backfill_days, where_expression, time_bucket) %}
   {{ elementary.test_table_anomalies(
       model=model,
-      table_anomalies=["freshness"],
+      table_anomalies=["freshness_v2"],
       freshness_column=none,
       timestamp_column=timestamp_column,
       sensitivity=sensitivity,
