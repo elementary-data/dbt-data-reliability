ad45e4b330eddc6298cac31de00354f469554e68
Elon Gliksberg <elongliks@gmail.com>
2022-12-28 14:55:06 +0200

    Exported 'max_bucket_end' to its own expression.

--- macros/edr/system/system_utils/buckets_cte.sql
+++ macros/edr/system/system_utils/buckets_cte.sql
@@ -9,7 +9,7 @@
           edr_bucket_start,
           {{ edr_bucket_end_expr }} as edr_bucket_end
         from (select explode(sequence({{ elementary.cast_as_timestamp(elementary.quote(elementary.get_min_bucket_start())) }}, {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}, interval {{ time_bucket.count }} {{ time_bucket.period }})) as edr_bucket_start)
-        where {{ edr_bucket_end }} < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
+        where {{ edr_bucket_end_expr }} < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
     {%- endset %}
     {{ return(complete_buckets_cte) }}
 {% endmacro %}
@@ -26,9 +26,9 @@
         )
         select
           edr_bucket_start,
-          {{ edr_bucket_end }} as edr_bucket_end
+          {{ edr_bucket_end_expr }} as edr_bucket_end
         from timestamps
-        where {{ edr_bucket_end }} < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
+        where {{ edr_bucket_end_expr }} < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
     {%- endset %}
     {{ return(complete_buckets_cte) }}
 {% endmacro %}
@@ -41,7 +41,7 @@
           edr_bucket_start,
           {{ edr_bucket_end_expr }} as edr_bucket_end
         from unnest(generate_timestamp_array({{ elementary.cast_as_timestamp(elementary.quote(elementary.get_min_bucket_start())) }}, {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}, interval {{ time_bucket.count }} {{ time_bucket.period }})) as edr_bucket_start
-        where {{ edr_bucket_end }} < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
+        where {{ edr_bucket_end_expr }} < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
     {%- endset %}
     {{ return(complete_buckets_cte) }}
 {% endmacro %}
@@ -57,7 +57,7 @@
         {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_min_bucket_start())) }} + (num * interval '{{ time_bucket.count }} {{ time_bucket.period }}') as edr_bucket_start,
         {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_min_bucket_start())) }} + ((num + 1) * interval '{{ time_bucket.count }} {{ time_bucket.period }}') as edr_bucket_end
       from integers
-      where edr_bucket_end < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
+      where edr_bucket_end_expr < {{ elementary.cast_as_timestamp(elementary.quote(elementary.get_max_bucket_end())) }}
     {%- endset %}
     {{ return(complete_buckets_cte) }}
 {% endmacro %}
