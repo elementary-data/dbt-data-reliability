6afb4dfacf7170fd8317a301af98157ffed4a11c
IDoneShaveIt <48473443+IDoneShaveIt@users.noreply.github.com>
2022-12-28 11:51:48 +0200

    fix compiling error with overridden dbt macros (#205)

--- macros/utils/cross_db_utils/dateadd.sql
+++ macros/utils/cross_db_utils/dateadd.sql
@@ -1,4 +1,7 @@
 {% macro dateadd(datepart, interval, from_date_or_timestamp) %}
+    {% if not execute %}
+        {% do return(none) %}
+    {% endif %}
     {% set macro = dbt.dateadd or dbt_utils.dateadd %}
     {% if not macro %}
         {{ exceptions.raise_compiler_error("Did not find a `dateadd` macro.") }}
