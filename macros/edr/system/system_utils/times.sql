1545d9ea4f8a2339a4fc600a3371c2c5fadce9f9
Elon Gliksberg <elongliks@gmail.com>
2023-01-01 14:59:44 +0200

    now() also uses custom_run_started_at.

--- macros/edr/system/system_utils/times.sql
+++ macros/edr/system/system_utils/times.sql
@@ -1,3 +1,7 @@
+{% macro get_utc_tzinfo() %}
+  {% do return(run_started_at.tzinfo) %}
+{% endmacro %}
+
 {% macro get_time_format() %}
   {% do return("%Y-%m-%d %H:%M:%S") %}
 {% endmacro %}
@@ -7,5 +11,8 @@
 {% endmacro %}
 
 {% macro datetime_now_utc_as_string() %}
-    {% do return(modules.datetime.datetime.utcnow().strftime(elementary.get_time_format())) %}
+    {% set utcnow = modules.datetime.datetime.utcnow().replace(tzinfo=elementary.get_utc_tzinfo()) %}
+    {% set custom_run_started_at = elementary.get_run_started_at() %}
+    {% set custom_now = custom_run_started_at + (utcnow - run_started_at) %}
+    {% do return(custom_now.strftime(elementary.get_time_format())) %}
 {% endmacro %}
