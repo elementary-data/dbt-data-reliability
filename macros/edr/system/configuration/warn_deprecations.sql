2b668aa974a685ccba17bb5c9947cc5eb9710fe3
Elon Gliksberg <elongliks@gmail.com>
2023-01-02 16:19:52 +0200

    Updated deprecation message.

--- macros/edr/system/configuration/warn_deprecations.sql
+++ macros/edr/system/configuration/warn_deprecations.sql
@@ -5,10 +5,10 @@
   {% endif %}
 
   {% set msg %}
-In the next version, project-related artifacts (dbt_models, dbt_tests, dbt_sources, etc.) will not be automatically uploaded in the on-run-end.
-If you would like to keep this behavior, please add `disable_dbt_artifacts_autoupload: false` to your vars.
-Run-related artifacts (dbt_run_results, dbt_invocations, elementary_test_results, etc.) will keep working as usual.
-You can remove this warning by adding `disable_dbt_artifacts_autoupload: true` to your vars.
+To improve on-run-end hook's performance, from next version Elementary's CLI (edr) will automatically upload project-related artifacts (dbt_models, dbt_tests, dbt_sources, etc.) instead of uploading during the on-run-end hook.
+If you wish to keep it running in your on-run-end hook, add `disable_dbt_artifacts_autoupload: false` to your vars.
+Otherwise, you can mute this warning by adding `disable_dbt_artifacts_autoupload: true` to your vars.
+The on-run-end hook will still upload run-related artifacts (dbt_run_results, dbt_invocations, elementary_test_results, etc.).
   {% endset %}
   {% do exceptions.warn(msg) %}
 {% endmacro %}
