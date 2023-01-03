7f91d41fcfd22db530573f6c42022bf02bf1db01
Itamar Hartstein <haritamar@gmail.com>
2023-01-03 17:01:42 +0200

    test_freshness_anomalies.sql: return freshness_column=none

--- macros/edr/tests/test_freshness_anomalies.sql
+++ macros/edr/tests/test_freshness_anomalies.sql
@@ -2,6 +2,7 @@
   {{ elementary.test_table_anomalies(
       model=model,
       table_anomalies=["freshness"],
+      freshness_column=none,
       timestamp_column=timestamp_column,
       sensitivity=sensitivity,
       backfill_days=backfill_days,
