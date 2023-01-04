d1ce07fb86107c3759e8cab89d826f20a2e11fb3
Itamar Hartstein <haritamar@gmail.com>
2023-01-04 19:03:12 +0200

    get_start_bucket_in_data: use timeadd instead of dateadd

--- macros/edr/data_monitoring/monitors_query/get_start_bucket_in_data.sql
+++ macros/edr/data_monitoring/monitors_query/get_start_bucket_in_data.sql
@@ -2,5 +2,5 @@
     {% set bucket_start_datediff_expr %}
       floor({{ elementary.datediff(min_bucket_start, elementary.cast_as_timestamp(timestamp_column), time_bucket.period) }} / {{ time_bucket.count }}) * {{ time_bucket.count }}
     {% endset %}
-    {% do return(elementary.cast_as_timestamp(elementary.dateadd(time_bucket.period, elementary.cast_as_int(bucket_start_datediff_expr), min_bucket_start))) %}
+    {% do return(elementary.cast_as_timestamp(elementary.timeadd(time_bucket.period, elementary.cast_as_int(bucket_start_datediff_expr), min_bucket_start))) %}
 {% endmacro %}
