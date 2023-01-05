e6e25177290477dc2aa1f0ec9c2285fd8b3b4385
Elon Gliksberg <elongliks@gmail.com>
2023-01-05 20:06:11 +0200

    Not using target.database in singular test.

--- integration_tests/tests/singular_test_with_no_ref.sql
+++ integration_tests/tests/singular_test_with_no_ref.sql
@@ -1,1 +1,1 @@
-select min from {{ elementary.target_database() ~ '.' ~ target.schema ~ '.' ~ 'numeric_column_anomalies' }} where min < 100
+select min from {{ target.schema ~ '.' ~ 'numeric_column_anomalies' }} where min < 100
