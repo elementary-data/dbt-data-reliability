d224645bb554df4592275534607ba066fa6dcf03
Itamar Hartstein <haritamar@gmail.com>
2023-01-06 16:54:10 +0200

    dummy materialization: add needed things

--- macros/edr/materializations/models/dummy.sql
+++ macros/edr/materializations/models/dummy.sql
@@ -1,9 +1,20 @@
 {% materialization dummy %}
   {% do elementary.debug_log("Dummy materialization invoked for model {}, doing nothing!".format(model.name)) %}
 
+  {% set identifier = model['alias'] %}
+  {%- set target_relation = api.Relation.create(
+      identifier=identifier, schema=schema, database=database,
+      type='table') -%}
+
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
   {% call statement('main') -%}
     SELECT 'dummy'
   {% endcall %}
 
-  {% do return({'relations': []}) %}
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  {% do return({'relations': [target_relation]}) %}
 {% endmaterialization %}
