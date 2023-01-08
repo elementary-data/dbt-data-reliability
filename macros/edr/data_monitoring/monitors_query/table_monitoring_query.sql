f8ef3605e9d333708024ac351d99d46949f1cf45
Itamar Hartstein <haritamar@gmail.com>
2023-01-09 00:43:48 +0200

    table_monitoring_query: bugfix

--- macros/edr/data_monitoring/monitors_query/table_monitoring_query.sql
+++ macros/edr/data_monitoring/monitors_query/table_monitoring_query.sql
@@ -185,14 +185,20 @@
         order by 1
     ),
 
-    -- compute freshness for every update as the time difference from the previous update
-    consecutive_updates_freshness as (
+    consecutive_updates as (
         select
             timestamp_val as update_timestamp,
             lag(timestamp_val) over (order by timestamp_val) as prev_timestamp,
-            {{ elementary.timediff('second', 'lag(timestamp_val)', 'timestamp_val') }} as freshness
         from unique_timestamps
         where timestamp_val >= (select min(edr_bucket_start) from buckets)
+    ),
+
+    -- compute freshness for every update as the time difference from the previous update
+    consecutive_updates_freshness as (
+        select
+            update_timestamp,
+            {{ elementary.timediff('second', 'prev_timestamp', 'update_timestamp') }} as freshness
+        from consecutive_updates
     ),
 
     -- divide the freshness metrics above to buckets
