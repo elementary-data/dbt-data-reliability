4f2144fe213b5e03a35c3f22fa368e09d14fad3d
Elon Gliksberg <elongliks@gmail.com>
2023-01-15 14:14:00 +0200

    'incremental_predicates' is conditional.

--- macros/utils/table_operations/merge_sql.sql
+++ macros/utils/table_operations/merge_sql.sql
@@ -4,11 +4,18 @@
 
 {# Snowflake, Bigquery and Databricks #}
 {% macro default__merge_sql(target_relation, tmp_relation, unique_key, dest_columns, incremental_predicates) %}
-    {%- set merge_sql = dbt.get_merge_sql(target_relation, tmp_relation, unique_key, dest_columns, incremental_predicates) %}
-    {{ return(merge_sql) }}
+    {% set args = dbt.get_merge_sql.get_macro().arguments %}
+    {% if "incremental_predicates" in args %}
+      {% do return(dbt.get_merge_sql(target_relation, tmp_relation, unique_key, dest_columns, incremental_predicates)) %}
+    {% endif %}
+    {% do return(dbt.get_merge_sql(target_relation, tmp_relation, unique_key, dest_columns)) %}
 {% endmacro %}
 
 {% macro postgres__merge_sql(target_relation, tmp_relation, unique_key, dest_columns, incremental_predicates) %}
-    {%- set merge_sql = dbt.get_delete_insert_merge_sql(target_relation, tmp_relation, unique_key, dest_columns, incremental_predicates) %}
+    {% set args = dbt.get_merge_sql.get_macro().arguments %}
+    {% if "incremental_predicates" in args %}
+      {% do return(dbt.get_delete_insert_merge_sql(target_relation, tmp_relation, unique_key, dest_columns, incremental_predicates)) %}
+    {% endif %}
+    {% do return(dbt.get_delete_insert_merge_sql(target_relation, tmp_relation, unique_key, dest_columns)) %}
     {{ return(merge_sql) }}
 {% endmacro %}
