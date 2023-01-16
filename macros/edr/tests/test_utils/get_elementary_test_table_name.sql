90500cc2bc4c7a6bc3eb026dcdb3065e427d8972
Elon Gliksberg <elongliks@gmail.com>
2023-01-16 13:52:12 +0200

    Changed implementation of Elementary's test table name.

--- macros/edr/tests/test_utils/get_elementary_test_table_name.sql
+++ macros/edr/tests/test_utils/get_elementary_test_table_name.sql
@@ -2,5 +2,5 @@
     {% set test_node = model %}
     {% set test_hash = test_node.unique_id.split(".")[-1] %}
     {% set test_name = test_node.name %}
-    {% do return("{}_{}".format(test_name, test_hash)) %}
+    {% do return("test_{}_{}".format(test_hash, test_name)) %}
 {% endmacro %}
