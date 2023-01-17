57928e831336be8f4b51d91660a7fd1f20dd9ca1
Itamar Hartstein <haritamar@gmail.com>
2023-01-17 15:23:42 +0200

    replace_table_data: refactor a bit + faster implementation for snowflake + atomic implementation for Databricks

--- macros/edr/dbt_artifacts/upload_artifacts_to_table.sql
+++ macros/edr/dbt_artifacts/upload_artifacts_to_table.sql
@@ -11,28 +11,6 @@
         {# In append mode, just insert, and no need to be atomic #}
         {% do elementary.insert_rows(table_relation, flatten_artifact_dicts, should_commit, elementary.get_config_var('dbt_artifacts_chunk_size')) %}
     {% else %}
-        {% do elementary.overwrite_artifacts_table(table_relation, flatten_artifact_dicts, should_commit) %}
+        {% do elementary.replace_table_data(table_relation, flatten_artifact_dicts) %}
     {% endif %}
-{% endmacro %}
-
-{% macro overwrite_artifacts_table(table_relation, flatten_artifact_dicts, should_commit) %}
-    {% do return(adapter.dispatch('overwrite_artifacts_table', 'elementary')(table_relation, flatten_artifact_dicts, should_commit)) %}
-{% endmacro %}
-
-{% macro default__overwrite_artifacts_table(table_relation, flatten_artifact_dicts, should_commit) %}
-    {# First upload everything to a temp table #}
-    {% set temp_table_relation = dbt.make_temp_relation(table_relation) %}
-    {% do elementary.create_table_like(temp_table_relation, table_relation, temporary=True) %}
-    {% do elementary.insert_rows(temp_table_relation, flatten_artifact_dicts, should_commit, elementary.get_config_var('dbt_artifacts_chunk_size')) %}
-
-    {# Now atomically replace the data #}
-    {% do elementary.replace_data_with_table_contents(table_relation, temp_table_relation) %}
-{% endmacro %}
-
-{% macro spark__overwrite_artifacts_table(table_relation, flatten_artifact_dicts, should_commit) %}
-    {# Databricks does not support temp tables well enough, and does not have transactions, so we provide
-       a more straightforward (non-atomic) implementation here #}
-
-    {% do dbt.truncate_relation(table_relation) %}
-    {% do elementary.insert_rows(table_relation, flatten_artifact_dicts, should_commit, elementary.get_config_var('dbt_artifacts_chunk_size')) %}
 {% endmacro %}
---
+++ macros/utils/table_operations/replace_table_data.sql
@@ -0,0 +1,54 @@
+{% macro replace_table_data(relation, rows) %}
+    {{ return(adapter.dispatch('replace_table_data', 'elementary')(relation, sql_query)) }}
+{% endmacro %}
+
+{# BQ / Databricks - upload data to a temp table, and then atomically replace the table with a new one #}
+{% macro default__replace_table_data(relation, rows) %}
+    {# First upload everything to an intermediate table (temporary) #}
+    {% set intermediate_relation = elementary.create_intermediate_relation(relation, rows, temporary=True) %}
+
+    {# Now atomically replace the table with data from the temp table #}
+    {% do dbt.run_query(dbt.create_table_as(False, relation, 'select * from {}'.format(intermediate_relation))) %}
+{% endmacro %}
+
+{# In Snowflake we can atomically swap two tables atomically, so we can provide a faster implementation #}
+{% macro snowflake__replace_table_data(relation, rows) %}
+    {# First upload everything to an intermediate table (non-temporary) #}
+    {% set intermediate_relation = elementary.create_intermediate_relation(relation, rows, temporary=False) %}
+
+    {# Atomically swap the tables #}
+    {% do dbt.run_query("alter table {} swap with {}".format(relation, intermediate_relation)) %}
+
+    {# Drop the old (swapped) relation #}
+    {% do dbt.drop_relation(intermediate_relation) %}
+{% endmacro %}
+
+{# In postgres/redshift we do not want to replace the table, because that will cause views without
+   late binding to be deleted. So instead we atomically replace the data in a transaction #}
+{% macro postgres__replace_table_data(relation, rows) %}
+    {# First upload everything to an intermediate table (temporary) #}
+    {% set intermediate_relation = elementary.create_intermediate_relation(relation, rows, temporary=True) %}
+
+    {% set query %}
+        begin transaction;
+        delete from {{ relation }};   -- truncate supported in Redshift transactions, but causes an immediate commit
+        insert into {{ relation }} select * from {{ intermediate_relation }};
+        commit;
+    {% endset %}
+    {% do dbt.run_query(query) %}
+{% endmacro %}
+
+{% macro create_intermediate_relation(base_relation, rows, temporary) %}
+    {% if temporary %}
+        {% set int_relation = dbt.make_temp_relation(base_relation) %}
+    {% else %}
+        {# for non temporary relations - make sure the name is unique #}
+        {% set int_identifier = base_relation.identifier + modules.datetime.datetime.utcnow().strftime('__tmp_%Y%m%d%H%M%S%f') %}
+        {% set int_relation = base_relation.incorporate(path={'identifier': int_identifier}) %}
+    {% endif %}
+
+    {% do elementary.create_table_like(int_relation, base_relation, temporary=temporary) %}
+    {% do elementary.insert_rows(int_relation, rows, should_commit, elementary.get_config_var('dbt_artifacts_chunk_size')) %}
+
+    {% do return(int_relation) %}
+{% endmacro %}
