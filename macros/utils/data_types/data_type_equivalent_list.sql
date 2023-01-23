a0b8f1ea217885dfccc4ae0671cbee5616a6eeea
RoiTabach <roi.tabach@gmail.com>
2023-01-23 12:30:43 +0200

    add all types per data warehouse

--- macros/utils/data_types/data_type_equivalent_list.sql
+++ macros/utils/data_types/data_type_equivalent_list.sql
@@ -4,33 +4,35 @@
 {% endmacro %}
 
 {% macro default__exact_data_type_equivalent_list(exact_data_type) %}
-
- {% set exact_data_type_to_data_type_returned_by_the_info_schema = {'VARCHAR': 'TEXT',
-                'STRING': 'TEXT'}%}
- {%- if exact_data_type in exact_data_type_to_data_type_returned_by_the_info_schema%}
-   {{ return (exact_data_type_to_data_type_returned_by_the_info_schema[exact_data_type])}}
- {%- else %}
    {{return (exact_data_type) }}
- {%- endif%}
 {% endmacro %}
 
 {% macro bigquery__exact_data_type_equivalent_list(exact_data_type) %}
-
- {% set exact_data_type_to_data_type_returned_by_the_info_schema = {'VARCHAR': 'TEXT',
-                'STRING': 'TEXT'}%}
- {%- if exact_data_type in exact_data_type_to_data_type_returned_by_the_info_schema%}
-   {{ return (exact_data_type_to_data_type_returned_by_the_info_schema[exact_data_type])}}
- {%- else %}
+{# BigQuery has no concept of data type synonyms,
+ see https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types #}
    {{return (exact_data_type) }}
- {%- endif%}
 {% endmacro %}
 
 
 
 {% macro snowflake__exact_data_type_equivalent_list(exact_data_type) %}
-
+{# understanding Snowflake data type synonyms:
+ https://docs.snowflake.com/en/sql-reference/intro-summary-data-types.html #}
  {% set exact_data_type_to_data_type_returned_by_the_info_schema = {'VARCHAR': 'TEXT',
-                'STRING': 'TEXT'}%}
+                'STRING': 'TEXT',
+                'NVARCHAR': 'TEXT',
+                'NUMERIC': 'NUMBER',
+                'DECIMAL': 'NUMBER',
+                'INT':'NUMBER',
+                'INTEGER':'NUMBER',
+                'SMALLINT':'NUMBER',
+                'BIGINT':'NUMBER',
+                'TINYINT':'NUMBER',
+                'BYTEINT':'NUMBER',
+                'REAL': 'FLOAT',
+                'DOUBLE':'FLOAT',
+                'DOUBLE PRECISION': 'FLOAT'
+                }%}
  {%- if exact_data_type in exact_data_type_to_data_type_returned_by_the_info_schema%}
    {{ return (exact_data_type_to_data_type_returned_by_the_info_schema[exact_data_type])}}
  {%- else %}
@@ -41,12 +43,7 @@
 
 
 {% macro spark__exact_data_type_equivalent_list(exact_data_type) %}
-
- {% set exact_data_type_to_data_type_returned_by_the_info_schema = {'VARCHAR': 'TEXT',
-                'STRING': 'TEXT'}%}
- {%- if exact_data_type in exact_data_type_to_data_type_returned_by_the_info_schema%}
-   {{ return (exact_data_type_to_data_type_returned_by_the_info_schema[exact_data_type])}}
- {%- else %}
+{# spark also has no concept of data type synonyms :
+   see https://spark.apache.org/docs/latest/sql-ref-datatypes.html #}
    {{return (exact_data_type) }}
- {%- endif%}
 {% endmacro %}
