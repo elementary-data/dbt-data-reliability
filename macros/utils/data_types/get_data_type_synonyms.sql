f09bfc866cae46289a1bae1f6a56e4c7d0d0b773
RoiTabach <roi.tabach@gmail.com>
2023-01-23 13:10:38 +0200

    rename macro to get_data_type_synonyms

--- macros/utils/data_types/get_data_type_synonyms.sql
+++ macros/utils/data_types/get_data_type_synonyms.sql
@@ -1,13 +1,13 @@
-{% macro exact_data_type_equivalent_list(exact_data_type) %}
-    {% set result = adapter.dispatch('exact_data_type_equivalent_list','elementary')(exact_data_type) %}
+{% macro get_data_type_synonyms(exact_data_type) %}
+    {% set result = adapter.dispatch('get_data_type_synonyms','elementary')(exact_data_type) %}
     {{ return(result) }}
 {% endmacro %}
 
-{% macro default__exact_data_type_equivalent_list(exact_data_type) %}
+{% macro default__get_data_type_synonyms(exact_data_type) %}
    {{return (exact_data_type) }}
 {% endmacro %}
 
-{% macro bigquery__exact_data_type_equivalent_list(exact_data_type) %}
+{% macro bigquery__get_data_type_synonyms(exact_data_type) %}
 {# BigQuery has no concept of data type synonyms,
  see https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types #}
    {{return (exact_data_type) }}
@@ -15,7 +15,7 @@
 
 
 
-{% macro snowflake__exact_data_type_equivalent_list(exact_data_type) %}
+{% macro snowflake__get_data_type_synonyms(exact_data_type) %}
 {# understanding Snowflake data type synonyms:
  https://docs.snowflake.com/en/sql-reference/intro-summary-data-types.html #}
  {% set exact_data_type_to_data_type_returned_by_the_info_schema = {'VARCHAR': 'TEXT',
@@ -42,7 +42,7 @@
 
 
 
-{% macro spark__exact_data_type_equivalent_list(exact_data_type) %}
+{% macro spark__get_data_type_synonyms(exact_data_type) %}
 {# spark also has no concept of data type synonyms :
    see https://spark.apache.org/docs/latest/sql-ref-datatypes.html #}
    {{return (exact_data_type) }}
