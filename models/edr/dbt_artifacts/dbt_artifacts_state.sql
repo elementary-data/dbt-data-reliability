4a1901d8a5abe6d620d29000f4da2a8da016da8e
Elon Gliksberg <elongliks@gmail.com>
2023-01-24 17:19:34 +0200

    dbt_artifacts_state is not available on Spark targets.

--- models/edr/dbt_artifacts/dbt_artifacts_state.sql
+++ models/edr/dbt_artifacts/dbt_artifacts_state.sql
@@ -1,15 +1,20 @@
 {{
   config(
     materialized = 'view',
-    bind=False
+    bind=False,
+    enabled =(target.type != 'databricks' and target.type != 'spark')
   )
 }}
 
+{% set hash_agg_expr %}
+md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }})
+{% endset %}
+
 select
-  (select md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }}) from {{ ref("dbt_models") }}) as dbt_models,
-  (select md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }}) from {{ ref("dbt_tests") }}) as dbt_tests,
-  (select md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }}) from {{ ref("dbt_sources") }}) as dbt_sources,
-  (select md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }}) from {{ ref("dbt_snapshots") }}) as dbt_snapshots,
-  (select md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }}) from {{ ref("dbt_exposures") }}) as dbt_exposures,
-  (select md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }}) from {{ ref("dbt_metrics") }}) as dbt_metrics,
-  (select md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }}) from {{ ref("dbt_seeds") }}) as dbt_seeds
+  (select {{ hash_agg_expr }} from {{ ref("dbt_models") }}) as dbt_models,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_tests") }}) as dbt_tests,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_sources") }}) as dbt_sources,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_snapshots") }}) as dbt_snapshots,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_exposures") }}) as dbt_exposures,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_metrics") }}) as dbt_metrics,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_seeds") }}) as dbt_seeds
