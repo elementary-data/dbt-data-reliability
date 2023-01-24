ccc7e528208514e6cb2fdabb991a765f3a950a69
Elon Gliksberg <elongliks@gmail.com>
2023-01-25 00:25:11 +0200

    Cached artifacts are only tested from dbt version v1.4.0+.

--- integration_tests/integration_tests/test_artifacts.py
+++ integration_tests/integration_tests/test_artifacts.py
@@ -1,8 +1,13 @@
 import json
 from pathlib import Path
 
+import pytest
+from dbt.version import __version__
+from packaging import version
+
 from elementary.clients.dbt.dbt_runner import DbtRunner
 
+dbt_version = version.parse(__version__)
 INTEG_TESTS_DIR = str(Path(__file__).parent.parent)
 
 
@@ -18,6 +23,10 @@ def test_artifacts_on_run_end(dbt_target: str):
     assert first_generated_at != second_generated_at
 
 
+@pytest.mark.skipif(
+    dbt_version < version.parse("1.4.0"),
+    reason="Cached artifacts are only supported from dbt v1.4.0+.",
+)
 def test_cache_artifacts(dbt_target: str):
     dbt_runner = DbtRunner(
         project_dir=INTEG_TESTS_DIR,
