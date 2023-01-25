f4f7dced9b919c54409e9ff86a8c711c2b0c7713
Elon Gliksberg <elongliks@gmail.com>
2023-01-25 13:07:01 +0200

    Revert "Added supported for Spark adapters."
    This reverts commit 811f5733e58a83fcb4953a8254975f726257934e.

--- macros/edr/dbt_artifacts/artifacts_hash.sql
+++
@@ -1,4 +0,0 @@
-{% macro get_ordered_artifact_hash_query(model) %}
-select artifact_hash from {{ ref(model) }}
-order by artifact_hash
-{% endmacro %}
--- models/edr/dbt_artifacts/dbt_artifacts_state.sql
+++ models/edr/dbt_artifacts/dbt_artifacts_state.sql
@@ -1,22 +1,20 @@
 {{
   config(
     materialized = 'view',
-    bind=False
+    bind=False,
+    enabled=(target.type not in ['databricks', 'spark'])
   )
 }}
 
 {% set hash_agg_expr %}
-md5({{ elementary.listagg("artifact_hash") }})
+md5({{ elementary.listagg("artifact_hash", order_by_clause="order by artifact_hash") }})
 {% endset %}
 
-
-
-
 select
-  (select {{ hash_agg_expr }} from ({{ elementary.get_ordered_artifact_hash_query("dbt_models") }}) results) as dbt_models,
-  (select {{ hash_agg_expr }} from ({{ elementary.get_ordered_artifact_hash_query("dbt_tests") }}) results) as dbt_tests,
-  (select {{ hash_agg_expr }} from ({{ elementary.get_ordered_artifact_hash_query("dbt_sources") }}) results) as dbt_sources,
-  (select {{ hash_agg_expr }} from ({{ elementary.get_ordered_artifact_hash_query("dbt_snapshots") }}) results) as dbt_snapshots,
-  (select {{ hash_agg_expr }} from ({{ elementary.get_ordered_artifact_hash_query("dbt_exposures") }}) results) as dbt_exposures,
-  (select {{ hash_agg_expr }} from ({{ elementary.get_ordered_artifact_hash_query("dbt_metrics") }}) results) as dbt_metrics,
-  (select {{ hash_agg_expr }} from ({{ elementary.get_ordered_artifact_hash_query("dbt_seeds") }}) results) as dbt_seeds
+  (select {{ hash_agg_expr }} from {{ ref("dbt_models") }}) as dbt_models,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_tests") }}) as dbt_tests,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_sources") }}) as dbt_sources,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_snapshots") }}) as dbt_snapshots,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_exposures") }}) as dbt_exposures,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_metrics") }}) as dbt_metrics,
+  (select {{ hash_agg_expr }} from {{ ref("dbt_seeds") }}) as dbt_seeds
