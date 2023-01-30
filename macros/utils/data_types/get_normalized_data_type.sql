41c5de631398b17c26d184faef2d50fe7fb312ac
RoiTabach <roi.tabach@gmail.com>
2023-01-30 12:48:11 +0200

    rename to get_normalized_data_type.

--- macros/utils/data_types/get_normalized_data_type.sql
+++ macros/utils/data_types/get_normalized_data_type.sql
@@ -1,13 +1,13 @@
-{% macro get_data_type_synonyms(exact_data_type) %}
-    {% set result = adapter.dispatch('get_data_type_synonyms','elementary')(exact_data_type) %}
+{% macro get_normalized_data_type(exact_data_type) %}
+    {% set result = adapter.dispatch('get_normalized_data_type','elementary')(exact_data_type) %}
     {{ return(result) }}
 {% endmacro %}
 
-{% macro default__get_data_type_synonyms(exact_data_type) %}
+    {% macro default__get_normalized_data_type(exact_data_type) %}
    {{return (exact_data_type) }}
 {% endmacro %}
 
-{% macro bigquery__get_data_type_synonyms(exact_data_type) %}
+{% macro bigquery__get_normalized_data_type(exact_data_type) %}
 {# BigQuery has no concept of data type synonyms,
  see https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types #}
    {{return (exact_data_type) }}
@@ -15,7 +15,7 @@
 
 
 
-{% macro snowflake__get_data_type_synonyms(exact_data_type) %}
+{% macro snowflake__get_normalized_data_type(exact_data_type) %}
 {# understanding Snowflake data type synonyms:
  https://docs.snowflake.com/en/sql-reference/intro-summary-data-types.html #}
  {% set exact_data_type_to_data_type_returned_by_the_info_schema = {'VARCHAR': 'TEXT',
@@ -42,14 +42,14 @@
 
 
 
-{% macro spark__get_data_type_synonyms(exact_data_type) %}
+{% macro spark__get_normalized_data_type(exact_data_type) %}
 {# spark also has no concept of data type synonyms :
    see https://spark.apache.org/docs/latest/sql-ref-datatypes.html #}
    {{return (exact_data_type) }}
 {% endmacro %}
 
 
-{% macro redshift__get_data_type_synonyms(exact_data_type) %}
+{% macro redshift__get_normalized_data_type(exact_data_type) %}
 {# understanding Redshift data type synonyms:
  https://docs.aws.amazon.com/redshift/latest/dg/c_Supported_data_types.html #}
  {% set exact_data_type_to_data_type_returned_by_the_info_schema = {'INT2':	'SMALLINT',
@@ -81,7 +81,7 @@
 {% endmacro %}
 
 
-{% macro postgres__get_data_type_synonyms(exact_data_type) %}
+{% macro postgres__get_normalized_data_type(exact_data_type) %}
 {# understanding Postgres data type synonyms:
  https://www.postgresql.org/docs/current/datatype.html #}
  {% set exact_data_type_to_data_type_returned_by_the_info_schema =  {'BIGINT': 'INT8',
