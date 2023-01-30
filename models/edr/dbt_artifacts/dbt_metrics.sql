43474319a2f47f0e3e6d08f388ae7ed74b5f61cc
Elon Gliksberg <elongliks@gmail.com>
2023-01-30 15:36:59 +0200

    Changed 'append_new_columns' to 'sync_all_columns'.

--- models/edr/dbt_artifacts/dbt_metrics.sql
+++ models/edr/dbt_artifacts/dbt_metrics.sql
@@ -4,7 +4,7 @@
     transient=False,
     post_hook='{{ elementary.upload_dbt_metrics() }}',
     unique_key='unique_id',
-    on_schema_change='append_new_columns',
+    on_schema_change='sync_all_columns',
     full_refresh=elementary.get_config_var('elementary_full_refresh')
     )
 }}
