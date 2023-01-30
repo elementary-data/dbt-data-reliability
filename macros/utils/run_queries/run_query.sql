c5425aba39686506166923408abdd05db641aa2a
Elon Gliksberg <elongliks@gmail.com>
2023-01-30 16:45:32 +0200

    Added a 'lowercase_column_names' flag to 'run_query'.

--- macros/utils/run_queries/run_query.sql
+++ macros/utils/run_queries/run_query.sql
@@ -1,8 +1,11 @@
-{% macro run_query(query) %}
+{% macro run_query(query, lowercase_column_names=True) %}
     {% set query_result = dbt.run_query(query) %}
-    {% set lowercased_column_names = {} %}
-    {% for column_name in query_result.column_names %}
-        {% do lowercased_column_names.update({column_name: column_name.lower()}) %}
-    {% endfor %}
-    {% do return(query_result.rename(lowercased_column_names)) %}
+    {% if lowercase_column_names %}
+        {% set lowercased_column_names = {} %}
+        {% for column_name in query_result.column_names %}
+            {% do lowercased_column_names.update({column_name: column_name.lower()}) %}
+        {% endfor %}
+        {% set query_result = query_result.rename(lowercased_column_names) %}
+    {% endif %}
+    {% do return(query_result) %}
 {% endmacro %}
