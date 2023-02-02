752a5db6eb8fb06db2ca948e12877f491b669caf
Maayan-s <maayansalom@gmail.com>
2023-02-02 16:04:03 +0200

    add current_timestamp for Spark

--- macros/utils/cross_db_utils/current_timestamp.sql
+++ macros/utils/cross_db_utils/current_timestamp.sql
@@ -10,6 +10,14 @@
     {{ return(macro()) }}
 {%- endmacro %}
 
+{% macro databricks__current_timestamp() -%}
+    {% set macro = dbt.current_timestamp_backcompat or dbt_utils.current_timestamp %}
+    {% if not macro %}
+        {{ exceptions.raise_compiler_error("Did not find a `current_timestamp` macro.") }}
+    {% endif %}
+    {{ return(macro()) }}
+{%- endmacro %}
+
 {% macro spark__current_timestamp() %}
     current_timestamp()
 {% endmacro %}
@@ -20,6 +28,14 @@
 {%- endmacro %}
 
 {% macro default__current_timestamp_in_utc() -%}
+    {% set macro = dbt.current_timestamp_in_utc_backcompat or dbt_utils.current_timestamp_in_utc %}
+    {% if not macro %}
+        {{ exceptions.raise_compiler_error("Did not find a `current_timestamp_in_utc` macro.") }}
+    {% endif %}
+    {{ return(macro()) }}
+{%- endmacro %}
+
+{% macro databricks__current_timestamp_in_utc() -%}
     {% set macro = dbt.current_timestamp_in_utc_backcompat or dbt_utils.current_timestamp_in_utc %}
     {% if not macro %}
         {{ exceptions.raise_compiler_error("Did not find a `current_timestamp_in_utc` macro.") }}
