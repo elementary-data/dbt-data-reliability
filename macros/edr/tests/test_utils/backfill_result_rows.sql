dbd5fa74101c774e6f8b213d8d03a678f51472cd
Elon Gliksberg <elongliks@gmail.com>
2023-02-05 13:09:13 +0200

    Updated the backfill macro.

--- macros/edr/tests/test_utils/backfill_result_rows.sql
+++ macros/edr/tests/test_utils/backfill_result_rows.sql
@@ -1,25 +1,25 @@
 {% macro backfill_result_rows() %}
     {% if is_incremental() %}
-        {% do return(none) %}
+        {% do return('') %}
     {% endif %}
 
     {% set elementary_test_results_relation = ref("elementary", "elementary_test_results") %}
     {% set result_row_exists = elementary.get_column_in_relation(elementary_test_results_relation, "result_rows") is not none %}
     {% if not result_row_exists %}
-        {% do return(none) %}
+        {% do return('') %}
     {% endif %}
 
     {% set test_result_rows_rows = [] %}
-    {% set query %}
-    select
-      id,
-      detected_at,
-      result_rows
-    from {{ ref("elementary", "elementary_test_results") }}
-    where detected_at > {{ elementary.timediff(elementary.cast_as_timestamp("detected_at"), elementary.current_timestamp(), "day") }} < {{ elementary.get_config_var("days_back") }}
-    and result_rows is not null
+    {% set backfill_query %}
+        select
+        id,
+        detected_at,
+        result_rows
+        from {{ ref("elementary", "elementary_test_results") }}
+        where {{ elementary.datediff(elementary.cast_as_timestamp('detected_at'), elementary.current_timestamp(), 'day') }} < {{ elementary.get_config_var("days_back") }}
+        and result_rows is not null
     {% endset %}
-    {% set test_results_with_result_rows = elementary.run_query(query) %}
+    {% set test_results_with_result_rows = elementary.run_query(backfill_query) %}
     {% for elementary_test_results_row in test_results_with_result_rows %}
         {% set result_rows = fromjson(elementary_test_results_row.result_rows) %}
         {% for result_row in result_rows %}
