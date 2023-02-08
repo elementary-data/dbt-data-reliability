afc5fc3f6eef7dde003dd34d31949b751cddf7d0
RoiTabach <roi.tabach@gmail.com>
2023-02-08 10:27:47 +0200

    cast_to_string in dict_to_quoted_json to fix Redshift issue

--- macros/utils/cross_db_utils/quote_column.sql
+++ macros/utils/cross_db_utils/quote_column.sql
@@ -5,7 +5,7 @@
 {% endmacro %}
 
 {% macro dict_to_quoted_json(d) %}
-    {% do return(elementary.quote(tojson(d, sort_keys=true))) %}
+    {% do return(elementary.cast_as_string(elementary.quote(tojson(d, sort_keys=true)))) %}
 {% endmacro %}
 
 {% macro quote_column(column_name) %}
