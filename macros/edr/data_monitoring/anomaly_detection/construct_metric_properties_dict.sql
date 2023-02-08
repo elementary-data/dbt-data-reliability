2c9f75644d153cc5859d6fdf4351617a54a9051e
RoiTabach <roi.tabach@gmail.com>
2023-02-08 16:42:01 +0200

    add comment to explain Undefined vs none issue

--- macros/edr/data_monitoring/anomaly_detection/construct_metric_properties_dict.sql
+++ macros/edr/data_monitoring/anomaly_detection/construct_metric_properties_dict.sql
@@ -1,5 +1,5 @@
 {% macro construct_metric_properties_dict(timestamp_column, where_expression, time_bucket) %}
-
+  {# each one of the 3 params will go into the returned dictionary, unless "Undefined" which would be converted to "None" #}
   {%set d = {'timestamp_column': ( timestamp_column if timestamp_column else None ) ,
              'where_expression': ( where_expression if where_expression else None ) ,
              'time_bucket':      ( time_bucket if time_bucket else None )            } %}
