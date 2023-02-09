18736e75cec6436d0a64ae75d7368b605b5bc16a
RoiTabach <roi.tabach@gmail.com>
2023-02-09 17:04:32 +0200

    uses elementary.escape_special_chars instead of re-implementing it inside elementary.quote(str)

--- macros/utils/cross_db_utils/quote_column.sql
+++ macros/utils/cross_db_utils/quote_column.sql
@@ -1,6 +1,6 @@
 
 {% macro quote(str) %}
-    {% set escaped_str = str | replace("'","\\'") %}
+    {% set escaped_str = elementary.escape_special_chars(str) %}
     {% do return("'{}'".format(escaped_str)) %}
 {% endmacro %}
 
