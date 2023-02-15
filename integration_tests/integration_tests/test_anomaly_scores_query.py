cf30b9ddf797ed7f9a0e597528d4d9cd362afa6a
Elon Gliksberg <elongliks@gmail.com>
2023-02-15 15:52:20 +0200

    Formatted code.

--- integration_tests/integration_tests/test_anomaly_scores_query.py
+++ integration_tests/integration_tests/test_anomaly_scores_query.py
@@ -5,25 +5,37 @@ from parametrization import Parametrization
 
 
 from .dbt_project import DbtProject
-from .utils import create_test_table, insert_rows, update_var, lowercase_column_names, assert_dfs_equal, \
-    agate_table_to_pandas_dataframe, get_package_database_and_schema
+from .utils import (
+    create_test_table,
+    insert_rows,
+    update_var,
+    lowercase_column_names,
+    assert_dfs_equal,
+    agate_table_to_pandas_dataframe,
+    get_package_database_and_schema,
+)
 
 MIN_BUCKET_START = datetime(2022, 1, 1, 0, 0, 0)
 RUN_STARTED_AT = datetime(2022, 1, 4, 0, 13, 42)
-DATA_MONITORING_METRICS_COLUMNS_TO_TYPES = [('id', 'string'),  # same as empty_table.sql::empty_data_monitoring_metrics
-                                             ('full_table_name', 'string'),
-                                             ('column_name', 'string'),
-                                             ('metric_name', 'string'),
-                                             ('metric_value', 'float'),
-                                             ('source_value', 'string'),
-                                             ('bucket_start', 'timestamp'),
-                                             ('bucket_end', 'timestamp'),
-                                             ('bucket_duration_hours', 'int'),
-                                             ('updated_at', 'timestamp'),
-                                             ('dimension', 'string'),
-                                             ('dimension_value', 'string'),
-                                             ('metric_properties', 'string')]
-BASE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'anomaly_scores_query_test_cases')
+DATA_MONITORING_METRICS_COLUMNS_TO_TYPES = [
+    ("id", "string"),  # same as empty_table.sql::empty_data_monitoring_metrics
+    ("full_table_name", "string"),
+    ("column_name", "string"),
+    ("metric_name", "string"),
+    ("metric_value", "float"),
+    ("source_value", "string"),
+    ("bucket_start", "timestamp"),
+    ("bucket_end", "timestamp"),
+    ("bucket_duration_hours", "int"),
+    ("updated_at", "timestamp"),
+    ("dimension", "string"),
+    ("dimension_value", "string"),
+    ("metric_properties", "string"),
+]
+BASE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "anomaly_scores_query_test_cases"
+)
+
 
 @Parametrization.autodetect_parameters()
 @Parametrization.default_parameters(
@@ -33,49 +45,71 @@ BASE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'anomaly_sc
 @Parametrization.case(
     name="freshness_1_day_mostly_non_anomalous",
     metric_name="freshness",
-    input_rows=os.path.join(BASE_DIR, 'freshness_1_day_mostly_non_anomalous_test.csv'),
-    input_rows_history=os.path.join(BASE_DIR, 'freshness_1_day_mostly_non_anomalous_history.csv'),
-    expected_output=os.path.join(BASE_DIR, 'freshness_1_day_mostly_non_anomalous_after.csv'),
-    alias_table_name='numeric_column_anomalies'
+    input_rows=os.path.join(BASE_DIR, "freshness_1_day_mostly_non_anomalous_test.csv"),
+    input_rows_history=os.path.join(
+        BASE_DIR, "freshness_1_day_mostly_non_anomalous_history.csv"
+    ),
+    expected_output=os.path.join(
+        BASE_DIR, "freshness_1_day_mostly_non_anomalous_after.csv"
+    ),
+    alias_table_name="numeric_column_anomalies",
 )
 @Parametrization.case(
     name="row_count_1_day_1_row_no_anomalies",
     metric_name="row_count",
-    input_rows=os.path.join(BASE_DIR, 'row_count_1_day_1_row_no_anomalies_test.csv'),
-    input_rows_history=os.path.join(BASE_DIR, 'row_count_1_day_1_row_no_anomalies_history.csv'),
-    expected_output=os.path.join(BASE_DIR, 'row_count_1_day_1_row_no_anomalies_after.csv'),
-    alias_table_name='any_type_column_anomalies_training'
+    input_rows=os.path.join(BASE_DIR, "row_count_1_day_1_row_no_anomalies_test.csv"),
+    input_rows_history=os.path.join(
+        BASE_DIR, "row_count_1_day_1_row_no_anomalies_history.csv"
+    ),
+    expected_output=os.path.join(
+        BASE_DIR, "row_count_1_day_1_row_no_anomalies_after.csv"
+    ),
+    alias_table_name="any_type_column_anomalies_training",
 )
 @Parametrization.case(
     name="row_count_1_day_mostly_non_anomalous",
     metric_name="row_count",
-    input_rows=os.path.join(BASE_DIR, 'row_count_1_day_mostly_non_anomalous_test.csv'),
-    input_rows_history=os.path.join(BASE_DIR, 'row_count_1_day_mostly_non_anomalous_history.csv'),
-    expected_output=os.path.join(BASE_DIR, 'row_count_1_day_mostly_non_anomalous_after.csv'),
-    alias_table_name='string_column_anomalies_training'
+    input_rows=os.path.join(BASE_DIR, "row_count_1_day_mostly_non_anomalous_test.csv"),
+    input_rows_history=os.path.join(
+        BASE_DIR, "row_count_1_day_mostly_non_anomalous_history.csv"
+    ),
+    expected_output=os.path.join(
+        BASE_DIR, "row_count_1_day_mostly_non_anomalous_after.csv"
+    ),
+    alias_table_name="string_column_anomalies_training",
 )
 @Parametrization.case(
     name="row_count_4_hour_non_empty_history_with_anomalies_in_the_end",
     metric_name="row_count",
-    input_rows=os.path.join(BASE_DIR, 'row_count_4_hour_non_empty_history_with_anomalies_in_the_end_test.csv'),
-    input_rows_history=os.path.join(BASE_DIR, 'row_count_4_hour_non_empty_history_with_anomalies_in_the_end_history.csv'),
-    expected_output=os.path.join(BASE_DIR, 'row_count_4_hour_non_empty_history_with_anomalies_in_the_end_after.csv'),
-    alias_table_name='any_type_column_anomalies',
+    input_rows=os.path.join(
+        BASE_DIR,
+        "row_count_4_hour_non_empty_history_with_anomalies_in_the_end_test.csv",
+    ),
+    input_rows_history=os.path.join(
+        BASE_DIR,
+        "row_count_4_hour_non_empty_history_with_anomalies_in_the_end_history.csv",
+    ),
+    expected_output=os.path.join(
+        BASE_DIR,
+        "row_count_4_hour_non_empty_history_with_anomalies_in_the_end_after.csv",
+    ),
+    alias_table_name="any_type_column_anomalies",
     time_bucket={"period": "hour", "count": 4},
 )
-def test_anomaly_scores_query(dbt_project: DbtProject,
-                              input_rows,
-                              input_rows_history,
-                              expected_output,
-                              metric_name,
-                              alias_table_name,
-                              time_bucket,
-                              timestamp_column,
-                              where_expression=None,
-                              dimensions=None,
-                              columns_only=False,
-                              column_name=None
-                              ):
+def test_anomaly_scores_query(
+    dbt_project: DbtProject,
+    input_rows,
+    input_rows_history,
+    expected_output,
+    metric_name,
+    alias_table_name,
+    time_bucket,
+    timestamp_column,
+    where_expression=None,
+    dimensions=None,
+    columns_only=False,
+    column_name=None,
+):
     """
     get_anomaly_scores_query macro returns a query that expects 2 input tables.
     first - history of metrics, assumed to be the parameter `data_monitoring_metrics_table` if given, or else,
@@ -95,51 +129,62 @@ def test_anomaly_scores_query(dbt_project: DbtProject,
     :param where_expression:
     :return:
     """
-    update_var(dbt_project, "custom_run_started_at", RUN_STARTED_AT.strftime("%Y-%m-%d %H:%M:%S"))
+    update_var(
+        dbt_project,
+        "custom_run_started_at",
+        RUN_STARTED_AT.strftime("%Y-%m-%d %H:%M:%S"),
+    )
 
-    my_test_metrics_relation = create_test_table(dbt_project,
-                                 "my_test_metrics_table_relation",
-                                 {x: y for x, y in DATA_MONITORING_METRICS_COLUMNS_TO_TYPES})
-    my_test_data_monitoring_metrics = create_test_table(dbt_project,
-                                                        "my_test_data_monitoring_metrics",
-                                                        {x: y for x, y in DATA_MONITORING_METRICS_COLUMNS_TO_TYPES})
-
+    my_test_metrics_relation = create_test_table(
+        dbt_project,
+        "my_test_metrics_table_relation",
+        {x: y for x, y in DATA_MONITORING_METRICS_COLUMNS_TO_TYPES},
+    )
+    my_test_data_monitoring_metrics = create_test_table(
+        dbt_project,
+        "my_test_data_monitoring_metrics",
+        {x: y for x, y in DATA_MONITORING_METRICS_COLUMNS_TO_TYPES},
+    )
 
     insert_rows(dbt_project, my_test_metrics_relation, input_rows)
     insert_rows(dbt_project, my_test_data_monitoring_metrics, input_rows_history)
 
-
-    metric_properties = {'time_bucket': time_bucket,
-                         'timestamp_column': timestamp_column,
-                         'where_expression': where_expression,
-                         'freshness_column': None,
-                         'event_timestamp_column': None
-                         }
+    metric_properties = {
+        "time_bucket": time_bucket,
+        "timestamp_column": timestamp_column,
+        "where_expression": where_expression,
+        "freshness_column": None,
+        "event_timestamp_column": None,
+    }
     if dbt_project.adapter_name not in ["postgres", "redshift"]:
         database, schema = get_package_database_and_schema(dbt_project)
     else:
         database = schema = None
 
     node = {
-        'resource_type': "dummy_type_not_incremental",
-        'alias': alias_table_name,
-        'database': database,
-        'schema': schema
+        "resource_type": "dummy_type_not_incremental",
+        "alias": alias_table_name,
+        "database": database,
+        "schema": schema,
     }
 
-    query = dbt_project.execute_macro("elementary.get_anomaly_scores_query",
-                                      test_metrics_table_relation=my_test_metrics_relation,
-                                      model_graph_node=node,  # the table the test has ran on / is monitoring.
-                                      sensitivity=3,  # for z-score (3 sigmas is the default)
-                                      backfill_days=2,  # should be removed from the macro since it's not used TODO
-                                      monitors=[metric_name],  # a list of strings that should include "metric_name" as well.
-                                                      # in code it's used to calc a bunch of metrics "all at once" on 1
-                                                      # table, but in tests I think it makes sense to have 1 at a time.
-                                      column_name=column_name,  # should be the same as in the input
-                                      columns_only=columns_only,  # if True, don't do table-level anomalies
-                                      dimensions=dimensions,  # should be the same as in the input
-                                      metric_properties=metric_properties,
-                                      data_monitoring_metrics_table=my_test_data_monitoring_metrics)
+    query = dbt_project.execute_macro(
+        "elementary.get_anomaly_scores_query",
+        test_metrics_table_relation=my_test_metrics_relation,
+        model_graph_node=node,  # the table the test has ran on / is monitoring.
+        sensitivity=3,  # for z-score (3 sigmas is the default)
+        backfill_days=2,  # should be removed from the macro since it's not used TODO
+        monitors=[
+            metric_name
+        ],  # a list of strings that should include "metric_name" as well.
+        # in code it's used to calc a bunch of metrics "all at once" on 1
+        # table, but in tests I think it makes sense to have 1 at a time.
+        column_name=column_name,  # should be the same as in the input
+        columns_only=columns_only,  # if True, don't do table-level anomalies
+        dimensions=dimensions,  # should be the same as in the input
+        metric_properties=metric_properties,
+        data_monitoring_metrics_table=my_test_data_monitoring_metrics,
+    )
     res_table = dbt_project.execute_sql(query)
     res_table = lowercase_column_names(res_table)
     res_df = agate_table_to_pandas_dataframe(res_table)
@@ -149,9 +194,26 @@ def test_anomaly_scores_query(dbt_project: DbtProject,
         expected_res_df = pd.read_csv(expected_output)
     expected_res_df = expected_res_df.fillna("")
     res_df = res_df.fillna("")
-    assert_dfs_equal(expected_res_df,
-                     res_df,
-                     columns_to_ignore=['detected_at','id','test_execution_id','test_unique_id'],
-                     column_to_index_by='bucket_start',
-                     datetime_columns=['bucket_start','bucket_end','detected_at','training_start','training_end'],
-                     numeric_columns = ['anomaly_score', 'anomaly_score_threshold','metric_value','min_metric_value','max_metric_value', 'training_avg','training_stddev','training_set_size'])
+    assert_dfs_equal(
+        expected_res_df,
+        res_df,
+        columns_to_ignore=["detected_at", "id", "test_execution_id", "test_unique_id"],
+        column_to_index_by="bucket_start",
+        datetime_columns=[
+            "bucket_start",
+            "bucket_end",
+            "detected_at",
+            "training_start",
+            "training_end",
+        ],
+        numeric_columns=[
+            "anomaly_score",
+            "anomaly_score_threshold",
+            "metric_value",
+            "min_metric_value",
+            "max_metric_value",
+            "training_avg",
+            "training_stddev",
+            "training_set_size",
+        ],
+    )
