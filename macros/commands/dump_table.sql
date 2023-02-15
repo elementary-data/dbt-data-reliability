158c98341df5334fcc07182a88700c18752f485e
Elon Gliksberg <elongliks@gmail.com>
2023-02-15 18:24:28 +0200

    Added dump_table macro.

---
+++ macros/commands/dump_table.sql
@@ -0,0 +1,16 @@
+{% macro dump_table(model_unique_id, output_path, since=none, days_back=7) %}
+    {% set node = graph.nodes[model_unique_id] %}
+    {% set timestamp_column = node.meta.timestamp_column %}
+    {% set query %}
+        select * from {{ node.relation_name }}
+        {% if timestamp_column %}
+            {% if since %}
+                where {{ timestamp_column }} > {{ elementary.edr_cast_as_timestamp(elementary.quote(since)) }}
+            {% else %}
+                where {{ elementary.edr_datediff(timestamp_column, elementary.edr_current_timestamp(), 'day') }} < {{ days_back }}
+            {% endif %}
+        {% endif %}
+    {% endset %}
+    {% set results = elementary.run_query(query) %}
+    {% do results.to_csv(output_path) %}
+{% endmacro %}
