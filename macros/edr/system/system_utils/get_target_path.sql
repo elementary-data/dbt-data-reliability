0fc351c71cabb15e592f7d3853b883d6a15a116b
Elon Gliksberg <elongliks@gmail.com>
2023-02-23 13:09:20 +0200

    Code cleanup.

--- macros/edr/system/system_utils/get_target_path.sql
+++
@@ -1,9 +0,0 @@
-{% macro get_target_path(identifier=none) %}
-  {% set runtime_conf = elementary.get_runtime_config() %}
-  {% set elementary_target_path = flags.Path(runtime_conf.target_path) / 'elementary' %}
-  {% if identifier %}
-    {{ return(elementary_target_path / identifier) }}
-  {% else %}
-    {{ return(elementary_target_path) }}
-  {% endif %}
-{% endmacro %}
--- macros/edr/system/system_utils/not_edr_run.sql
+++
@@ -1,7 +0,0 @@
-{% macro not_edr_run() %}
-    {%- if elementary.get_config_var('edr_run') %}
-        {{ return(false) }}
-    {%- else %}
-        {{ return(true) }}
-    {%- endif %}
-{% endmacro %}
