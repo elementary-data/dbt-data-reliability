453347815dbba4acfb70cd33f9e5c0019bb6cc90
Elon Gliksberg <elongliks@gmail.com>
2023-02-28 16:16:34 +0200

    Avoid using dict as a default parameter.

--- macros/utils/log_macro_results.sql
+++ macros/utils/log_macro_results.sql
@@ -1,4 +1,7 @@
-{% macro log_macro_results(macro_name, macro_args = {}) %}
+{% macro log_macro_results(macro_name, macro_args=none) %}
+    {% if macro_args is none %}
+        {% set macro_args = {} %}
+    {% endif %}
     {% set results = context[macro_name](**macro_args) %}
     {% do elementary.edr_log(tojson(results)) %}
 {% endmacro %}
