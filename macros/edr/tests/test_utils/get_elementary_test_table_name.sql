c5ae3ec48d28e1d174fb1fa6cc54fb58dd8be359
go006521 <goffredo.lepori@bis.org>
2023-03-14 15:24:08 +0100

    workaround for length > 128

--- macros/edr/tests/test_utils/get_elementary_test_table_name.sql
+++ macros/edr/tests/test_utils/get_elementary_test_table_name.sql
@@ -1,6 +1,20 @@
-{% macro get_elementary_test_table_name() %}
+{% macro get_elementary_test_table_name() -%}
+    {{ return(adapter.dispatch('get_elementary_test_table_name', 'elementary') ()) }}
+{%- endmacro %}
+
+{% macro default__get_elementary_test_table_name() %}
     {% set test_node = model %}
     {% set test_hash = test_node.unique_id.split(".")[-1] %}
     {% set test_name = test_node.name %}
-    {% do return("test_{}_{}".format(test_hash, test_name)) %}
+    {% do return('test_{}_{}'.format(test_hash, test_name)) %}
+{% endmacro %}
+
+{% macro sqlserver__get_elementary_test_table_name() %}
+    {% set test_node = model %}
+    {% set test_hash = test_node.unique_id.split(".")[-1] %}
+    {% set test_name = test_node.name %}
+        {%- if 'test_{}_{}'.format(test_hash, test_name)|length > 100 -%}
+            {% set test_name = test_name.split("_")[0:4]|join('_') %}
+        {%- endif -%}
+        {% do return('test_{}_{}'.format(test_hash, test_name)) %}
 {% endmacro %}
