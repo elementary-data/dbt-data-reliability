1534a5a240d73bad22412d1be86859b6d4109bf1
Maayan Salom <maayansalom@gmail.com>
2023-03-16 11:59:31 +0200

    test table not temp (#288)

--- macros/edr/tests/test_utils/create_model_baseline_table.sql
+++ macros/edr/tests/test_utils/create_model_baseline_table.sql
@@ -2,7 +2,7 @@
     {% set empty_table_query = elementary.empty_table([('column_name','string'),('data_type','string')]) %}
     {% set baseline_table_relation = elementary.create_elementary_test_table(database_name, schema_name,
                                                                              test_name | lower, 'schema_baseline',
-                                                                             empty_table_query, is_temp_table=True) %}
+                                                                             empty_table_query, is_temp_table=False) %}
     {% do elementary.insert_rows(baseline_table_relation, baseline_columns, should_commit=True) %}
     {% do return(baseline_table_relation) %}
 {% endmacro %}
