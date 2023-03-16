3180e75b4209e411852d645777e595be0f3f7eaa
Maayan-s <maayansalom@gmail.com>
2023-03-16 17:02:04 +0200

    lower

--- macros/utils/log_macro_results.sql
+++ macros/utils/log_macro_results.sql
@@ -2,6 +2,15 @@
     {% if macro_args is none %}
         {% set macro_args = {} %}
     {% endif %}
-    {% set results = context[macro_name](**macro_args) %}
+    {%- set package_and_macro_name = macro_name.split('.') %}
+    {%- if package_and_macro_name | length == 1 %}
+        {% set macro = context[macro_name] %}
+    {%- elif package_and_macro_name | length == 2 %}
+        {%- set package_name, macro_name = package_and_macro_name %}
+        {% set macro = context[package_name][macro_name] %}
+    {%- else %}
+        {% do exceptions.raise_compiler_error("Received invalid macro name: {}".format(macro_name)) %}
+    {% endif %}
+    {% set results = macro(**macro_args) %}
     {% do elementary.edr_log(tojson(results)) %}
 {% endmacro %}
