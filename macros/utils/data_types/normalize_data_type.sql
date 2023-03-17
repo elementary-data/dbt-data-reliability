0446573a2ea330b1d88bcd5046151250bc7be4fd
Maayan-s <maayansalom@gmail.com>
2023-03-17 14:56:14 +0200

    fix decimal data type databricks

--- macros/utils/data_types/normalize_data_type.sql
+++ macros/utils/data_types/normalize_data_type.sql
@@ -1,4 +1,10 @@
 {% macro normalize_data_type(data_type) %}
+
+    {# decimal data type is in the format decimal(p,s) #}
+    {%- if '(' in data_type %}
+        {%- set data_type = data_type.split('(')[0] %}
+    {%- endif %}
+
     {%- if data_type is defined and data_type is not none %}
         {%- if data_type in elementary.data_type_list('string') %}
             {{ return('string') }}
