286ca755d916142aec560a09924df85ac2568cf4
Maayan-s <maayansalom@gmail.com>
2023-04-01 18:44:27 +0300

    fixes

--- integration_tests/models/test_alerts_union.sql
+++ integration_tests/models/test_alerts_union.sql
@@ -1,11 +1,13 @@
 with dbt as (
     select * from {{ ref('alerts_dbt_tests') }}
 ),
-anomalies as (
-    select * from {{ ref('alerts_anomaly_detection') }}
-),
+{%- if target.type != 'databricks' %}
 schema_changes as (
     select * from {{ ref('alerts_schema_changes') }}
+),
+{%- endif %}
+anomalies as (
+    select * from {{ ref('alerts_anomaly_detection') }}
 )
 select * from dbt
 union all
