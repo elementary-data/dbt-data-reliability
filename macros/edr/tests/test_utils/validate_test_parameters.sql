0e31f0067b3c39c595e6ceb96c9d0b3ec747924e
RoiTabach <roi.tabach@gmail.com>
2023-04-03 10:36:47 +0300

    Print prettier error message in case time bucket is not 1 day

--- macros/edr/tests/test_utils/validate_test_parameters.sql
+++ macros/edr/tests/test_utils/validate_test_parameters.sql
@@ -5,10 +5,10 @@
       {% endif %}
       {% set supported_seasonality_values = ['day_of_week'] %}
       {% if seasonality not in supported_seasonality_values %}
-        {% do exceptions.raise_compiler_error('Seasonality value should be one of' ~ supported_seasonality_values ~ ', got ' ~ seasonality ~ ' instead')%}
+        {% do exceptions.raise_compiler_error('Seasonality value should be one of' ~ supported_seasonality_values ~ ', got ' ~ seasonality ~ ' instead') %}
       {% endif %}
       {% if (time_bucket.count != 1) or (time_bucket.period != 'day') %}
-        {% do exceptions.raise_compiler_error('Seasonality is supported only with time_bucket 1 day, got ' ~ time_bucket ~ ' instead')%}
+        {% do exceptions.raise_compiler_error('Seasonality is supported only with time_bucket 1 day, got period: ' ~ time_bucket.period ~ ' and count: ' ~ time_bucket.count ~ ' instead') %}
       {% endif %}
     {% endif %}
 {% endmacro %}
