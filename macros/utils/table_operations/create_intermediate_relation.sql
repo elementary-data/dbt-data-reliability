a92fdee324d4102ebc50707001187e22129129a4
Elon Gliksberg <elongliks@gmail.com>
2023-04-04 12:35:30 +0300

    Databricks doesn't support temporary tables.

--- macros/utils/table_operations/create_intermediate_relation.sql
+++ macros/utils/table_operations/create_intermediate_relation.sql
@@ -2,6 +2,10 @@
     {% set int_suffix = modules.datetime.datetime.utcnow().strftime('__tmp_%Y%m%d%H%M%S%f') %}
     {% set int_relation = dbt.make_temp_relation(base_relation, suffix=int_suffix).incorporate(type='table') %}
 
+    {% if not elementary.has_temp_table_support() %}
+        {% set temporary = false %}
+    {% endif %}
+
     {% do elementary.create_table_like(int_relation, base_relation, temporary, like_columns) %}
     {% do elementary.insert_rows(int_relation, rows, should_commit=false, chunk_size=elementary.get_config_var('dbt_artifacts_chunk_size')) %}
     {% do return(int_relation) %}
---
+++ macros/utils/table_operations/has_temp_table_support.sql
@@ -0,0 +1,11 @@
+{% macro has_temp_table_support() %}
+    {% do return(adapter.dispatch("has_temp_table_support", "elementary")()) %}
+{% endmacro %}
+
+{% macro default__has_temp_table_support() %}
+    {% do return(true) %}
+{% endmacro %}
+
+{% macro spark__has_temp_table_support() %}
+    {% do return(false) %}
+{% endmacro %}
