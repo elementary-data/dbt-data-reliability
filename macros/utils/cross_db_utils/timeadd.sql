d69d4e58985feb7fb68d0a3c05d06de2c9945f5c
Maayan-s <maayansalom@gmail.com>
2023-04-10 12:22:08 +0300

    bq fixes

--- macros/utils/cross_db_utils/timeadd.sql
+++ macros/utils/cross_db_utils/timeadd.sql
@@ -5,14 +5,14 @@
 
 {# Snowflake #}
 {% macro default__edr_timeadd(date_part, number, timestamp_expression) %}
-    dateadd({{ date_part }}, {{ number }}, {{ elementary.edr_cast_as_timestamp(timestamp_expression) }})
+    dateadd({{ date_part }}, {{ elementary.edr_cast_as_int(number) }}, {{ elementary.edr_cast_as_timestamp(timestamp_expression) }})
 {% endmacro %}
 
 {% macro bigquery__edr_timeadd(date_part, number, timestamp_expression) %}
     {%- if date_part | lower in ['second', 'minute', 'hour', 'day'] %}
-       timestamp_add({{ elementary.edr_cast_as_timestamp(timestamp_expression) }}, INTERVAL {{ number }} {{ date_part }})
+       timestamp_add({{ elementary.edr_cast_as_timestamp(timestamp_expression) }}, INTERVAL {{ elementary.edr_cast_as_int(number) }} {{ date_part }})
     {%- elif date_part | lower in ['week', 'month', 'quarter', 'year'] %}
-       date_add({{ elementary.edr_cast_as_date(timestamp_expression) }}, INTERVAL {{ number }} {{ date_part }})
+       date_add({{ elementary.edr_cast_as_date(timestamp_expression) }}, INTERVAL {{ elementary.edr_cast_as_int(number) }} {{ date_part }})
     {%- else %}
         {{ exceptions.raise_compiler_error("Unsupported date_part in edr_timeadd: ".format(date_part)) }}
     {%- endif %}
