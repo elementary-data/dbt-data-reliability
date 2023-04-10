e3a1dc59fd92ea4af0667547f89e2a488ce9b39d
Maayan-s <maayansalom@gmail.com>
2023-04-10 14:20:26 +0300

    fix

--- macros/edr/tests/test_utils/find_normalized_data_type_for_column.sql
+++ macros/edr/tests/test_utils/find_normalized_data_type_for_column.sql
@@ -1,7 +1,7 @@
 {% macro find_normalized_data_type_for_column(model_relation, column_name) %}
 
 {% set columns_from_relation = adapter.get_columns_in_relation(model_relation) %}
-{% if columns_from_relation and columns_from_relation is iterable %}
+{% if column_name and columns_from_relation and columns_from_relation is iterable %}
     {% for column_obj in columns_from_relation %}
         {% if column_obj.column | lower == column_name | trim('\'\"\`') | lower %}
             {{ return(elementary.normalize_data_type(column_obj.dtype)) }}
