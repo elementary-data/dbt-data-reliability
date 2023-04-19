2b0e938412d68bcf0ce2d32ddf3c3236a35a533f
RoiTabach <roi.tabach@gmail.com>
2023-04-19 17:25:52 +0300

    simplify code

--- macros/edr/materializations/tests/test.sql
+++ macros/edr/materializations/tests/test.sql
@@ -1,12 +1,6 @@
 {% macro query_test_result_rows(sample_limit=none) %}
   {% if sample_limit == 0 %}
-    {% set cache_key = "query_test_results_rows_sample_limit_0" %}
-    {% set cached_empty_test_results_rows = elementary.get_cache("elementary_test_results").get(cache_key) %}
-    {% if cached_empty_test_results_rows is none %} {# update the cache #}
-      {% set cached_empty_test_results_rows = elementary.agate_to_dicts(elementary.run_query(elementary.empty_elementary_test_results())) %}
-      {% do elementary.get_cache("elementary_test_results").update({cache_key: cached_empty_test_results_rows}) %}
-    {% endif %}
-    {% do return(cached_empty_test_results_rows) %}
+    {% do return([]) %}
   {% endif %}
   {% set query %}
     with test_results as (
