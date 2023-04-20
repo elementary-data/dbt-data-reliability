978db32a18f4864dcc80e4638f5f8ae9a346a84d
Noy Arie <noyarie1992@gmail.com>
2023-04-20 18:02:25 +0300

    use new macros

--- models/edr/metadata_store/filtered_information_schema_columns.sql
+++ models/edr/metadata_store/filtered_information_schema_columns.sql
@@ -5,16 +5,10 @@
   )
 }}
 
-{% set configured_schemas = elementary.get_configured_schemas_from_graph() %}
+{% set configured_databases = elementary.get_configured_databases_from_graph() %}
 
 with filtered_information_schema_columns as (
-
-    {%- if configured_schemas | length > 0 -%}
-        {{ elementary.union_macro_queries(configured_schemas, elementary.get_columns_from_information_schema) }}
-    {%- else %}
-        {{ elementary.empty_table([('full_table_name', 'string'), ('database_name', 'string'), ('schema_name', 'string'), ('table_name', 'string'), ('column_name', 'string'), ('data_type', 'string')]) }}
-    {%- endif %}
-
+    {{ elementary.get_columns_by_configured_databases(configured_databases) }}
 )
 
 select *
