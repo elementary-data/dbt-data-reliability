3a3f3e5b04e34f7601db97294d1f9a1af81fb590
Noy Arie <noyarie1992@gmail.com>
2023-04-23 08:26:18 +0300

    add dbt_columns

---
+++ models/edr/dbt_artifacts/dbt_columns.sql
@@ -0,0 +1,28 @@
+{{
+  config(
+    materialized = 'view',
+    bind=False,
+    enabled = target.type != 'databricks' and target.type != 'spark' | as_bool()
+  )
+}}
+
+with dbt_models_data as (
+    select * from {{ ref('dbt_models') }}
+),
+
+schemas_information as (
+    {{ elementary.get_columns_in_project() }}
+),
+
+dbt_columns as (
+    select col_info.*
+    from dbt_models_data models
+    join schemas_information col_info
+        on (lower(models.database_name) = lower(col_info.database_name) and
+            lower(models.schema_name) = lower(col_info.schema_name) and
+            lower(models.name) = lower(col_info.table_name)
+        )
+)
+
+select *
+from dbt_columns
