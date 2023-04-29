8b41151006b6dbdc54a20f72a60e2778e2c52abe
Maayan-s <maayansalom@gmail.com>
2023-04-29 15:58:27 +0300

    added default to time_bucket

--- macros/edr/data_monitoring/data_monitors_configuration/time_bucket.sql
+++ macros/edr/data_monitoring/data_monitors_configuration/time_bucket.sql
@@ -5,3 +5,18 @@
 {% macro get_default_time_bucket() %}
   {% do return(elementary.get_daily_time_bucket()) %}
 {% endmacro %}
+
+{% macro get_time_bucket(time_bucket, model_node) %}
+    {%- set configured_time_bucket = elementary.get_test_argument('time_bucket', time_bucket, model_node) %}
+    {%- set default_time_bucket = elementary.get_default_time_bucket() %}
+
+    {%- if not configured_time_bucket %}
+        {{ return(default_time_bucket) }}
+    {%- elif configured_time_bucket and not configured_time_bucket.period %}
+        {%- do configured_time_bucket.update({"period": default_time_bucket.period }) -%}
+    {%- elif configured_time_bucket and not configured_time_bucket.count %}
+        {%- do configured_time_bucket.update({"count": default_time_bucket.count }) %}
+    {%- endif %}
+    {{ return(configured_time_bucket) }}
+{% endmacro %}
+
