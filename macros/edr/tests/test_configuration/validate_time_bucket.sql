7924302dde10769e8f8f840a1434c8c276aeae61
Maayan-s <maayansalom@gmail.com>
2023-04-29 17:32:36 +0300

    default time_bucket and validation

--- macros/edr/tests/test_configuration/validate_time_bucket.sql
+++ macros/edr/tests/test_configuration/validate_time_bucket.sql
@@ -1,11 +1,14 @@
 {% macro validate_time_bucket(time_bucket) %}
+-- TODO: not mapping or mapping with other keys
     {% if time_bucket %}
-      {% if time_bucket.count and time_bucket.count is not integer %}
-        {% do exceptions.raise_compiler_error("time_bucket.count expectes valid integer, got: {} (If it's an integer, try to remove quotes)".format(time_bucket.count)) %}
-      {% endif %}
-      {% set supported_periods = ['hour','day','week','month'] %}
-      {% if time_bucket.period and time_bucket.period not in supported_periods %}
-        {% do exceptions.raise_compiler_error("time_bucket.period value should be one of {0}, got: {1}".format(supported_periods, time_bucket.period)) %}
-      {% endif %}
+      {%- if time_bucket is mapping %}
+        {% if time_bucket.count and time_bucket.count is not integer %}
+            {% do exceptions.raise_compiler_error("time_bucket.count expectes valid integer, got: {} (If it's an integer, try to remove quotes)".format(time_bucket.count)) %}
+        {% endif %}
+        {% set supported_periods = ['hour','day','week','month'] %}
+        {% if time_bucket.period and time_bucket.period not in supported_periods %}
+            {% do exceptions.raise_compiler_error("time_bucket.period value should be one of {0}, got: {1}".format(supported_periods, time_bucket.period)) %}
+        {% endif %}
+      {%- endif %}
     {% endif %}
 {% endmacro %}
