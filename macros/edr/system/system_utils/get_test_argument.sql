6ea071602b968e6e70d8569a9a5462758aecd29b
Maayan-s <maayansalom@gmail.com>
2023-04-29 15:58:27 +0300

    added default to time_bucket

--- macros/edr/system/system_utils/get_test_argument.sql
+++ macros/edr/system/system_utils/get_test_argument.sql
@@ -1,6 +1,11 @@
-{% macro get_test_argument(argument_name, value=none) %}
+{% macro get_test_argument(argument_name, value=none, model_node=none) %}
   {% if value %}
     {{ return(value) }}
+  {%- elif model_node %}
+    {% set elementary_config = elementary.get_elementary_config_from_node(model_node) %}
+    {% if elementary_config and elementary_config is mapping %}
+        {{ return(elementary_config.get(argument_name)) }}
+    {% endif %}
   {% else %}
     {{ return(elementary.get_config_var(argument_name)) }}
   {% endif %}
