81640f96424123ea31c9a41f9f993d19176e0de0
Maayan-s <maayansalom@gmail.com>
2023-04-29 17:51:34 +0300

    fix

--- macros/edr/tests/test_configuration/validate_time_bucket.sql
+++ macros/edr/tests/test_configuration/validate_time_bucket.sql
@@ -1,7 +1,37 @@
 {% macro validate_time_bucket(time_bucket) %}
--- TODO: not mapping or mapping with other keys
     {% if time_bucket %}
-      {%- if time_bucket is mapping %}
+        {%- if time_bucket is not mapping %}
+            {% do exceptions.raise_compiler_error(
+            "
+            Invalid time_bucket format. Expected format:
+
+               time_bucket:
+                 count: int
+                 period: string
+            ") %}
+        {%- endif %}
+        {%- if time_bucket is mapping %}
+            {%- set invalid_keys = [] %}
+            {%- set valid_keys = ['period', 'count'] %}
+            {%- for key, value in time_bucket.items() %}
+                {%- if key not in ['period', 'count'] %}
+                    {%- do invalid_keys.append(key) -%}
+                {%- endif %}
+            {%- endfor %}
+            {%- if invalid_keys | length > 0 %}
+                {% do exceptions.raise_compiler_error(
+                ("
+                Found invalid keys in time_bucket: {0}.
+                Supported keys: {1}.
+                Expected format:
+
+                   time_bucket:
+                     count: int
+                     period: string
+                ").format(invalid_keys, valid_keys)) %}
+            {%- endif %}
+        {%- endif %}
+
         {% if time_bucket.count and time_bucket.count is not integer %}
             {% do exceptions.raise_compiler_error("time_bucket.count expectes valid integer, got: {} (If it's an integer, try to remove quotes)".format(time_bucket.count)) %}
         {% endif %}
@@ -9,6 +39,5 @@
         {% if time_bucket.period and time_bucket.period not in supported_periods %}
             {% do exceptions.raise_compiler_error("time_bucket.period value should be one of {0}, got: {1}".format(supported_periods, time_bucket.period)) %}
         {% endif %}
-      {%- endif %}
     {% endif %}
 {% endmacro %}
