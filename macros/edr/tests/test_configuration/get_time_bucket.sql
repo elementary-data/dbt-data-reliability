c6364078abd7d948536a24a161238c1e437a3fd7
Maayan-s <maayansalom@gmail.com>
2023-04-30 11:11:06 +0300

    Validation time.bucket

--- macros/edr/tests/test_configuration/get_time_bucket.sql
+++ macros/edr/tests/test_configuration/get_time_bucket.sql
@@ -57,6 +57,9 @@
         {% if time_bucket.count and time_bucket.count is not integer %}
             {% do exceptions.raise_compiler_error("time_bucket.count expects valid integer, got: {} (If it's an integer, try to remove quotes)".format(time_bucket.count)) %}
         {% endif %}
+        {% if time_bucket.count <= 0 %}
+            {% do exceptions.raise_compiler_error("time_bucket.count has to be larger than 0, got: {})".format(time_bucket.count)) %}
+        {% endif %}
         {% set supported_periods = ['hour','day','week','month'] %}
         {% if time_bucket.period and time_bucket.period not in supported_periods %}
             {% do exceptions.raise_compiler_error("time_bucket.period value should be one of {0}, got: {1}".format(supported_periods, time_bucket.period)) %}
