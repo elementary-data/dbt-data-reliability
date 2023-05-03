66aee8b929f0f77c04e39fe670a35a0e7841a4ab
Maayan-s <maayansalom@gmail.com>
2023-05-03 10:45:36 +0300

    WIP

--- macros/edr/tests/test_configuration/get_anomalies_test_configuration.sql
+++ macros/edr/tests/test_configuration/get_anomalies_test_configuration.sql
@@ -1,4 +1,4 @@
-{% macro get_anomalies_test_configuration(model_relation, timestamp_column, where_expression, time_bucket, anomaly_sensitivity, anomaly_direction) %}
+{% macro get_anomalies_test_configuration(model_relation, timestamp_column, where_expression, time_bucket, anomaly_sensitivity, anomaly_direction, days_back, backfill_days) %}
     {%- set model_graph_node = elementary.get_model_graph_node(model_relation) %}
 
     {# All anomaly detection tests #}
@@ -9,8 +9,21 @@
 
     {# timestamp_column anomaly detection tests #}
     {%- set time_bucket = elementary.get_time_bucket(time_bucket, model_graph_node) %}
+    {%- set days_back = elementary.get_days_back(days_back, model_graph_node, seasonality) %}
+    {%- set backfill_days = elementary.get_test_argument('backfill_days', backfill_days, model_graph_node) %}
 
-    {{ return([timestamp_column, time_bucket, where_expression, anomaly_sensitivity, anomaly_direction]) }}
+-- TODO:
+-- Add days_back and backfill_days to the test on schema.yml
+-- Add validation for backfill days and sensitivity?
+
+
+    {{ return([timestamp_column,
+               time_bucket,
+               where_expression,
+               anomaly_sensitivity,
+               anomaly_direction,
+               days_back,
+               backfill_days]) }}
 {% endmacro %}
 
 
