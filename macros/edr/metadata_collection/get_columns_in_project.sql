ae751ee79c397d73ea9a5551faf8650123617381
Noy Arie <noyarie1992@gmail.com>
2023-05-03 11:05:01 +0300

    use new logic to get schemas

--- macros/edr/metadata_collection/get_columns_in_project.sql
+++ macros/edr/metadata_collection/get_columns_in_project.sql
@@ -1,20 +1,4 @@
 {% macro get_columns_in_project() %}
-    {% set configured_schemas = elementary.get_project_configured_schemas() %}
+    {% set configured_schemas = elementary.get_configured_schemas_from_graph() %}
     {{ elementary.get_columns_by_configured_schemas(configured_schemas) }}
-{% endmacro %}
-
-{% macro get_project_configured_schemas() %}
-    {% set configured_schemas = [] %}
-    {% if execute %}
-        {% set root_project = context["project_name"] %}
-        {% set nodes = elementary.get_nodes_from_graph() %}
-        {% for node in nodes %}
-            {% if node["package_name"] == root_project %}
-                {% if adapter.check_schema_exists(node['database'], node['schema']) %}
-                    {% do configured_schemas.append((node['database'], node['schema'])) %}
-                {% endif %}
-            {% endif %}
-        {% endfor %}
-    {% endif %}
-    {{ return(configured_schemas | unique | list ) }}
 {% endmacro %}
