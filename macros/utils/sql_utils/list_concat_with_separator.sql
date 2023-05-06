16a5def0f93faf7c4366992ea1401b11060396ad
Maayan Salom <maayansalom@gmail.com>
2023-05-06 15:27:16 +0300

    bug fix (#344)

--- macros/utils/sql_utils/list_concat_with_separator.sql
+++ macros/utils/sql_utils/list_concat_with_separator.sql
@@ -1,9 +1,9 @@
 {% macro list_concat_with_separator(item_list, separator, handle_nulls = true) %}
     {% set new_list = [] %}
     {% for item in item_list %}
-        {% set new_item = item %}
+        {% set new_item = elementary.edr_quote(item) %}
         {% if handle_nulls %}
-            {% set new_item = "case when " ~ elementary.edr_cast_as_string(item) ~ " is null then 'NULL' else " ~ elementary.edr_cast_as_string(item) ~ " end" %}
+            {% set new_item = "case when " ~ elementary.edr_cast_as_string(elementary.edr_quote(item)) ~ " is null then 'NULL' else " ~ elementary.edr_cast_as_string(elementary.edr_quote(item)) ~ " end" %}
         {% endif %}
         {% do new_list.append(new_item) %}
         {% if not loop.last %}
