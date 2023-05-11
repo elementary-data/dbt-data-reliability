70c17ed71aff1015c9b18e6effbde82a9688765f
Elon Gliksberg <elongliks@gmail.com>
2023-05-11 14:06:12 +0300

    Updated the macro.

--- macros/utils/table_operations/make_temp_relation.sql
+++ macros/utils/table_operations/make_temp_relation.sql
@@ -2,8 +2,19 @@
     {% if not suffix %}
         {% set suffix = modules.datetime.datetime.utcnow().strftime('__tmp_%Y%m%d%H%M%S%f') %}
     {% endif %}
+    {% do return(adapter.dispatch("make_temp_relation", "elementary")(base_relation, suffix)) %}
+{% endmacro %}
 
-    {% set temp_identifier = base_relation.identifier ~ suffix %}
-    {% set temp_relation = base_relation.incorporate(path={"identifier": temp_identifier}) %}
-    {{ return(temp_relation) }}
+{% macro default__make_temp_relation(base_relation, suffix) %}
+    {% do return(dbt.make_temp_relation(base_relation, suffix)) %}
+{% endmacro %}
+
+{% macro databricks__make_temp_relation(base_relation, suffix) %}
+    {% set tmp_identifier = base_relation.identifier ~ suffix %}
+    {% set tmp_relation = api.Relation.create(
+        identifier=tmp_identifier,
+        schema=base_relation.schema,
+        database=base_relation.database,
+        type='table') %}
+    {% do return(tmp_relation) %}
 {% endmacro %}
