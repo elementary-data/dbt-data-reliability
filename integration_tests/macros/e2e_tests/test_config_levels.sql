a32eba0351193b8b697268e0a1f2c3764f1cc9f7
Maayan-s <maayansalom@gmail.com>
2023-05-12 15:19:58 +0300

    params at different levels

--- integration_tests/macros/e2e_tests/test_config_levels.sql
+++ integration_tests/macros/e2e_tests/test_config_levels.sql
@@ -1,14 +1,38 @@
-{% test config_levels(model, expected_config, time_bucket) %}
+{% test config_levels(model, expected_config, timestamp_column, time_bucket, where_expression, anomaly_sensitivity, anomaly_direction, days_back, backfill_days, seasonality, min_training_set_size) %}
     {%- if execute and flags.WHICH in ['test', 'build'] %}
         {%- set unexpected_config = [] %}
         {%- set model_relation = dbt.load_relation(model) %}
-        {% set model_graph_node = elementary.get_model_graph_node(model_relation) %}
 
-        {%- set time_bucket = elementary.get_time_bucket(time_bucket, model_graph_node) %}
-        {%- if time_bucket != expected_config.get('time_bucket') %}
-            {%- set unexpected_message = ('got config: {0}, expected config: {1}').format(time_bucket, expected_config.get('time_bucket') ) %}
-            {%- do unexpected_config.append(unexpected_message) -%}
-        {%- endif %}
+        {% set configuration_dict, metric_properties_dict =
+               elementary.get_anomalies_test_configuration(model_relation,
+                                                           timestamp_column,
+                                                           where_expression,
+                                                           anomaly_sensitivity,
+                                                           anomaly_direction,
+                                                           min_training_set_size,
+                                                           time_bucket,
+                                                           days_back,
+                                                           backfill_days,
+                                                           seasonality) %}
+
+        {%- set configs_to_test = [('timestamp_column', metric_properties_dict.timestamp_column),
+                                   ('where_expression', metric_properties_dict.where_expression),
+                                   ('time_bucket', configuration_dict.time_bucket),
+                                   ('anomaly_sensitivity', configuration_dict.anomaly_sensitivity),
+                                   ('anomaly_direction', configuration_dict.anomaly_direction),
+                                   ('min_training_set_size', configuration_dict.min_training_set_size),
+                                   ('days_back', configuration_dict.days_back),
+                                   ('backfill_days', configuration_dict.backfill_days),
+                                   ('seasonality', configuration_dict.seasonality)
+                                   ] %}
+
+        {%- for config in configs_to_test %}
+            {%- set config_name, config_value = config %}
+            {%- set config_check = compare_configs(config_name, config_value, expected_config) %}
+            {%- if config_check %}
+                {%- do unexpected_config.append(config_check) -%}
+            {%- endif %}
+        {%- endfor %}
 
         {%- if unexpected_config | length > 0 %}
             {%- do exceptions.raise_compiler_error('Failure config_levels: ' ~ unexpected_config) -%}
@@ -18,3 +42,11 @@
         {%- endif %}
     {%- endif %}
 {%- endtest %}
+
+{% macro compare_configs(config_name, config, expected_config) %}
+    {%- if config != expected_config.get(config_name) %}
+        {%- set unexpected_message = ('For {0} - got config: {1}, expected config: {2}').format(config_name, config, expected_config.get(config_name) ) %}
+        {{ return(unexpected_message) }}
+    {%- endif %}
+    {{ return(none) }}
+{% endmacro %}
---
+++ macros/edr/tests/test_configuration/get_anomalies_test_configuration.sql
@@ -0,0 +1,57 @@
+-- TODO: Add validation for backfill days, sensitivity and min_training_size?
+-- TODO: Add min_training_set_size to tests and use it
+
+{% macro get_anomalies_test_configuration(model_relation,
+                                          timestamp_column,
+                                          where_expression,
+                                          anomaly_sensitivity,
+                                          anomaly_direction,
+                                          min_training_set_size,
+                                          time_bucket,
+                                          days_back,
+                                          backfill_days,
+                                          seasonality,
+                                          freshness_column,
+                                          event_timestamp_column,
+                                          dimensions) %}
+
+    {%- set model_graph_node = elementary.get_model_graph_node(model_relation) %}
+
+    {# All anomaly detection tests #}
+    {%- set timestamp_column = elementary.get_timestamp_column(timestamp_column, model_graph_node, model_relation) %}
+    {%- set where_expression = elementary.get_test_argument('where_expression', where_expression, model_graph_node) %}
+    {%- set anomaly_sensitivity = elementary.get_test_argument('anomaly_sensitivity', anomaly_sensitivity, model_graph_node) %}
+    {%- set anomaly_direction = elementary.get_anomaly_direction(anomaly_direction, model_graph_node) %}
+    {%- set min_training_set_size = elementary.get_test_argument('min_training_set_size', min_training_set_size, model_graph_node) %}
+
+    {# timestamp_column anomaly detection tests #}
+    {%- set time_bucket = elementary.get_time_bucket(time_bucket, model_graph_node) %}
+    {%- set days_back = elementary.get_days_back(days_back, model_graph_node, seasonality) %}
+    {%- set backfill_days = elementary.get_test_argument('backfill_days', backfill_days, model_graph_node) %}
+    {%- set seasonality = elementary.get_seasonality(seasonality, model_graph_node, time_bucket, timestamp_column) %}
+
+    {% set anomalies_test_configuration_dict =
+      {'timestamp_column': (timestamp_column if timestamp_column else None),
+       'where_expression': (where_expression if where_expression else None),
+       'anomaly_sensitivity': (anomaly_sensitivity if anomaly_sensitivity else None),
+       'anomaly_direction': (anomaly_direction if anomaly_direction else None),
+       'min_training_set_size': (min_training_set_size if min_training_set_size else None),
+       'time_bucket': (time_bucket if time_bucket else None) ,
+       'days_back': (days_back if days_back else None) ,
+       'backfill_days':(backfill_days if backfill_days else None),
+       'seasonality':(seasonality if seasonality else None)
+        } %}
+
+  {# Changes in these configs impact the metric id of the test. #}
+  {# If these configs change, we ignore the old metrics and recalculate. #}
+    {% set metric_properties_dict =
+      {'timestamp_column': (timestamp_column if timestamp_column else None) ,
+       'where_expression': (where_expression if where_expression else None) ,
+       'time_bucket': (time_bucket if time_bucket else None),
+       'freshness_column': (freshness_column if freshness_column else None) ,
+       'event_timestamp_column':(event_timestamp_column if event_timestamp_column else None),
+       'dimensions':(dimensions if dimensions else None)
+        } %}
+
+    {{ return([anomalies_test_configuration_dict, metric_properties_dict]) }}
+{% endmacro %}
