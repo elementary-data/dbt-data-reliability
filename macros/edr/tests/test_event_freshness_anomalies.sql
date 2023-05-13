860b870f2667b830089125ee1988f451d7a540ea
Maayan-s <maayansalom@gmail.com>
2023-05-13 10:11:32 +0300

    fixes

--- macros/edr/tests/test_event_freshness_anomalies.sql
+++ macros/edr/tests/test_event_freshness_anomalies.sql
@@ -18,7 +18,7 @@
       where_expression=where_expression,
       anomaly_sensitivity=anomaly_sensitivity,
       min_training_set_size=min_training_set_size,
-      time_bucket=time_bucket
+      time_bucket=time_bucket,
       days_back=days_back,
       backfill_days=backfill_days,
       event_timestamp_column=event_timestamp_column
