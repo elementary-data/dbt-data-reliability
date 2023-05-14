133ed73e7218b7999793d4ba11d6efe377437fc1
Maayan Salom <maayansalom@gmail.com>
2023-05-14 15:08:50 +0300

    Ele 700 add default values to time bucket  (#346)
    * added default to time_bucket
    
    * default time_bucket and validation
    
    * default time_bucket and validation
    
    * fix
    
    * fix
    
    * added e2e
    
    * added e2e
    
    * remove debug
    
    * format
    
    * PR fixes

--- macros/edr/data_monitoring/data_monitors_configuration/time_bucket.sql
+++
@@ -1,7 +0,0 @@
-{% macro get_daily_time_bucket() %}
-  {% do return({"period": "day", "count": 1}) %}
-{% endmacro %}
-
-{% macro get_default_time_bucket() %}
-  {% do return(elementary.get_daily_time_bucket()) %}
-{% endmacro %}
--- macros/edr/system/system_utils/get_test_argument.sql
+++ macros/edr/system/system_utils/get_test_argument.sql
@@ -1,7 +1,18 @@
-{% macro get_test_argument(argument_name, value=none) %}
+{% macro get_test_argument(argument_name, value, model_graph_node) %}
   {% if value %}
     {{ return(value) }}
-  {% else %}
+  {%- endif %}
+  {%- if model_graph_node %}
+    {% set elementary_config = elementary.get_elementary_config_from_node(model_graph_node) %}
+    {% if elementary_config and elementary_config is mapping %}
+        {%- set model_config_value = elementary_config.get(argument_name) %}
+        {%- if model_config_value %}
+            {{ return(model_config_value) }}
+        {%- endif %}
+    {% endif %}
+  {% endif %}
+  {%- if elementary.get_config_var(argument_name) %}
     {{ return(elementary.get_config_var(argument_name)) }}
   {% endif %}
+  {{ return(none) }}
 {% endmacro %}
