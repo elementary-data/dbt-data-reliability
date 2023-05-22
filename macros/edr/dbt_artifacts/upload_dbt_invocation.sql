44fd7c4823fab08ab891bdec74885fbdad2e5092
noyaarie <noyarie1992@gmail.com>
2023-05-22 15:32:13 +0300

    fix indentation

--- macros/edr/dbt_artifacts/upload_dbt_invocation.sql
+++ macros/edr/dbt_artifacts/upload_dbt_invocation.sql
@@ -137,7 +137,7 @@
       ('command', 'string'),
       ('dbt_version', 'string'),
       ('elementary_version', 'string'),
-	  ('full_refresh', 'boolean'),
+      ('full_refresh', 'boolean'),
       ('invocation_vars', 'long_string'),
       ('vars', 'long_string'),
       ('target_name', 'string'),
