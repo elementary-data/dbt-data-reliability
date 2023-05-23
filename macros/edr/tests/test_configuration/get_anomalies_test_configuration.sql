3775e7b0741c85d1ae3fa39971277f6ea30a2011
Maayan-s <maayansalom@gmail.com>
2023-05-23 12:23:43 +0300

    moved file

--- macros/edr/tests/test_configuration/get_anomalies_test_configuration.sql
+++ macros/edr/tests/test_configuration/get_anomalies_test_configuration.sql
@@ -92,13 +92,3 @@
         {% do exceptions.raise_compiler_error('Missing mandatory configuration: {}'.format(missing_mandatory_params)) %}
     {%- endif %}
 {% endmacro %}
-
-
-{% macro empty_dict_keys_to_none(dict) %}
-    {% for key in dict %}
-        {% if not dict[key] %}
-            {% do dict.update({key: none}) %}
-        {% endif %}
-    {% endfor %}
-    {{ return(dict) }}
-{% endmacro %}
