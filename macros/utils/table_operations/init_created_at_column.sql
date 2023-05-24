95c06a8c96d39d712394f0ff8b6c5eb7ff5248af
Elon Gliksberg <elongliks@gmail.com>
2023-05-24 14:29:28 +0300

    Updated the created_at column creation method.

--- macros/utils/table_operations/init_created_at_column.sql
+++ macros/utils/table_operations/init_created_at_column.sql
@@ -8,9 +8,38 @@
         {% do return('') %}
     {% endif %}
 
-    {% set add_created_at_column_query %}
+    {% do elementary.create_created_at_column() %}
+{% endmacro %}
+
+
+{% macro create_created_at_column() %}
+    {% do adapter.dispatch("create_created_at_column", "elementary")() %}
+{% endmacro %}
+
+{% macro default__create_created_at_column() %}
+    {% set query %}
         ALTER TABLE {{ this }}
         ADD COLUMN created_at {{ elementary.edr_type_timestamp() }} DEFAULT {{ elementary.edr_current_timestamp() }};
     {% endset %}
-    {% do elementary.run_query(add_created_at_column_query) %}
+    {% do elementary.run_query(query) %}
+{% endmacro %}
+
+{% macro snowflake__create_created_at_column() %}
+    {% do exceptions.raise_compiler_error("Snowflake is unsupported.") %}
+{% endmacro %}
+
+{% macro bigquery__create_created_at_column() %}
+    {% do elementary.add_and_alter_created_at_column() %}
+{% endmacro %}
+
+{% macro spark__create_created_at_column() %}
+    {% do elementary.add_and_alter_created_at_column() %}
+{% endmacro %}
+
+{% macro add_and_alter_created_at_column() %}
+    {% set query %}
+        ALTER TABLE {{ this }} ADD COLUMN created_at;
+        ALTER TABLE {{ this }} ALTER COLUMN created_at SET DEFAULT {{ elementary.edr_current_timestamp() }};
+    {% endset %}
+    {% do elementary.run_query(query) %}
 {% endmacro %}
