6a9ae6354f064a13f1ec1d205e51c73a59c1b0ef
Artem Garmash <artem.garmash@ouraring.com>
2023-06-08 10:22:23 +0300

    Athena delete and insert

--- macros/utils/table_operations/delete_and_insert.sql
+++ macros/utils/table_operations/delete_and_insert.sql
@@ -86,3 +86,26 @@
 
     {% do return(queries) %}
 {% endmacro %}
+
+{% macro athena__get_delete_and_insert_queries(relation, insert_relation, delete_relation, delete_column_key) %}
+    {% set queries = [] %}
+
+    {% if delete_relation %}
+        {% set delete_query %}
+            delete from {{ relation }}
+            where
+            {{ delete_column_key }} is null
+            or {{ delete_column_key }} in (select {{ delete_column_key }} from {{ delete_relation }});
+        {% endset %}
+        {% do queries.append(delete_query) %}
+    {% endif %}
+
+    {% if insert_relation %}
+        {% set insert_query %}
+            insert into {{ relation }} select * from {{ insert_relation }};
+        {% endset %}
+        {% do queries.append(insert_query) %}
+    {% endif %}
+
+    {% do return(queries) %}
+{% endmacro %}
