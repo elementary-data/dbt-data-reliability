cdcffaad644736054775c34cd032d5faefacfbfc
Elon Gliksberg <elongliks@gmail.com>
2023-06-22 12:13:08 +0300

    Fixed accessing properties that don't exist.

--- macros/edr/tests/test_configuration/get_timestamp_column.sql
+++ macros/edr/tests/test_configuration/get_timestamp_column.sql
@@ -12,12 +12,12 @@
 {% macro get_is_timestamp(timestamp_column, model_relation) %}
     {%- set timestamp_column_data_type = elementary.find_normalized_data_type_for_column(model_relation, timestamp_column) %}
     {{ elementary.debug_log('timestamp_column - ' ~ timestamp_column) }}
-    {{ elementary.debug_log('timestamp_column_data_type - ' ~ timestamp_column) }}
+    {{ elementary.debug_log('timestamp_column_data_type - ' ~ timestamp_column_data_type) }}
     {%- set is_timestamp = elementary.is_column_timestamp(model_relation, timestamp_column, timestamp_column_data_type) %}
     {{ elementary.debug_log('is_timestamp - ' ~ is_timestamp) }}
 
     {% if not is_timestamp %}
-      {% do exceptions.raise_compiler_error("Column `{}` is not a timestamp.".format(metric_properties.timestamp_column)) %}
+      {% do exceptions.raise_compiler_error("Column `{}` is not a timestamp.".format(timestamp_column)) %}
     {% endif %}
     {{ return(is_timestamp) }}
 {% endmacro %}
