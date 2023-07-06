89bb18bebe02ab608a569d248e7ab1af7141b7e9
Artem Garmash <artem.garmash@ouraring.com>
2023-07-06 15:54:17 +0300

    Limit insert query size for Athena

--- macros/edr/system/system_utils/get_config_var.sql
+++ macros/edr/system/system_utils/get_config_var.sql
@@ -73,3 +73,9 @@
     {% do default_config.update({'query_max_size': 250000}) %}
     {{- return(default_config) -}}
 {%- endmacro -%}
+
+{%- macro athena__get_default_config() -%}
+    {% set default_config = elementary.default__get_default_config() %}
+    {% do default_config.update({'query_max_size': 250000}) %}
+    {{- return(default_config) -}}
+{%- endmacro -%}
