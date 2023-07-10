b6544737ce106aa879668ccc511c1a4d813885d5
Elon Gliksberg <elongliks@gmail.com>
2023-07-10 18:19:25 +0300

    Updated seasonal tests.

--- integration_tests/macros/e2e_tests/validate_seasonal_volume_anomalies.sql
+++ integration_tests/macros/e2e_tests/validate_seasonal_volume_anomalies.sql
@@ -1,13 +1,16 @@
-{% macro validate_seasonal_volume_anomalies_after_training() %}
-    {% set alerts_relation = ref('elementary_test_results') %}
+{% macro validate_seasonal_volume_anomalies() %}
     {% set query %}
         select test_alias, status
-        from {{ alerts_relation }}
-        where table_name = 'users_per_day_weekly_seasonal'
+        from {{ ref('elementary_test_results') }}
+        where table_name in ('users_per_day_weekly_seasonal', 'users_per_hour_daily_seasonal')
     {% endset %}
     {% set results = elementary.run_query(query) %}
     {{ assert_lists_contain_same_items(results, [
-        ('volume_anomalies_no_seasonality_anomalies', 'fail'),
-        ('volume_anomalies_with_seasonality_anomalies', 'pass'),
+        ('day_of_week_volume_anomalies_no_seasonality', 'fail'),
+        ('day_of_week_volume_anomalies_with_seasonality', 'pass'),
+        ('hour_of_day_volume_anomalies_with_seasonality', 'pass'),
+        ('hour_of_day_volume_anomalies_no_seasonality', 'fail'),
+        ('hour_of_week_volume_anomalies_no_seasonality', 'fail'),
+        ('hour_of_week_volume_anomalies_with_seasonality', 'pass')
     ]) }}
 {% endmacro %}
