c33ce9a1473c1c86f21907514ea8fb4cf528beb8
Elon Gliksberg <elongliks@gmail.com>
2023-07-16 18:32:13 +0300

    Added more tests.

--- integration_tests_v2/tests/test_volume_anomalies.py
+++ integration_tests_v2/tests/test_volume_anomalies.py
@@ -4,6 +4,10 @@ from asserts import read_table
 from data_generator import DATE_FORMAT, generate_dates
 from run_dbt_test import run_dbt_test
 
+TIMESTAMP_COLUMN = "updated_at"
+DBT_TEST_NAME = "elementary.volume_anomalies"
+DBT_TEST_ARGS = {"timestamp_column": TIMESTAMP_COLUMN}
+
 
 def read_status(test_id):
     results = read_table(
@@ -16,24 +20,44 @@ def read_status(test_id):
 
 def test_anomalyless_table_volume_anomalies(request):
     test_id = request.node.name
-    dbt_test_name = "elementary.volume_anomalies"
     data = [
-        {"updated_at": date.strftime(DATE_FORMAT)}
+        {TIMESTAMP_COLUMN: date.strftime(DATE_FORMAT)}
         for date in generate_dates(base_date=datetime.now(), days_back=120)
         for _ in range(100)
     ]
-    run_dbt_test(data, test_id, dbt_test_name, {"timestamp_column": "updated_at"})
+    run_dbt_test(data, test_id, DBT_TEST_NAME, DBT_TEST_ARGS)
     assert all(result["status"] == "pass" for result in read_status(test_id))
 
 
-def test_anomalyful_table_volume_anomalies(request):
+def test_full_drop_table_volume_anomalies(request):
     test_id = request.node.name
-    dbt_test_name = "elementary.volume_anomalies"
     data = [
-        {"updated_at": date.strftime(DATE_FORMAT)}
+        {TIMESTAMP_COLUMN: date.strftime(DATE_FORMAT)}
         for date in generate_dates(base_date=datetime.now(), days_back=120)
         for _ in range(100)
         if date < datetime.now() - timedelta(days=2)
     ]
-    run_dbt_test(data, test_id, dbt_test_name, {"timestamp_column": "updated_at"})
+    run_dbt_test(data, test_id, DBT_TEST_NAME, DBT_TEST_ARGS)
+    assert all(result["status"] == "fail" for result in read_status(test_id))
+
+
+def test_partial_drop_table_volume_anomalies(request):
+    test_id = request.node.name
+    data = [
+        {TIMESTAMP_COLUMN: date.strftime(DATE_FORMAT)}
+        for date in generate_dates(base_date=datetime.now(), days_back=120)
+        for _ in range(100 if date < datetime.now() - timedelta(days=2) else 10)
+    ]
+    run_dbt_test(data, test_id, DBT_TEST_NAME, DBT_TEST_ARGS)
+    assert all(result["status"] == "fail" for result in read_status(test_id))
+
+
+def test_spike_table_volume_anomalies(request):
+    test_id = request.node.name
+    data = [
+        {TIMESTAMP_COLUMN: date.strftime(DATE_FORMAT)}
+        for date in generate_dates(base_date=datetime.now(), days_back=120)
+        for _ in range(100 if date < datetime.now() - timedelta(days=2) else 1000)
+    ]
+    run_dbt_test(data, test_id, DBT_TEST_NAME, DBT_TEST_ARGS)
     assert all(result["status"] == "fail" for result in read_status(test_id))
