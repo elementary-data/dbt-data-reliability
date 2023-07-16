9ad7791b95833cb4a79698222be6e96eff41cb23
Elon Gliksberg <elongliks@gmail.com>
2023-07-16 19:10:52 +0300

    Added default vars to the dbt project.

--- integration_tests_v2/tests/dbt_project.py
+++ integration_tests_v2/tests/dbt_project.py
@@ -4,6 +4,12 @@ from elementary.clients.dbt.dbt_runner import DbtRunner
 
 PATH = Path(__file__).parent.parent / "dbt_project"
 
+_DEFAULT_VARS = {
+    "disable_dbt_invocation_autoupload": True,
+    "disable_dbt_artifacts_autoupload": True,
+    "disable_run_results": True,
+}
+
 
 def get_dbt_runner(raise_on_failure: bool = False):
-    return DbtRunner(str(PATH), raise_on_failure=raise_on_failure)
+    return DbtRunner(str(PATH), vars=_DEFAULT_VARS, raise_on_failure=raise_on_failure)
