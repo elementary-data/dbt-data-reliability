55761547ef7761a52f76f067f0d88a6243597b6d
Elon Gliksberg <elongliks@gmail.com>
2023-07-17 19:43:53 +0300

    Added an artifacts caching test.

---
+++ integration_tests/integration_tests/tests/test_artifacts_caching.py
@@ -0,0 +1,24 @@
+from dbt_project import DbtProject
+
+TEST_MODEL = "one"
+
+
+def read_model_artifact_row(dbt_project: DbtProject):
+    return dbt_project.read_table("dbt_models", where=f"alias = '{TEST_MODEL}'")
+
+
+def test_artifacts_caching(dbt_project: DbtProject):
+    # Disabled by default in the tests for performance reasons.
+    dbt_project.dbt_runner.vars["disable_dbt_artifacts_autoupload"] = False
+
+    dbt_project.dbt_runner.run(select=TEST_MODEL, vars={"one_tags": ["hello", "world"]})
+    first_row = read_model_artifact_row(dbt_project)
+    dbt_project.dbt_runner.run(select=TEST_MODEL, vars={"one_tags": ["world", "hello"]})
+    second_row = read_model_artifact_row(dbt_project)
+    assert first_row == second_row, "Artifacts are not cached at the on run end."
+
+    dbt_project.dbt_runner.run(select=TEST_MODEL)
+    first_row = read_model_artifact_row(dbt_project)
+    dbt_project.dbt_runner.run(select=TEST_MODEL, vars={"one_owner": "ele"})
+    second_row = read_model_artifact_row(dbt_project)
+    assert first_row != second_row, "Artifacts are not cached at the on run end."
