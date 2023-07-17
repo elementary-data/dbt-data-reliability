63c7e1fa2c7c3357fe352eb0d030152059f5e371
Elon Gliksberg <elongliks@gmail.com>
2023-07-18 00:10:05 +0300

    Using correct table name in seeds.

--- integration_tests/integration_tests/tests/data_seeder.py
+++ integration_tests/integration_tests/tests/data_seeder.py
@@ -1,7 +1,6 @@
 import csv
 from contextlib import contextmanager
 from pathlib import Path
-from tempfile import NamedTemporaryFile
 from typing import List
 
 import dbt_project
@@ -19,14 +18,17 @@ class DbtDataSeeder:
 
     @contextmanager
     def seed(self, data: List[dict], table_name: str):
-        with NamedTemporaryFile(
-            mode="w+", dir=dbt_project.SEEDS_DIR_PATH, suffix=".csv"
-        ) as seed_file:
-            relative_seed_path = Path(seed_file.name).relative_to(dbt_project.PATH)
-            writer = csv.DictWriter(seed_file, fieldnames=data[0].keys())
-            writer.writeheader()
-            writer.writerows(data)
-            logger.info(f"Seeding {table_name} with {len(data)} rows.")
-            self.dbt_runner.seed(select=str(relative_seed_path), full_refresh=True)
-            logger.info(f"Seeded {table_name} with {len(data)} rows.")
-            yield
+        seed_path = dbt_project.SEEDS_DIR_PATH.joinpath(f"{table_name}.csv")
+        try:
+            with seed_path.open("w") as seed_file:
+                relative_seed_path = Path(seed_file.name).relative_to(dbt_project.PATH)
+                writer = csv.DictWriter(seed_file, fieldnames=data[0].keys())
+                writer.writeheader()
+                writer.writerows(data)
+                seed_file.flush()
+                logger.info(f"Seeding {table_name} with {len(data)} rows.")
+                self.dbt_runner.seed(select=str(relative_seed_path), full_refresh=True)
+                logger.info(f"Seeded {table_name} with {len(data)} rows.")
+                yield
+        finally:
+            seed_path.unlink()
