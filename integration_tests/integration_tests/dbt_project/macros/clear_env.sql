c49b0ec54ebcea40e3b169bbb9859f9255087a3c
Elon Gliksberg <elongliks@gmail.com>
2023-07-18 11:11:31 +0300

    Renamed a macro.

--- integration_tests/integration_tests/dbt_project/macros/clear_env.sql
+++ integration_tests/integration_tests/dbt_project/macros/clear_env.sql
@@ -1,11 +1,11 @@
 {% macro clear_env() %}
     {% do drop_schema(elementary.target_database(), target.schema) %}
     {% set database_name, schema_name = elementary.get_package_database_and_schema('elementary') %}
-    {% do drop_schema(database_name, schema_name) %}
-    {% do drop_schema(elementary.target_database(), "test_seeds") %}
+    {% do elementary_tests.edr_drop_schema(database_name, schema_name) %}
+    {% do elementary_tests.edr_drop_schema(elementary.target_database(), "test_seeds") %}
 {% endmacro %}
 
-{% macro drop_schema(database_name, schema_name) %}
+{% macro edr_drop_schema(database_name, schema_name) %}
     {% set schema_relation = api.Relation.create(database=database_name, schema=schema_name) %}
     {% do dbt.drop_schema(schema_relation) %}
     {% do adapter.commit() %}
