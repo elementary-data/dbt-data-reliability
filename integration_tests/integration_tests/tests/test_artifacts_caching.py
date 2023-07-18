ae19ddfa5048f6bfff51ae7d36c830c2790e23f1
Elon Gliksberg <elongliks@gmail.com>
2023-07-18 12:11:14 +0300

    Simplified code.

--- integration_tests/integration_tests/tests/test_artifacts_caching.py
+++ integration_tests/integration_tests/tests/test_artifacts_caching.py
@@ -1,5 +1,4 @@
 from dbt_project import DbtProject
-from elementary.clients.dbt.dbt_runner import DbtRunner
 
 TEST_MODEL = "one"
 
@@ -8,17 +7,13 @@ def read_model_artifact_row(dbt_project: DbtProject):
     return dbt_project.read_table("dbt_models", where=f"alias = '{TEST_MODEL}'")
 
 
-def enable_artifacts_autoupload(dbt_runner: DbtRunner):
-    new_vars = {
-        **dbt_runner.vars,
+def test_artifacts_caching(dbt_project: DbtProject):
+    dbt_project.dbt_runner.vars = {
+        **dbt_project.dbt_runner.vars,
         # Disabled by default in the tests for performance reasons.
         "disable_dbt_artifacts_autoupload": False,
     }
-    dbt_runner.vars = new_vars
 
-
-def test_artifacts_caching(dbt_project: DbtProject):
-    enable_artifacts_autoupload(dbt_project.dbt_runner)
     dbt_project.dbt_runner.run(select=TEST_MODEL, vars={"one_tags": ["hello", "world"]})
     first_row = read_model_artifact_row(dbt_project)
     dbt_project.dbt_runner.run(select=TEST_MODEL, vars={"one_tags": ["world", "hello"]})
