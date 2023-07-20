3b89bc799bbbeb90e3a914a5556f5e38ac501b93
Elon Gliksberg <elongliks@gmail.com>
2023-07-20 15:56:07 +0300

    Added BigQuery materializations.

--- macros/edr/materializations/model/table.sql
+++ macros/edr/materializations/model/table.sql
@@ -4,3 +4,10 @@
   {% do elementary.cache_row_count_metric(row_count) %}
   {% do return(relations) %}
 {% endmaterialization %}
+
+{% materialization table, adapter="bigquery" %}
+  {% set relations = dbt.materialization_table_bigquery() %}
+  {% set row_count = elementary.query_row_count_metric() %}
+  {% do elementary.cache_row_count_metric(row_count) %}
+  {% do return(relations) %}
+{% endmaterialization %}
