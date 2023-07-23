088c0753c2f7ece27a07a73b2b6a8845f9a93ad0
Elon Gliksberg <elongliks@gmail.com>
2023-07-23 10:54:48 +0300

    Changed a macro.

--- macros/edr/materializations/model/common.sql
+++ macros/edr/materializations/model/common.sql
@@ -1,22 +1,26 @@
-{% macro get_row_count_metric() %}
+{% macro query_table_metrics() %}
   {% set query %}
     select count(*) as row_count
     from {{ this }}
   {% endset %}
-  {% set value = elementary.result_value(query) %}
-  {% do return({
-    "id": "{}.{}".format(invocation_id, this),
-    "full_table_name": this | string,
-    "column_name": none,
-    "metric_name": "row_count",
-    "metric_value": value
-  }) %}
+
+  {% set metrics = [] %}
+  {% for metric_column in elementary.run_query(query).columns %}
+    {% set metric_name = metric_column.name %}
+    {% set metric_value = metric_column[0] %}
+    {% do metrics.append({
+      "id": "{}.{}".format(invocation_id, this),
+      "full_table_name": this | string,
+      "column_name": none,
+      "metric_name": metric_name,
+      "metric_value": metric_value 
+    }) %}
+  {% endfor %}
+  {% do return(metrics) %}
 {% endmacro %}
 
 {% macro query_metrics() %}
-  {% do return([
-    elementary.get_row_count_metric()
-  ]) %}
+  {% do return(elementary.query_table_metrics()) %}
 {% endmacro %}
 
 {% macro cache_metrics(metrics) %}
