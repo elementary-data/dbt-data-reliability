660e543080eaec70543fa32971b046ed249d88e7
Ofek Weiss <ofek@elementary-data.com>
2023-07-27 13:24:06 +0300

    Added python test

---
+++ integration_tests/integration_tests/tests/test_python.py
@@ -0,0 +1,74 @@
+import json
+from typing import Union
+import pytest
+from parametrization import Parametrization
+
+from dbt_project import DbtProject
+
+COLUMN_NAME = "column_name"
+TEST_NAME = "elementary.python"
+
+
+@pytest.mark.only_on_targets("snowflake", "bigquery")
+@Parametrization.autodetect_parameters()
+@Parametrization.case("successfull", python_result=0, expected_status="pass")
+@Parametrization.case("unsuccessfull", python_result=1, expected_status="fail")
+def test_python_int(
+    test_id: str,
+    dbt_project: DbtProject,
+    python_result: int,
+    expected_status: str,
+):
+    data = [{COLUMN_NAME: str()}]
+    result = dbt_project.test(
+        data,
+        test_id,
+        TEST_NAME,
+        dict(
+            code_macro="python_mock_test",
+            macro_args=dict(result=python_result),
+        ),
+    )
+    assert result["status"] == expected_status
+
+
+@pytest.mark.only_on_targets("snowflake", "bigquery")
+def test_python_full_df(test_id: str, dbt_project: DbtProject):
+    data = [{COLUMN_NAME: str()}]
+    result = dbt_project.test(
+        data,
+        test_id,
+        TEST_NAME,
+        dict(
+            code_macro="python_return_df",
+            macro_args=dict(result=json.dumps(data)),
+        ),
+    )
+    assert result["status"] == "fail"
+
+
+@pytest.mark.only_on_targets("snowflake", "bigquery")
+def test_python_empty_df(test_id: str, dbt_project: DbtProject):
+    data = [{COLUMN_NAME: str()}]
+    result = dbt_project.test(
+        data,
+        test_id,
+        TEST_NAME,
+        dict(code_macro="python_return_empty_df"),
+    )
+    assert result["status"] == "pass"
+
+
+@pytest.mark.skip_targets("snowflake", "bigquery")
+def test_invalid_target_python(test_id: str, dbt_project: DbtProject):
+    data = [{COLUMN_NAME: str()}]
+    result = dbt_project.test(
+        data,
+        test_id,
+        TEST_NAME,
+        dict(
+            code_macro="python_mock_test",
+            macro_args=dict(result=True),
+        ),
+    )
+    assert result["status"] == "error"
