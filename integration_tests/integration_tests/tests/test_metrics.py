3cd6bb563808cde6935b0476a86d48c965e439d9
Elon Gliksberg <elongliks@gmail.com>
2023-08-03 12:10:07 +0300

    Not using YAML selector.

--- integration_tests/integration_tests/tests/test_metrics.py
+++ integration_tests/integration_tests/tests/test_metrics.py
@@ -19,7 +19,7 @@ def test_metrics(dbt_project: DbtProject):
     ]
     dbt_project.seed(data1, "metrics_seed1")
     dbt_project.seed(data2, "metrics_seed2")
-    dbt_project.dbt_runner.run(selector="metrics")
+    dbt_project.dbt_runner.run(select="metrics")
 
     remaining_models_to_row_count = {
         "metrics_view": len(data1) + len(data2),
