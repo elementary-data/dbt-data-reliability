a6800714c74a1a210dfa37690365d3d2e9e0c714
roms <roms@ziprecruiter.com>
2023-08-03 07:26:23 +0300

    Resolve open issue: #1048

--- macros/utils/table_operations/create_temp_table.sql
+++ macros/utils/table_operations/create_temp_table.sql
@@ -3,7 +3,7 @@
                                                                                schema=schema_name,
                                                                                identifier=table_name,
                                                                                type='table') -%}
-    {% set temp_table_relation = dbt.make_temp_relation(temp_table_relation) %}
+    {% set temp_table_relation = elementary._make_temp_relation(temp_table_relation) %}
     {% if temp_table_exists %}
         {% do adapter.drop_relation(temp_table_relation) %}
         {% do elementary.run_query(dbt.create_table_as(True, temp_table_relation, sql_query)) %}
