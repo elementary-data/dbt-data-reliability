7e2f82cdf1b3ab095a12e905dea2fd4ad0663a7b
Elon Gliksberg <elongliks@gmail.com>
2023-08-03 11:37:14 +0300

    Changed an error to a warning.

--- macros/edr/system/hooks/on_run_end.sql
+++ macros/edr/system/hooks/on_run_end.sql
@@ -2,27 +2,27 @@
   {%- if execute and flags.WHICH not in ['generate', 'serve'] %}
       {% set edr_cli_run = elementary.get_config_var('edr_cli_run') %}
       {% if not execute or edr_cli_run %}
-        {{ return('') }}
+        {% do return("") %}
       {% endif %}
 
       {% if flags.WHICH in ['run', 'build'] %}
-        {{ elementary.insert_metrics() }}
+        {% do elementary.insert_metrics() %}
       {% endif %}
 
       {% if not elementary.get_config_var('disable_dbt_artifacts_autoupload') %}
-        {{ elementary.upload_dbt_artifacts() }}
+        {% do elementary.upload_dbt_artifacts() %}
       {% endif %}
 
       {% if not elementary.get_config_var('disable_run_results') %}
-        {{ elementary.upload_run_results() }}
+        {% do elementary.upload_run_results() %}
       {% endif %}
 
       {% if flags.WHICH in ['test', 'build'] and not elementary.get_config_var('disable_tests_results') %}
-        {{ elementary.handle_tests_results() }}
+        {% do elementary.handle_tests_results() %}
       {% endif %}
 
       {% if not elementary.get_config_var('disable_dbt_invocation_autoupload') %}
-        {{ elementary.upload_dbt_invocation() }}
+        {% do elementary.upload_dbt_invocation() %}
       {% endif %}
   {% endif %}
 {% endmacro %}
--- macros/edr/tests/on_run_end/insert_metrics.sql
+++ macros/edr/tests/on_run_end/insert_metrics.sql
@@ -3,10 +3,11 @@
   {% set database_name, schema_name = elementary.get_package_database_and_schema() %}
   {%- set target_relation = adapter.get_relation(database=database_name, schema=schema_name, identifier='data_monitoring_metrics') -%}
   {% if not target_relation %}
-    {% do exceptions.raise_compiler_error("Couldn't find Elementary's models. Please run `dbt run -s elementary`.") %}
+    {% do exceptions.warn("Couldn't find Elementary's models. Please run `dbt run -s elementary`.") %}
+    {% do return(none) %}
   {% endif %}
 
   {{ elementary.file_log("Inserting metrics into {}.".format(target_relation)) }}
   {% do elementary.insert_rows(target_relation, metrics, should_commit=true) %}
-  {{ return('') }}
+  {% do return(none) %}
 {% endmacro %}
