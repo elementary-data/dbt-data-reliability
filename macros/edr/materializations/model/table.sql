2e3d04f9f8dbd2bd085ab482745a7ead703ab398
Elon Gliksberg <elongliks@gmail.com>
2023-08-03 13:56:52 +0300

    Added materializations to rest of adapters.

--- macros/edr/materializations/model/table.sql
+++ macros/edr/materializations/model/table.sql
@@ -5,8 +5,29 @@
   {% do return(relations) %}
 {% endmaterialization %}
 
+{% materialization table, adapter="snowflake" %}
+  {% set relations = dbt.materialization_table_snowflake() %}
+  {% set metrics = elementary.query_metrics() %}
+  {% do elementary.cache_metrics(metrics) %}
+  {% do return(relations) %}
+{% endmaterialization %}
+
 {% materialization table, adapter="bigquery" %}
   {% set relations = dbt.materialization_table_bigquery() %}
+  {% set metrics = elementary.query_metrics() %}
+  {% do elementary.cache_metrics(metrics) %}
+  {% do return(relations) %}
+{% endmaterialization %}
+
+{% materialization table, adapter="spark" %}
+  {% set relations = dbt.materialization_table_spark() %}
+  {% set metrics = elementary.query_metrics() %}
+  {% do elementary.cache_metrics(metrics) %}
+  {% do return(relations) %}
+{% endmaterialization %}
+
+{% materialization table, adapter="databricks" %}
+  {% set relations = dbt.materialization_table_databricks() %}
   {% set metrics = elementary.query_metrics() %}
   {% do elementary.cache_metrics(metrics) %}
   {% do return(relations) %}
--- macros/edr/materializations/model/view.sql
+++ macros/edr/materializations/model/view.sql
@@ -5,8 +5,29 @@
   {% do return(relations) %}
 {% endmaterialization %}
 
+{% materialization view, adapter="snowflake" %}
+  {% set relations = dbt.materialization_view_snowflake() %}
+  {% set metrics = elementary.query_metrics() %}
+  {% do elementary.cache_metrics(metrics) %}
+  {% do return(relations) %}
+{% endmaterialization %}
+
 {% materialization view, adapter="bigquery" %}
   {% set relations = dbt.materialization_view_bigquery() %}
+  {% set metrics = elementary.query_metrics() %}
+  {% do elementary.cache_metrics(metrics) %}
+  {% do return(relations) %}
+{% endmaterialization %}
+
+{% materialization view, adapter="spark" %}
+  {% set relations = dbt.materialization_view_spark() %}
+  {% set metrics = elementary.query_metrics() %}
+  {% do elementary.cache_metrics(metrics) %}
+  {% do return(relations) %}
+{% endmaterialization %}
+
+{% materialization view, adapter="databricks" %}
+  {% set relations = dbt.materialization_view_databricks() %}
   {% set metrics = elementary.query_metrics() %}
   {% do elementary.cache_metrics(metrics) %}
   {% do return(relations) %}
