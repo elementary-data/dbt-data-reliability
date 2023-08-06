a709543965787b767d1377753835b368967e0aef
Elon Gliksberg <elongliks@gmail.com>
2023-08-06 10:56:29 +0300

    Added time bucket test.

--- integration_tests/integration_tests/tests/test_volume_anomalies.py
+++ integration_tests/integration_tests/tests/test_volume_anomalies.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 from typing import Any, Dict, List
 
 from data_generator import DATE_FORMAT, generate_dates
@@ -83,4 +83,20 @@ def test_volume_anomalies_with_where_parameter(
     test_result = dbt_project.test(
         data, test_id, DBT_TEST_NAME, params, as_model=as_model
     )
+    assert test_result["status"] == "fail"
+
+
+def test_volume_anomalies_with_time_buckets(test_id: str, dbt_project: DbtProject):
+    now = datetime.utcnow()
+    data = [
+        {TIMESTAMP_COLUMN: cur_date.strftime(DATE_FORMAT)}
+        for cur_date in generate_dates(base_date=now, period="hours", days_back=2)
+        if cur_date < now - timedelta(hours=1)
+    ]
+    test_args = {
+        **DBT_TEST_ARGS,
+        "time_bucket": {"period": "hour", "count": 1},
+        "days_back": 1,
+    }
+    test_result = dbt_project.test(data, test_id, DBT_TEST_NAME, test_args)
     assert test_result["status"] == "fail"
