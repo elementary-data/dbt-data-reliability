b1781b91dc7cef2ea008f6be30c5676f59e6de4e
Ofek Weiss <ofek@elementary-data.com>
2023-08-30 14:56:51 +0300

    added query history implementation for bigquery

---
+++ macros/utils/cross_db_utils/get_query_history.sql
@@ -0,0 +1,31 @@
+{% macro get_query_history(from_time) %}
+  {% do return(adapter.dispatch("get_query_history", "elementary")(from_time)) %}
+{% endmacro %}
+
+{% macro bigquery__get_query_history(from_time) %}
+select 
+  job_id || referenced_table.project_id || referenced_table.dataset_id || referenced_table.table_id as id,
+  job_id as job_id,
+  statement_type,
+  start_time,
+  end_time,
+  state,
+  referenced_table.project_id as source_db,
+  referenced_table.dataset_id as source_schema,
+  referenced_table.table_id as source_table,
+  destination_table.project_id as dest_db,
+  destination_table.dataset_id as dest_schema,
+  destination_table.table_id as dest_table
+from `{{ target.project }}`.`region-{{ target.location }}`.INFORMATION_SCHEMA.JOBS, 
+  UNNEST(referenced_tables) AS referenced_table
+where
+  {% if from_time %}
+    creation_time > {{ from_time }} and
+  {% endif %}
+  job_type = 'QUERY'
+{% endmacro %}
+
+{# TODO: impement for other DWH types after POC with BigQuery #}
+{% macro default__get_query_history(from_time) %}
+  {{ elementary.empty_query_history() }}
+{% endmacro %}
---
+++ models/edr/run_results/query_history.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='id',
+    on_schema_change='append_new_columns',
+    full_refresh=elementary.get_config_var('elementary_full_refresh'),
+  )
+}}
+
+{% if is_incremental() %}
+  {% set from_time %}
+    coalesce((select {{ elementary.edr_timeadd('day', -2, 'max(start_time)') }} from {{ this }} ), 
+              {{ elementary.edr_timeadd('day', -30, elementary.edr_current_timestamp()) }})
+  {% endset %}
+{% else %}
+  {% set from_time = elementary.edr_timeadd('day', -30, elementary.edr_current_timestamp()) %}
+{% endif %}
+
+{{ elementary.get_query_history(from_time) }}
