b93b90c25509f2d63959c136fcd4e126817ef9fa
arunkc <arunkc91@gmail.com>
2023-09-08 20:46:21 +0530

    change models on_schema_change

--- models/edr/dbt_artifacts/dbt_seeds.sql
+++ models/edr/dbt_artifacts/dbt_seeds.sql
@@ -5,7 +5,7 @@
     transient=False,
     post_hook='{{ elementary.upload_dbt_seeds() }}',
     unique_key='unique_id',
-    on_schema_change='append_new_columns',
+    on_schema_change='sync_all_columns',
     full_refresh=elementary.get_config_var('elementary_full_refresh')
   )
 }}
