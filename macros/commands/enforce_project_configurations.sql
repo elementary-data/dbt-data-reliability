2fd622a7bfeb2b891ee5b02995ee2a772bb6b90a
oravi <oravi90@gmail.com>
2023-09-21 17:14:09 +0300

    Excluded non project models / sources

--- macros/commands/enforce_project_configurations.sql
+++ macros/commands/enforce_project_configurations.sql
@@ -19,43 +19,45 @@
     {% set validation_result = {'success': true} %}
     {% for node in nodes -%}
         {% set flattened_node = flatten_callback(node) %}
-        {%- if enforce_owners and flattened_node.owner | length == 0 -%}
-            {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have an owner") %}
-            {% do validation_result.update({'success': false}) %}
-        {%- endif -%}
-
-        {%- if enforce_tags | length > 0 -%}
-            {%- if flattened_node.tags | length == 0 -%}
-                {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required tags") %}
+        {%- if flattened_node.package_name == context[project_name] -%}
+            {%- if enforce_owners and flattened_node.owner | length == 0 -%}
+                {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have an owner") %}
                 {% do validation_result.update({'success': false}) %}
             {%- endif -%}
 
-            {% set flattened_node_tags_set = set(flattened_node.tags) %}
-            {% set enforced_node_tags_set = set(enforce_node_tags) %}
-            {%- if flattened_node_tags_set.intersect(enforced_node_tags_set) | length == 0 -%}
-                {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required tags") %}
-                {% do validation_result.update({'success': false}) %}
-            {%- endif -%}
-        {%- endif -%}
-
-        {%- if enforce_meta_params | length > 0 -%}
-            {%- for meta_param in enforce_meta_params -%}
-                {%- if meta_param not in flattened_node.meta -%}
-                    {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required meta param " ~ meta_param) %}
+            {%- if enforce_tags | length > 0 -%}
+                {%- if flattened_node.tags | length == 0 -%}
+                    {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required tags") %}
                     {% do validation_result.update({'success': false}) %}
                 {%- endif -%}
-            {%- endfor -%}
-        {%- endif -%}
 
-        {%- if enforce_config_params | length > 0 -%}
-            {%- for config_param in enforce_config_params -%}
-                {%- if flattened_node.config is not none -%}
-                    {%- if config_param not in flattened_node.config -%}
-                        {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required config param " ~ config_param) %}
+                {% set flattened_node_tags_set = set(flattened_node.tags) %}
+                {% set enforced_node_tags_set = set(enforce_node_tags) %}
+                {%- if flattened_node_tags_set.intersect(enforced_node_tags_set) | length == 0 -%}
+                    {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required tags") %}
+                    {% do validation_result.update({'success': false}) %}
+                {%- endif -%}
+            {%- endif -%}
+
+            {%- if enforce_meta_params | length > 0 -%}
+                {%- for meta_param in enforce_meta_params -%}
+                    {%- if meta_param not in flattened_node.meta -%}
+                        {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required meta param " ~ meta_param) %}
                         {% do validation_result.update({'success': false}) %}
                     {%- endif -%}
-                {%- endif -%}
-            {%- endfor -%}
+                {%- endfor -%}
+            {%- endif -%}
+
+            {%- if enforce_config_params | length > 0 -%}
+                {%- for config_param in enforce_config_params -%}
+                    {%- if flattened_node.config is not none -%}
+                        {%- if config_param not in flattened_node.config -%}
+                            {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have required config param " ~ config_param) %}
+                            {% do validation_result.update({'success': false}) %}
+                        {%- endif -%}
+                    {%- endif -%}
+                {%- endfor -%}
+            {%- endif -%}
         {%- endif -%}
     {%- endfor -%}
     {{- return(validation_result['success']) -}}
