999a7e6070f016df0120b8894122cf5a6206f776
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-09-22 16:54:17 +0900

    Empty schema of `filtered_information_schema_columns` when initializing
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- macros/edr/metadata_collection/get_columns_by_schemas.sql
+++ macros/edr/metadata_collection/get_columns_by_schemas.sql
@@ -1,5 +1,8 @@
 {% macro get_columns_by_schemas(configured_schemas) %}
-    {%- if configured_schemas | length > 0 -%}
+    {# if 'dbt run --select elementary' is executed, then the variable is set to true #}
+    {% set is_selected_elementary = ('elementary' in context["invocation_args_dict"]["select"]) %}
+
+    {%- if configured_schemas | length > 0 and not is_selected_elementary -%}
         {{ elementary.union_macro_queries(configured_schemas, elementary.get_columns_from_information_schema) }}
     {%- else %}
         {{ elementary.get_empty_columns_from_information_schema_table() }}
