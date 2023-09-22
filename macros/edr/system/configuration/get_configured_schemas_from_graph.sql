920332c046b6d228149c438c8ae7df55bdaedc08
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-09-22 15:46:36 +0900

    Add print
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- macros/edr/system/configuration/get_configured_schemas_from_graph.sql
+++ macros/edr/system/configuration/get_configured_schemas_from_graph.sql
@@ -9,8 +9,12 @@
         {% set nodes = elementary.get_nodes_from_graph() %}
         {% for node in nodes %}
             {% if node.resource_type in ['model', 'source', 'snapshot', 'seed'] and node.package_name == root_project %}
+                {{ print("node.package_name: " ~ toyaml(node)) }}
+                {{ print("condition1: " ~ node.package_name == root_project) }}
+                {{ print("condition2: " ~ node.resource_type in ['model', 'source', 'snapshot', 'seed'] and node.package_name == root_project ) }}
                 {% set schema_tuple = (node.database, node.schema) %}
                 {% if schema_tuple not in configured_schemas %}
+                    {{ print("schema_tuple: " ~ toyaml(schema_tuple)) }}
                     {% do configured_schemas.append(schema_tuple) %}
                 {% endif %}
             {% endif %}
