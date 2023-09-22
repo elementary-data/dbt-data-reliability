5e5a9af44d46e68bda4169ec47e275b000741567
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2023-09-22 15:20:54 +0900

    Show logs more
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- models/edr/metadata_store/filtered_information_schema_columns.sql
+++ models/edr/metadata_store/filtered_information_schema_columns.sql
@@ -6,6 +6,10 @@
 
 {% set configured_schemas = elementary.get_configured_schemas_from_graph() %}
 
+{{ log("============================================") }}
+{{ log("configured_schemas: " ~ toyaml(configured_schemas)) }}
+{{ log("============================================") }}
+
 with filtered_information_schema_columns as (
     {{ elementary.get_columns_by_schemas(configured_schemas) }}
 )
