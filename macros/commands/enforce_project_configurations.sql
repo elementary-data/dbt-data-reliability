ad015db5a16cdef7d7fd66f22646fc133ce97665
oravi <oravi90@gmail.com>
2023-09-25 13:04:08 +0300

    Added an option to configure enforcement from the dbt project vars and meta/config as well

--- macros/commands/enforce_project_configurations.sql
+++ macros/commands/enforce_project_configurations.sql
@@ -1,12 +1,14 @@
-{%- macro enforce_project_configurations(enforce_model_owners=false, enforce_source_owners=false, enforce_model_tags=[], enforce_source_tags=[], enforce_model_meta_params=[], enforce_model_config_params=[], enforce_source_config_params=[]) -%}
+{%- macro enforce_project_configurations(enforce_owners=false, enforce_tags=[], enforce_meta_params=[], enforce_config_params=[], exclude_sources=false) -%}
     {%- if execute -%}
         {# enforcing source params #}
-        {% set sources = graph.sources.values() | selectattr('resource_type', '==', 'source') %}
-        {% set sources_result = elementary.enforce_configuration(sources, elementary.flatten_source, enforce_source_owners, enforce_source_tags, enforce_source_meta_params, enforce_source_config_params) %}
+        {%- if not exclude_sources -%}
+            {% set sources = graph.sources.values() | selectattr('resource_type', '==', 'source') %}
+            {% set sources_result = elementary.enforce_configuration(sources, elementary.flatten_source, enforce_owners, enforce_tags, enforce_meta_params, enforce_config_params) %}
+        {%- endif -%}
 
         {# enforcing model params #}
         {% set models = graph.nodes.values() | selectattr('resource_type', '==', 'model') %}
-        {% set models_result = elementary.enforce_configuration(models, elementary.flatten_model, enforce_model_owners, enforce_model_tags, enforce_model_meta_params, enforce_model_config_params)%}
+        {% set models_result = elementary.enforce_configuration(models, elementary.flatten_model, enforce_owners, enforce_tags, enforce_meta_params, enforce_config_params)%}
 
         {%- if models_result or sources_result -%}
             {{ exceptions.raise_compiler_error("Found issues in projdct configurations") }}
@@ -14,12 +16,21 @@
     {%- endif -%}
 {%- endmacro -%}
 
+{%- macro get_enforcement_param(flattened_node, enforcement_param_name, enforcement_param_arg_value) -%}
+    {% set node_enforcement_param = flattened_node.meta.get(enforcement_param_name) or flattened_node.config.get(enforcement_param_name) or elementary.elementary.get_config_var(enforcement_param_name) or enforcement_param_arg_value %}
+    {{- return(node_enforcement_param) -}}
+{%- endmacro -%}
 
 {%- macro enforce_configuration(nodes, flatten_callback, enforce_owners, enforce_tags=[], enforce_meta_params=[], enforce_config_params=[]) -%}
     {% set validation_result = {'success': true} %}
     {% for node in nodes -%}
         {% set flattened_node = flatten_callback(node) %}
         {%- if flattened_node.package_name == project_name -%}
+            {% set enforce_owners = elementary.get_enforcement_param(flattened_node, 'enforce_owners', enforce_owners) %}
+            {% set enforce_tags = elementary.get_enforcement_param(flattened_node, 'enforce_tags', enforce_tags) %}
+            {% set enforce_meta_params = elementary.get_enforcement_param(flattened_node, 'enforce_meta_params', enforce_meta_params) %}
+            {% set enforce_config_params = elementary.get_enforcement_param(flattened_node, 'enforce_config_params', enforce_config_params) %}
+
             {%- if enforce_owners and flattened_node.owner | length == 0 -%}
                 {% do elementary.edr_log(node.resource_type ~ " " ~ node.name ~ " does not have an owner") %}
                 {% do validation_result.update({'success': false}) %}
