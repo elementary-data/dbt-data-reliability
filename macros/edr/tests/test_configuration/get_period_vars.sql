86dbcac1363c77c8d8e4162175e0313b0035c505
Daniel Pollak <daniel@elementary-data.com>
2023-09-27 14:09:55 +0300

    add params basics

---
+++ macros/edr/tests/test_configuration/get_period_vars.sql
@@ -0,0 +1,78 @@
+{% macro get_detection_period_var(detection_period, model_graph_node) %}
+  {% set detection_period = elementary.get_test_argument('detection_period', detection_period, model_graph_node) %}
+  {{ return(detection_period) }}
+{% endmacro %}
+
+{% macro get_training_period_var(training_period, model_graph_node) %}
+  {% set training_period = elementary.get_test_argument('training_period', training_period, model_graph_node) %}
+  {{ return(training_period) }}
+{% endmacro %}
+
+{% macro get_period_default_var(unit, count) %}
+  {{ return({'unit': unit, 'count': count}) }}
+{% endmacro %}
+
+{% macro get_unit_of_period(period) %}
+  {{ return(period.unit) }}
+{% endmacro %}
+
+{% macro get_count_of_period(period) %}
+  {{ return(period.count) }}
+{% endmacro %}
+
+{% macro convert_period(period, convert_to) %}
+  {% set convert_from = elementary.get_unit_of_period(period) %}
+  {% set period_count = elementary.get_count_of_period(period) %}
+  
+  {% if convert_from == 'week' %}
+    {% if convert_to == 'day' %}
+      {% set period_count = period_count * 7 %}
+    {% elif convert_to == 'hour' %}
+      {% set period_count = period_count * 7 * 24 %}
+    {% elif convert_to == 'month' %}
+      {% set period_count = (period_count * 7) / 30 %}
+    {% endif %}
+  
+  {% elif convert_from == 'day' %}
+    {% if convert_to == 'hour' %}
+      {% set period_count = period_count * 24 %}
+    {% elif convert_to == 'week '%}
+      {% set period_count = period_count / 7 %}
+    {% elif convert_to == 'month' %}
+      {% set period_count = period_count / 30 %}
+    {% endif %}
+
+  {% elif convert_from == 'hour' %}
+    {% if convert_to == 'day' %}
+      {% set period_count = period_count / 24 %}
+    {% elif convert_to == 'week' %}
+      {% set period_count = period_count / (7 * 24) %}
+    {% elif convert_to == 'month' %}
+      {% set period_count = period_count / (24 * 30) %}
+    {% endif %}
+
+    {% elif convert_from == 'month' %}
+    {% if convert_to == 'day' %}
+      {% set period_count = period_count * 30 %}
+    {% elif convert_to == 'week' %}
+      {% set period_count = (period_count * 30) / 7  %}
+    {% elif convert_to == 'hour' %}
+      {% set period_count = period_count * 30 * 24 %}
+    {% endif %}
+  {% endif %}
+
+  {% set period_count = period_count | int %}
+  {{ return({'unit': convert_to, 'count': period_count}) }}
+{% endmacro %}
+
+{% macro months_to_days(count_of_months, end_datetime) %}
+  {% set diff = value %}
+  {{ return(end_datetime - modules.datetime.timedelta(months=count_of_months)) }}
+{% endmacro %}
+
+{% macro check() %}
+  {{ log("### Start Check ###", true) }}
+  {# {{ log(get_count_of_period({"count": 4}), true) }} #}
+  {{ log(convert_period({"unit": "month", "count": 1}, "day"), true) }}
+  {{ log("### End Check ###", true) }}
+{% endmacro %}
