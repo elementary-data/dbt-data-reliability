872eb5f8f889ec39d6c5eb24d75bdcf81c97dde3
Elon Gliksberg <elongliks@gmail.com>
2023-10-08 15:09:19 +0300

    Added 'as_dicts' to 'run_query'.

--- macros/utils/run_queries/run_query.sql
+++ macros/utils/run_queries/run_query.sql
@@ -1,4 +1,4 @@
-{% macro run_query(query, lowercase_column_names=True) %}
+{% macro run_query(query, lowercase_column_names=True, as_dicts=false) %}
     {% set query_result = dbt.run_query(query) %}
     {% if lowercase_column_names %}
         {% set lowercased_column_names = {} %}
@@ -7,5 +7,10 @@
         {% endfor %}
         {% set query_result = query_result.rename(lowercased_column_names) %}
     {% endif %}
+
+    {% if as_dicts %}
+        {% do return(elementary.agate_to_dicts(query_result)) %}
+    {% endif %}
+
     {% do return(query_result) %}
 {% endmacro %}
