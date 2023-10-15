af6bdebecb41eb137326fb65ed4b3e18f2107ccb
Daniel Pollak <daniel@elementary-data.com>
2023-10-14 22:19:49 -0400

    take out metric map fetcher

---
+++ macros/edr/data_monitoring/monitors_query/get_metrics_macro_map.sql
@@ -0,0 +1,12 @@
+{% macro get_metrics_macro_map() %}
+  {% do return(adapter.dispatch("get_metrics_macro_map", "elementary")()) %}
+{% endmacro %}
+
+{% macro default__get_metrics_macro_map() %}
+    {% do return({
+        "row_count": elementary.row_count_metric_query,
+        "freshness": elementary.freshness_metric_query,
+        "event_freshness": elementary.event_freshness_metric_query,
+        "update_freshness": elementary.update_freshness_query
+    }) %}
+{% endmacro %}
