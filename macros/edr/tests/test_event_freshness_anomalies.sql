612f51dff4b805116387802a4557da3f7fbb53d7
Itamar Hartstein <haritamar@gmail.com>
2023-10-18 15:16:46 +0300

    test_event_freshness: bugfix - use model_relation instaed of model (#591)

--- macros/edr/tests/test_event_freshness_anomalies.sql
+++ macros/edr/tests/test_event_freshness_anomalies.sql
@@ -12,7 +12,7 @@
     {%- if not event_timestamp_column -%}
       {%- do exceptions.raise_compiler_error('event_timestamp_column must be specified for the event freshness test!') -%}
     {%- endif -%}
-    {%- set event_timestamp_column_data_type = elementary.find_normalized_data_type_for_column(model, event_timestamp_column) -%}
+    {%- set event_timestamp_column_data_type = elementary.find_normalized_data_type_for_column(model_relation, event_timestamp_column) -%}
     {%- if not elementary.is_column_timestamp(model_relation, event_timestamp_column, event_timestamp_column_data_type) -%}
       {% do exceptions.raise_compiler_error("Column `{}` is not a timestamp.".format(event_timestamp_column)) %}
     {%- endif -%}
