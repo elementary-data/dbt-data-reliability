93152284f3ee90e03f1b19270f102f7a35d8a9d8
oravi <oravi90@gmail.com>
2023-10-25 09:42:07 +0300

    Small perf improvements

--- macros/utils/graph/get_elementary_relation.sql
+++ macros/utils/graph/get_elementary_relation.sql
@@ -3,10 +3,11 @@
     {%- set identifier_node = elementary.get_node('model.elementary.' ~ identifier) %}
     {%- if identifier_node -%}
       {%- set identifier_alias = elementary.safe_get_with_default(identifier_node, 'alias', identifier) %}
+      {% set elementary_database, elementary_schema = identifier_node.database, identifier_node.schema %}
     {%- else -%}
       {% set identifier_alias = identifier %}
+      {% set elementary_database, elementary_schema = elementary.get_package_database_and_schema() %}
     {%- endif -%}
-    {% set elementary_database, elementary_schema = elementary.get_package_database_and_schema() %}
     {% if this and this.database == elementary_database and this.schema == elementary_schema and this.identifier == identifier_alias %}
       {% do return(this) %}
     {% endif %}
