588d38fec6c587a86456b1ee556ff8520bffa10c
Ella Katz <ella@elementary-data.com>
2023-12-04 05:08:55 +0200

    add not-null, not-missing, not-zero %

--- macros/edr/data_monitoring/monitors/monitors.sql
+++ macros/edr/data_monitoring/monitors/monitors.sql
@@ -1,9 +1,9 @@
 {% macro get_available_monitors() %}
     {% do return({
       'table': ['row_count', 'freshness'],
-      'column_any_type': ['null_count', 'null_percent'],
-      'column_string': ['min_length', 'max_length', 'average_length', 'missing_count', 'missing_percent'],
-      'column_numeric': ['min', 'max', 'zero_count', 'zero_percent', 'average', 'standard_deviation', 'variance', 'sum']
+      'column_any_type': ['null_count', 'null_percent', 'not_null_percent'],
+      'column_string': ['min_length', 'max_length', 'average_length', 'missing_count', 'missing_percent', 'not_missing_percent'],
+      'column_numeric': ['min', 'max', 'zero_count', 'zero_percent', 'not_zero_percent', 'average', 'standard_deviation', 'variance', 'sum']
     }) %}
 {% endmacro %}
 
