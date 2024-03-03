ec7a642b6fe85881ca7617f16b964639b3a7c213
Itamar Hartstein <haritamar@gmail.com>
2024-03-03 15:06:14 +0200

    artifacts: use cache also for model post-hook

--- macros/edr/dbt_artifacts/upload_artifacts_to_table.sql
+++ macros/edr/dbt_artifacts/upload_artifacts_to_table.sql
@@ -15,26 +15,40 @@
         {# In append mode, just insert, and no need to be atomic #}
         {% do elementary.insert_rows(table_relation, flatten_artifact_dicts, should_commit, elementary.get_config_var('dbt_artifacts_chunk_size'), on_query_exceed) %}
     {% else %}
-        {% if metadata_hashes is not none and elementary.get_config_var("cache_artifacts") %}
+        {% set upload_artifact_method = get_upload_artifact_method(table_relation, metadata_hashes) %}
+        {% if upload_artifact_method.type == "diff" %}
             {% do elementary.file_log("[{}] Comparing the artifacts state.".format(table_relation.identifier)) %}
             {% set new_metadata_hashes = flatten_artifact_dicts | map(attribute="metadata_hash") | sort %}
-            {% if new_metadata_hashes == metadata_hashes %}
+            {% if new_metadata_hashes == upload_artifact_method.metadata_hashes %}
                 {% do elementary.file_log("[{}] Artifacts did not change.".format(table_relation.identifier)) %}
             {% else %}
                 {% do elementary.file_log("[{}] Artifacts changed.".format(table_relation.identifier)) %}
-                {% set upload_artifacts_method = elementary.get_config_var("upload_artifacts_method") %}
-                {% if upload_artifacts_method == "diff" %}
-                    {% set added_artifacts = flatten_artifact_dicts | rejectattr("metadata_hash", "in", metadata_hashes) | list %}
-                    {% set removed_artifact_hashes = metadata_hashes | reject("in", new_metadata_hashes) | list %}
-                    {% do elementary.delete_and_insert(table_relation, insert_rows=added_artifacts, delete_values=removed_artifact_hashes, delete_column_key="metadata_hash") %}
-                {% elif upload_artifacts_method == "replace" %}
-                    {% do elementary.replace_table_data(table_relation, flatten_artifact_dicts) %}
-                {% else %}
-                    {% do exceptions.raise_compiler_error("Invalid var('upload_artifacts_method') provided.") %}
-                {% endif %}
+                {% set added_artifacts = flatten_artifact_dicts | rejectattr("metadata_hash", "in", upload_artifact_method.metadata_hashes) | list %}
+                {% set removed_artifact_hashes = upload_artifact_method.metadata_hashes | reject("in", new_metadata_hashes) | list %}
+                {% do elementary.delete_and_insert(table_relation, insert_rows=added_artifacts, delete_values=removed_artifact_hashes, delete_column_key="metadata_hash") %}
             {% endif %}
         {% else %}
             {% do elementary.replace_table_data(table_relation, flatten_artifact_dicts) %}
         {% endif %}
+    {% endif %}
+{% endmacro %}
+
+{% macro get_upload_artifact_method(table_relation, metadata_hashes) %}
+    {% if not elementary.get_config_var("cache_artifacts") %}
+        {% do return({"type": "replace"}) %}
+    {% endif %}
+
+    {% set upload_artifacts_method = elementary.get_config_var("upload_artifacts_method") %}
+    {% if upload_artifacts_method == 'diff' %}
+        {% set metadata_hashes = metadata_hashes if metadata_hashes is not none else elementary.get_artifacts_hashes_for_model(table_relation.name) %}
+        {% if metadata_hashes is not none %}
+            {% do return({"type": "diff", "metadata_hashes": metadata_hashes}) %}
+        {% else %}
+            {% do return({"type": "replace"}) %}
+        {% endif %}
+    {% elif upload_artifacts_method == "replace" %}
+        {% do return({"type": "replace"}) %}
+    {% else %}
+        {% do exceptions.raise_compiler_error("Invalid var('upload_artifacts_method') provided.") %}
     {% endif %}
 {% endmacro %}
