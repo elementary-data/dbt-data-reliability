7278562233b1b09f4713435650ca5c747fff9d94
Chris Dong <86695140+dongchris@users.noreply.github.com>
2024-03-05 09:35:31 -0800

    improvement: bigquery specific for query_table_metrics
    Using information schema to get row count is much more performant than doing a full table scan

--- macros/edr/materializations/model/metrics.sql
+++ macros/edr/materializations/model/metrics.sql
@@ -22,6 +22,30 @@
   {% do return(metrics) %}
 {% endmacro %}
 
+{% macro bigquery__query_table_metrics() %}
+    select
+      {{ modules.datetime.datetime.utcnow().timestamp() }} as build_timestamp,
+      row_count
+    from {{ this.schema }}.__TABLES__
+    where table_id = '{{ this.name }}'
+  {% endset %}
+
+  {% set metrics = [] %}
+  {% for metric_column in elementary.run_query(query).columns %}
+    {% set metric_name = metric_column.name %}
+    {% set metric_value = metric_column[0] %}
+    {% do metrics.append({
+      "id": "{}.{}".format(invocation_id, this),
+      "full_table_name": elementary.relation_to_full_name(this),
+      "column_name": none,
+      "metric_name": metric_name,
+      "metric_value": metric_value,
+      "updated_at": elementary.datetime_now_utc_as_string()
+    }) %}
+  {% endfor %}
+  {% do return(metrics) %}
+{% endmacro %}
+  
 {% macro can_query_metrics() %}
   {% if not elementary.get_config_var('collect_metrics') %}
     {% do return(false) %}
