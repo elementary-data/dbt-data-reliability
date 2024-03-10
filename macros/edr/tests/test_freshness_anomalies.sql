e216cd35e43b035370379409ddb131b89565379b
Ofek Weiss <ofek1weiss@gmail.com>
2024-03-10 11:15:37 +0200

    added ignore_small_changes to freshness and event_freshness

--- macros/edr/tests/test_freshness_anomalies.sql
+++ macros/edr/tests/test_freshness_anomalies.sql
@@ -1,4 +1,4 @@
-{% test freshness_anomalies(model, timestamp_column, where_expression, anomaly_sensitivity, anomaly_direction, min_training_set_size, time_bucket, days_back, backfill_days, seasonality, sensitivity, detection_delay, anomaly_exclude_metrics, detection_period, training_period) %}
+{% test freshness_anomalies(model, timestamp_column, where_expression, anomaly_sensitivity, anomaly_direction, min_training_set_size, time_bucket, days_back, backfill_days, seasonality, sensitivity, ignore_small_changes, detection_delay, anomaly_exclude_metrics, detection_period, training_period) %}
   {{ config(tags = ['elementary-tests']) }}
   {{ elementary.test_table_anomalies(
       model=model,
@@ -14,6 +14,7 @@
       mandatory_params=['timestamp_column'],
       seasonality=seasonality,
       sensitivity=sensitivity,
+      ignore_small_changes=ignore_small_changes,
       detection_delay=detection_delay,
       anomaly_exclude_metrics=anomaly_exclude_metrics,
       detection_period=detection_period,
