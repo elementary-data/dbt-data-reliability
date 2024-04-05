d8837c4a9c2b2903aaa46fbd1082f9411473ae3e
Konstantin Baidin <128369285+konstantin-baidin-y42@users.noreply.github.com>
2024-04-05 16:40:12 +0200

    test_freshness_anomalies - support dbt_model_id (#12)

--- macros/edr/tests/test_freshness_anomalies.sql
+++ macros/edr/tests/test_freshness_anomalies.sql
@@ -1,4 +1,4 @@
-{% test freshness_anomalies(model, timestamp_column, where_expression, anomaly_sensitivity, anomaly_direction, min_training_set_size, time_bucket, days_back, backfill_days, seasonality, sensitivity, detection_delay, anomaly_exclude_metrics, detection_period, training_period) %}
+{% test freshness_anomalies(model, timestamp_column, where_expression, anomaly_sensitivity, anomaly_direction, min_training_set_size, time_bucket, days_back, backfill_days, seasonality, sensitivity, detection_delay, anomaly_exclude_metrics, detection_period, training_period, dbt_model_id) %}
   {{ elementary.test_table_anomalies(
       model=model,
       table_anomalies=["freshness"],
@@ -16,7 +16,8 @@
       detection_delay=detection_delay,
       anomaly_exclude_metrics=anomaly_exclude_metrics,
       detection_period=detection_period,
-      training_period=training_period
+      training_period=training_period,
+      dbt_model_id=dbt_model_id
     )
   }}
 {% endtest %}
