25bd3e03c881a8c86541134b3a9f7b701fb4d344
Daniel Pollak <daniel@elementary-data.com>
2024-04-11 16:19:18 +0300

    fix redshift

--- integration_tests/tests/test_anomalies_ranges.py
+++ integration_tests/tests/test_anomalies_ranges.py
@@ -34,7 +34,6 @@ def get_latest_anomaly_test_points(dbt_project: DbtProject, test_id: str):
 
 
 def test_anomaly_ranges_are_valid(test_id: str, dbt_project: DbtProject):
-    dbt_project.dbt_runner.run()
     utc_today = datetime.utcnow().date()
     test_date, *training_dates = generate_dates(base_date=utc_today - timedelta(1))
 
@@ -70,7 +69,6 @@ def test_anomaly_ranges_are_valid(test_id: str, dbt_project: DbtProject):
 def test_anomaly_ranges_are_valid_with_seasonality(
     test_id: str, dbt_project: DbtProject
 ):
-    dbt_project.dbt_runner.run(capture_output=True)
     utc_today = datetime.utcnow().date()
     test_date, *training_dates = generate_dates(
         base_date=utc_today - timedelta(1), days_back=7 * 14
