b411e13a435e56b5d877cfaf3acf02c14345ad48
Michael Connell <connellmp@gmail.com>
2024-04-23 14:03:35 -0700

    Update get_relation_max_length for redshift from 127 to 125 because of duplicate quoting
    explained in [this issue](https://github.com/elementary-data/dbt-data-reliability/issues/695)

--- macros/utils/table_operations/get_relation_max_length.sql
+++ macros/utils/table_operations/get_relation_max_length.sql
@@ -12,7 +12,7 @@
 {% endmacro %}
 
 {% macro redshift__get_relation_max_name_length(temporary, relation, sql_query) %}
-    {{ return(127) }}
+    {{ return(125) }}
 {% endmacro %}
 
 {% macro postgres__get_relation_max_name_length(temporary, relation, sql_query) %}
