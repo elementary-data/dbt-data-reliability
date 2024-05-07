929b0b9af4aedb7795e3986c0705022c3809d067
Itamar Hartstein <haritamar@gmail.com>
2024-05-07 17:52:25 +0300

    get_run_result_dict: change to a more accurate version check

--- macros/utils/graph/get_run_result_dict.sql
+++ macros/utils/graph/get_run_result_dict.sql
@@ -1,5 +1,8 @@
 {% macro get_run_result_dict(run_result) %}
-    {% if dbt_version <= "1.8" %}
+    {% set major, minor, revision = dbt_version.split(".") %}
+    {% set major = major | int %}
+    {% set minor = minor | int %}
+    {% if major < 1 or major == 1 and minor < 8 %}
         {% do return(run_result.to_dict()) %}
     {% else %}
         {# There's a bug in dbt 1.8 which causes run_result.to_dict to fail on an exception #}
