d5b2a42320df241d84a6d83addcf3239461efb27
Itamar Hartstein <haritamar@gmail.com>
2024-05-08 02:18:14 +0300

    test_dimension_anomalies: bugfix - only anomalous rows should be stored for dimension anomalies

--- integration_tests/tests/test_dimension_anomalies.py
+++ integration_tests/tests/test_dimension_anomalies.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime, timedelta
 from typing import Any, Dict, List
 
@@ -7,6 +8,26 @@ from dbt_project import DbtProject
 TIMESTAMP_COLUMN = "updated_at"
 DBT_TEST_NAME = "elementary.dimension_anomalies"
 DBT_TEST_ARGS = {"timestamp_column": TIMESTAMP_COLUMN, "dimensions": ["superhero"]}
+
+# This returns data points used in the latest anomaly test
+ANOMALY_TEST_POINTS_QUERY = """
+    with latest_elementary_test_result as (
+        select id
+        from {{{{ ref("elementary_test_results") }}}}
+        where lower(table_name) = lower('{test_id}')
+        order by created_at desc
+        limit 1
+    )
+
+    select result_row
+    from {{{{ ref("test_result_rows") }}}}
+    where elementary_test_results_id in (select * from latest_elementary_test_result)
+"""
+
+
+def get_latest_anomaly_test_points(dbt_project: DbtProject, test_id: str):
+    results = dbt_project.run_query(ANOMALY_TEST_POINTS_QUERY.format(test_id=test_id))
+    return [json.loads(result["result_row"]) for result in results]
 
 
 def test_anomalyless_dimension_anomalies(test_id: str, dbt_project: DbtProject):
@@ -21,6 +42,10 @@ def test_anomalyless_dimension_anomalies(test_id: str, dbt_project: DbtProject):
     ]
     test_result = dbt_project.test(test_id, DBT_TEST_NAME, DBT_TEST_ARGS, data=data)
     assert test_result["status"] == "pass"
+
+    # Dimension anomalies only stores anomalous rows (unlike other anomaly tests) - so we should get 0 rows for a passing test.
+    anomaly_test_points = get_latest_anomaly_test_points(dbt_project, test_id)
+    assert len(anomaly_test_points) == 0
 
 
 def test_dimension_anomalies_with_timestamp_as_sql_expression(
@@ -65,6 +90,13 @@ def test_anomalous_dimension_anomalies(test_id: str, dbt_project: DbtProject):
 
     test_result = dbt_project.test(test_id, DBT_TEST_NAME, DBT_TEST_ARGS, data=data)
     assert test_result["status"] == "fail"
+
+    # Dimension anomalies only stores anomalous rows (unlike other anomaly tests) - so we should only get 1 row with the problematic value
+    anomaly_test_points = get_latest_anomaly_test_points(dbt_project, test_id)
+    assert len(anomaly_test_points) == 1
+    assert anomaly_test_points[0]["is_anomalous"]
+    assert anomaly_test_points[0]["dimension"] == "superhero"
+    assert anomaly_test_points[0]["dimension_value"] == "Superman"
 
 
 def test_dimensions_anomalies_with_where_parameter(
