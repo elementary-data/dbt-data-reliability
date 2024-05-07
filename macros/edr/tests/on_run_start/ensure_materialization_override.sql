ab766616f838a4263032b04cc95cbc21823dff7c
Itamar Hartstein <haritamar@gmail.com>
2024-05-07 16:06:15 +0300

    change warning message for the flag

--- macros/edr/tests/on_run_start/ensure_materialization_override.sql
+++ macros/edr/tests/on_run_start/ensure_materialization_override.sql
@@ -14,7 +14,15 @@
     {% set minor = minor | int %}
     {% if major > 1 or major == 1 and minor >= 8 %}
         {%- set msg %}
-AAAA
+IMPORTANT - Starting from dbt 1.8, users must explicitly allow packages to override materializations. Elementary requires this ability to support collection of samples and failed row count for dbt tests.
+Please add the following flag to dbt_project.yml to allow it:
+
+flags:
+  require_explicit_package_overrides_for_builtin_materializations: false
+
+Notes - 
+* This is a temporary measure that will result in a deprecation warning, please ignore it for now. Elementary is working with the dbt-core team on a more permanent solution.
+* This message can be muted by setting the 'mute_ensure_materialization_override' var to true.
         {% endset %}
         {% do log(msg, info=true) %}
     {% endif %}
