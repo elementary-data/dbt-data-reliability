4315be0dff987757acd080bdc74a550257559ab6
konstantin-baidin-y42 <konstantin.baidin@y42.com>
2024-05-29 10:36:47 +0200

    add nodes

--- macros/utils/graph/get_elementary_config_from_node.sql
+++ macros/utils/graph/get_elementary_config_from_node.sql
@@ -2,9 +2,13 @@
     {% set res = {} %}
     {% set node_config = node.get('config') %}
     {% if node_config %}
+        {% do elementary.edr_log('Node config exists') %}
         {% set elementary_config = node.config.get('elementary') %}
         {% if elementary_config and elementary_config is mapping %}
+            {% do elementary.edr_log('Elementary config in node config exists') %}
             {% do res.update(elementary_config) %}
+        {% else %}
+            {% do elementary.edr_log('Elementary config in node config does not exist') %}
         {% endif %}
         {% set config_meta = node.config.get('meta') %}
         {% if config_meta and config_meta is mapping %}
@@ -13,13 +17,20 @@
                 {% do res.update(elementary_config) %}
             {% endif %}
         {% endif %}
+    {% else %}
+        {% do elementary.edr_log('Node config does not exist') %}
     {% endif %}
     {% set node_meta = node.get('meta') %}
     {% if node_meta and node_meta is mapping %}
+        {% do elementary.edr_log('Node meta exists') %}
         {% set elementary_config = node_meta.get('elementary') %}
         {% if elementary_config and elementary_config is mapping %}
             {% do res.update(elementary_config) %}
         {% endif %}
+    {% else %}
+        {% do elementary.edr_log('Node meta does not exist') %}
     {% endif %}
+    {% do elementary.edr_log('Print node:') %}
+    {% do elementary.edr_log(node) %}
     {{ return(res) }}
 {% endmacro %}
