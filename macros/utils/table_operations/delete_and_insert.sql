044d5a8e4cdd06ac11ce4dc021d1e49cfcfdb528
rloredo <rodrigo.loredo@vinted.com>
2025-01-03 13:47:59 +0100

    do it the same as in trino

--- macros/utils/table_operations/delete_and_insert.sql
+++ macros/utils/table_operations/delete_and_insert.sql
@@ -134,16 +134,24 @@
 {% endmacro %}
 
 {% macro bigquery__get_delete_and_insert_queries(relation, insert_relation, delete_relation, delete_column_key) %}
-    {% set query %}
-        {% if delete_relation %}
+    {% set queries = [] %}
+
+    {% if delete_relation %}
+        {% set delete_query %}
             delete from {{ relation }}
             where
             {{ delete_column_key }} is null
             or {{ delete_column_key }} in (select {{ delete_column_key }} from {{ delete_relation }});
-        {% endif %}
-        {% if insert_relation %}
+        {% endset %}
+        {% do queries.append(delete_query) %}
+    {% endif %}
+
+    {% if insert_relation %}
+        {% set insert_query %}
             insert into {{ relation }} select * from {{ insert_relation }};
-        {% endif %}
-    {% endset %}
-    {% do return([query]) %}
+        {% endset %}
+        {% do queries.append(insert_query) %}
+    {% endif %}
+
+    {% do return(queries) %}
 {% endmacro %}
