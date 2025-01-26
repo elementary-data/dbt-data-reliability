5587002ae81c78ffb26b5c2b878fb78431d790b9
999999333 <kvitekcrack@gmail.com>
2025-01-26 05:02:41 +0100

    Explicit group by def for cross-db compatibility

--- models/edr/data_monitoring/anomaly_detection/metrics_anomaly_score.sql
+++ models/edr/data_monitoring/anomaly_detection/metrics_anomaly_score.sql
@@ -32,7 +32,19 @@ time_window_aggregation as (
         last_value(bucket_end) over (partition by metric_name, full_table_name, column_name order by bucket_start asc rows between unbounded preceding and current row) training_end,
         first_value(bucket_end) over (partition by metric_name, full_table_name, column_name order by bucket_start asc rows between unbounded preceding and current row) as training_start
     from data_monitoring_metrics
-    {{ dbt_utils.group_by(12) }}
+    group by
+        id,
+        full_table_name,
+        column_name,
+        dimension,
+        dimension_value,
+        metric_name,
+        metric_value,
+        source_value,
+        bucket_start,
+        bucket_end,
+        bucket_duration_hours,
+        updated_at
 ),
 
 metrics_anomaly_score as (
@@ -63,9 +75,21 @@ metrics_anomaly_score as (
             metric_value is not null
             and training_avg is not null
             and bucket_end >= {{ elementary.edr_timeadd('day', '-7', elementary.edr_date_trunc('day', elementary.edr_current_timestamp())) }}
-    {{ dbt_utils.group_by(15) }}
-    order by bucket_end desc
-
+    group by
+        id,
+        full_table_name,
+        column_name,
+        dimension,
+        dimension_value,
+        metric_name,
+        metric_value,
+        bucket_start,
+        bucket_end,
+        training_avg,
+        training_stddev,
+        training_start,
+        training_end,
+        training_set_size
 
 ),
 
