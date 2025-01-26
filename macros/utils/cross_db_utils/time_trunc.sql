0e09b7628e2c411da05c4e640367fbc9250ccc8b
999999333 <kvitekcrack@gmail.com>
2025-01-26 15:24:51 +0100

    date and time compatibility

--- macros/utils/cross_db_utils/time_trunc.sql
+++ macros/utils/cross_db_utils/time_trunc.sql
@@ -10,3 +10,7 @@
 {% macro bigquery__edr_time_trunc(date_part, date_expression) %}
     timestamp_trunc(cast({{ date_expression }} as timestamp), {{ date_part }})
 {% endmacro %}
+
+{% macro fabric__edr_time_trunc(date_part, date_expression) %}
+    datetrunc({{date_part}}, cast({{ date_expression }} as {{ elementary.edr_type_timestamp() }}))
+{% endmacro %}
