eb207be33b90ba054e76e13f29c22e2f3bae2b0d
oravi <oravi90@gmail.com>
2025-03-12 12:17:50 +0200

    Add default value for llm_model_name parameter
    Updated `test_ai_data_validation` and `test_unstructured_data_validation` macros to set a default value of `none` for the `llm_model_name` parameter, providing more flexibility in AI data validation tests.

--- macros/edr/tests/test_unstructured_data_validation.sql
+++ macros/edr/tests/test_unstructured_data_validation.sql
@@ -1,4 +1,4 @@
-{% test unstructured_data_validation(model, column_name, expectation_prompt, llm_model_name) %}
+{% test unstructured_data_validation(model, column_name, expectation_prompt, llm_model_name=none) %}
     {{ config(tags = ['elementary-tests']) }}
     {% set context = "You are a data validator specializing in validating unstructured data." %}
     {{ return(elementary.test_ai_data_validation(model, column_name, expectation_prompt, llm_model_name, context)) }}
