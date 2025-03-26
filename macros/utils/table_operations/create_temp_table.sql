9b945b9d55e45cf45d5342ec5b7c90f88892b6d3
GuyEshdat <guy.eshdat@gmail.com>
2025-03-26 14:50:41 +0200

    using an adapter for creating temp tables in snowflake beacuse dbt-snoflake allows to create tables only in materialization scope. this logic waws moved from the elementary cli to here to avoid a confusion between the different implemntations

--- macros/utils/table_operations/create_temp_table.sql
+++ macros/utils/table_operations/create_temp_table.sql
@@ -1,4 +1,8 @@
 {% macro create_temp_table(database_name, schema_name, table_name, sql_query) %}
+    {% do return(adapter.dispatch('create_temp_table','elementary')(database_name, schema_name, table_name, sql_query)) %}
+{%- endmacro %}
+
+{% macro default__create_temp_table(database_name, schema_name, table_name, sql_query) %}
     {% set temp_table_exists, temp_table_relation = dbt.get_or_create_relation(database=database_name,
                                                                                schema=schema_name,
                                                                                identifier=table_name,
@@ -10,5 +14,23 @@
     {% else %}
         {% do elementary.run_query(dbt.create_table_as(True, temp_table_relation, sql_query)) %}
     {% endif %}
+    {{ return(temp_table_relation) }}{% endmacro %}
+
+{% macro snowflake__create_temp_table(database_name, schema_name, table_name, sql_query) %}
+    {% set temp_table_exists, temp_table_relation = dbt.get_or_create_relation(database=database_name,
+                                                                               schema=schema_name,
+                                                                               identifier=table_name,
+                                                                               type='table') -%}
+    {% set temp_table_relation = elementary.make_temp_table_relation(temp_table_relation) %}
+    {% set create_query %}
+        create or replace temporary table {{ temp_table_relation }} 
+        as (
+            {{ sql_query }}
+        );
+        
+    {% endset %}
+
+    {% do elementary.run_query(create_query) %}
+    
     {{ return(temp_table_relation) }}
 {% endmacro %}
