c0b0dbf646a377522356cef8e7d2549f2f449c94
Michael Myaskovsky <michael@elementary-data.com>
2025-03-27 18:04:28 +0200

    dbt run works on clickhouse

--- macros/utils/table_operations/delete_and_insert.sql
+++ macros/utils/table_operations/delete_and_insert.sql
@@ -54,6 +54,28 @@
     {% do return([query]) %}
 {% endmacro %}
 
+{% macro clickhouse__get_delete_and_insert_queries(relation, insert_relation, delete_relation, delete_column_key) %}
+    {% set queries = [] %}
+
+    {% if delete_relation %}
+        {% set delete_query %}
+            alter table {{ relation }} delete where
+            {{ delete_column_key }} is null
+            or {{ delete_column_key }} in (select {{ delete_column_key }} from {{ delete_relation }});
+        {% endset %}
+        {% do queries.append(delete_query) %}
+    {% endif %}
+
+    {% if insert_relation %}
+        {% set insert_query %}
+            insert into {{ relation }} select * from {{ insert_relation }};
+        {% endset %}
+        {% do queries.append(insert_query) %}
+    {% endif %}
+
+    {% do return(queries) %}
+{% endmacro %}
+
 {% macro spark__get_delete_and_insert_queries(relation, insert_relation, delete_relation, delete_column_key) %}
     {% set queries = [] %}
 
