9a1e6a6760f7822938541269c509b94f22042420
GuyEshdat <guy.eshdat@gmail.com>
2025-05-08 18:25:59 +0300

    renamed to edr_make_intermediate_relation because make_intermediate_relation is an internal dbt macro

--- macros/utils/table_operations/create_intermediate_relation.sql
+++ macros/utils/table_operations/create_intermediate_relation.sql
@@ -1,5 +1,5 @@
 {% macro create_intermediate_relation(base_relation, rows, temporary, like_columns=none) %}
-    {% set int_relation = elementary.make_intermediate_relation(base_relation).incorporate(type='table') %}
+    {% set int_relation = elementary.edr_make_intermediate_relation(base_relation).incorporate(type='table') %}
 
     {% if not elementary.has_temp_table_support() %}
         {% set temporary = false %}
@@ -10,15 +10,15 @@
     {% do return(int_relation) %}
 {% endmacro %}
 
-{% macro make_intermediate_relation(base_relation) %}
-    {% do return(adapter.dispatch("make_intermediate_relation", "elementary")(base_relation)) %}
+{% macro edr_make_intermediate_relation(base_relation) %}
+    {% do return(adapter.dispatch("edr_make_intermediate_relation", "elementary")(base_relation)) %}
 {% endmacro %}
 
-{% macro default__make_intermediate_relation(base_relation) %}
+{% macro default__edr_make_intermediate_relation(base_relation) %}
     {% do return(elementary.make_temp_table_relation(base_relation)) %}
 {% endmacro %}
 
-{% macro databricks__make_intermediate_relation(base_relation) %}
+{% macro databricks__edr_make_intermediate_relation(base_relation) %}
     {% set tmp_identifier = elementary.table_name_with_suffix(base_relation.identifier, elementary.get_timestamped_table_suffix()) %}
     {% set tmp_relation = api.Relation.create(
         identifier=tmp_identifier,
