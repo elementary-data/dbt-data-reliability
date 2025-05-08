13acaa1bb732ba8292832d023d6df59c0dec4f96
GuyEshdat <guy.eshdat@gmail.com>
2025-05-08 15:20:59 +0300

    seperating the create_intermediate_relation from the make_temp_table_relation, becuse we want different databricks behaviors in these cases

--- macros/utils/table_operations/make_temp_relation.sql
+++ macros/utils/table_operations/make_temp_relation.sql
@@ -45,16 +45,6 @@
     {% do return(elementary.edr_make_temp_relation(base_relation, suffix)) %}
 {% endmacro %}
 
-{% macro databricks__make_temp_table_relation(base_relation, suffix) %}
-    {% set tmp_identifier = elementary.table_name_with_suffix(base_relation.identifier, suffix) %}
-    {% set tmp_relation = api.Relation.create(
-        identifier=tmp_identifier,
-        schema=base_relation.schema,
-        database=base_relation.database,
-        type='table') %}
-    {% do return(tmp_relation) %}
-{% endmacro %}
-
 {% macro clickhouse__make_temp_table_relation(base_relation, suffix) %}
     {% set tmp_identifier = elementary.table_name_with_suffix(base_relation.identifier, suffix) %}
     {% set tmp_relation = api.Relation.create(
