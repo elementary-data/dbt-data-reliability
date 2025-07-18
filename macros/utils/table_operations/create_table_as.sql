d3579164c10fac67dd700fb1a59cacf8aada5673
Itamar Hartstein <haritamar@gmail.com>
2025-07-18 19:24:09 +0300

    create_table_as - bugfix for postgres

--- macros/utils/table_operations/create_table_as.sql
+++ macros/utils/table_operations/create_table_as.sql
@@ -13,9 +13,25 @@
     as {{ sql_query }}
   {% endset %}
 
+  {% set create_query = elementary.edr_get_create_table_as_sql(temporary, relation, sql_query) %}
   {% do elementary.run_query(create_query) %}
 
   {% if should_commit %}
     {% do adapter.commit() %}    
   {% endif %}
+{% endmacro %}
+
+
+{% macro edr_get_create_table_as_sql(temporary, relation, sql_query) %}
+  {{ return(adapter.dispatch("edr_get_create_table_as_sql", "elementary")(temporary, relation, sql_query)) }}
+{% endmacro %}
+
+{% macro default__edr_get_create_table_as_sql(temporary, relation, sql_query) %}
+  create or replace {% if temporary %} temporary {% endif %} table {{ relation }}
+  as {{ sql_query }}
+{% endmacro %}
+
+{% macro postgres__edr_get_create_table_as_sql(temporary, relation, sql_query) %}
+  create {% if temporary %} temporary {% endif %} table {{ relation }}
+  as {{ sql_query }}
 {% endmacro %}
