c7615407081cc070665f71b36182d02c9a63c166
arbiv <yosef@elementary-data.com>
2025-07-31 14:09:28 +0300

    removed unnecessary change

--- macros/edr/system/configuration/is_elementary_enabled.sql
+++ macros/edr/system/configuration/is_elementary_enabled.sql
@@ -1,6 +1,3 @@
 {% macro is_elementary_enabled() %}
-  {% if var('elementary_enabled', none) is not none %}
-    {% do return(var('elementary_enabled')) %}
-  {% endif %}
   {% do return("elementary" in graph) %}
 {% endmacro %}
