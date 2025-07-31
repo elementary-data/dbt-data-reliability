a1868a71f00603a44ee45eeff1d4f3cb6660c6b2
arbiv <yosef@elementary-data.com>
2025-07-31 11:49:14 +0300

    tags case insesitiveity

--- integration_tests/tests/test_sampling_pii.py
+++ integration_tests/tests/test_sampling_pii.py
@@ -53,10 +53,9 @@ def test_sampling_pii_disabled(test_id: str, dbt_project: DbtProject):
 
 
 @pytest.mark.skip_targets(["clickhouse"])
-def test_sampling_pii_disabled_with_default_config(
+def test_sampling_pii_disabled_with_default_config_and_casing(
     test_id: str, dbt_project: DbtProject
 ):
-    """Test that PII-tagged tables don't upload samples even when tests fail"""
     null_count = 50
     data = [{COLUMN_NAME: None} for _ in range(null_count)]
 
@@ -66,7 +65,7 @@ def test_sampling_pii_disabled_with_default_config(
         dict(column_name=COLUMN_NAME),
         data=data,
         as_model=True,
-        model_config={"config": {"tags": ["pii"]}},
+        model_config={"config": {"tags": ["pIi"]}},
         test_vars={
             "enable_elementary_test_materialization": True,
             "test_sample_row_count": TEST_SAMPLE_ROW_COUNT,
--- macros/edr/system/system_utils/is_pii_table.sql
+++ macros/edr/system/system_utils/is_pii_table.sql
@@ -4,8 +4,11 @@
     {% do return(false) %}
   {% endif %}
   
-  {% set pii_tags = elementary.get_config_var('pii_tags') %}
-  {% set model_tags = elementary.insensitive_get_dict_value(flattened_test, 'model_tags', []) %}
+  {% set raw_pii_tags = elementary.get_config_var('pii_tags') %}
+  {% set pii_tags = (raw_pii_tags if raw_pii_tags is iterable else [raw_pii_tags]) | map('lower') | list %}
+  
+  {% set raw_model_tags = elementary.insensitive_get_dict_value(flattened_test, 'model_tags', []) %}
+  {% set model_tags = (raw_model_tags if raw_model_tags is iterable else [raw_model_tags]) | map('lower') | list %}
   
   {% set intersection = elementary.lists_intersection(model_tags, pii_tags) %}
   {% set is_pii = intersection | length > 0 %}
