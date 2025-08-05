3e251ccffef95c9eec054f5b18b1befa2865ebe7
GuyEshdat <guy.eshdat@gmail.com>
2025-08-05 16:07:50 +0300

    macro for generating schema names of dremio in tests, needed because dremio schema names are dymainc (depends if model is table or not

--- integration_tests/dbt_project/macros/generate_schema_name.sql
+++ integration_tests/dbt_project/macros/generate_schema_name.sql
@@ -1,13 +1,39 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set schema_name = target.schema -%}
-    {% if custom_schema_name %}
-        {% set schema_name = "{}_{}".format(schema_name, custom_schema_name) %}
-    {% endif %}
-
+    {% set schema_name = get_default_schema_name(custom_schema_name, node) %}
     {% set schema_name_suffix_by_var = var('schema_name_suffix', '') %}
     {% if schema_name_suffix_by_var %}
         {% set schema_name = schema_name + schema_name_suffix_by_var %}
     {% endif %}
 
     {% do return(schema_name) %}
+{%- endmacro %}
+
+{% macro get_default_schema_name(custom_schema_name, node) -%}
+    {% do return(adapter.dispatch('get_default_schema_name', 'elementary_tests')(custom_schema_name, node)) %}
+{% endmacro %}
+
+{% macro default__get_default_schema_name(custom_schema_name, node) -%}
+    {%- set schema_name = target.schema -%}
+    {% if custom_schema_name %}
+        {% set schema_name = "{}_{}".format(schema_name, custom_schema_name) %}
+    {% endif %}
+    {% do return(schema_name) %}
+{%- endmacro %}
+
+{% macro dremio__get_default_schema_name(custom_schema_name, node) -%}
+  {%- set default_schema = target.schema if not is_datalake_node(node)
+    else target.root_path -%}
+  {%- if custom_schema_name is none -%}
+
+      {% do return(default_schema) %}
+
+  {%- elif default_schema == 'no_schema' -%}
+
+      {% do return(custom_schema_name) %}
+
+  {%- else -%}
+
+      {% do return("{}.{}".format(default_schema, custom_schema_name)) %}
+
+  {%- endif -%}
 {%- endmacro %}
