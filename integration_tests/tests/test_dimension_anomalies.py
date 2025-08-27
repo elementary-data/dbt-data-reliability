b46d220a21487a926f1cb50f5dc4c77e321f74c2
Noy Arie <noyarie1992@gmail.com>
2025-08-27 10:28:51 +0300

    Ele 4942 dimension anomalies visualization (#847)
    * fix
    
    * return metrics only if fail
    
    * fix tests
    
    * fix tests
    
    * only anomaly dim values
    
    * undo change
    
    * dim anomalies should return the data points of failed dimension values
    
    * nit
    
    * fix number of failed rows

--- integration_tests/tests/test_dimension_anomalies.py
+++ integration_tests/tests/test_dimension_anomalies.py
@@ -86,6 +86,7 @@ def test_anomalous_dimension_anomalies(test_id: str, dbt_project: DbtProject):
         }
         for superhero in ["Superman", "Superman", "Superman", "Spiderman"]
     ]
+
     data += [
         {
             TIMESTAMP_COLUMN: cur_date.strftime(DATE_FORMAT),
@@ -98,12 +99,19 @@ def test_anomalous_dimension_anomalies(test_id: str, dbt_project: DbtProject):
     test_result = dbt_project.test(test_id, DBT_TEST_NAME, DBT_TEST_ARGS, data=data)
     assert test_result["status"] == "fail"
 
-    # Dimension anomalies only stores anomalous rows (unlike other anomaly tests) - so we should only get 1 row with the problematic value
     anomaly_test_points = get_latest_anomaly_test_points(dbt_project, test_id)
-    assert len(anomaly_test_points) == 1
-    assert anomaly_test_points[0]["is_anomalous"]
-    assert anomaly_test_points[0]["dimension"] == "superhero"
-    assert anomaly_test_points[0]["dimension_value"] == "Superman"
+
+    # Only dimension values with anomalies are stored in the test points
+    dimension_values = set([x["dimension_value"] for x in anomaly_test_points])
+
+    superman_anomaly_test_points = [
+        x for x in anomaly_test_points if x["dimension_value"] == "Superman"
+    ]
+
+    assert len(dimension_values) == 1
+    assert "Superman" in dimension_values
+    assert len(anomaly_test_points) == len(superman_anomaly_test_points)
+    assert any(x["is_anomalous"] for x in superman_anomaly_test_points)
 
 
 # Anomalies currently not supported on ClickHouse
