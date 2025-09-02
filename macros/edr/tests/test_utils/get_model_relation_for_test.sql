8f5e5a7ebc825151541de60a8efe5a83dc8b9ed8
Itamar Hartstein <haritamar@gmail.com>
2025-09-03 01:57:55 +0300

    where parameter logic fix

--- macros/edr/tests/test_utils/get_model_relation_for_test.sql
+++ macros/edr/tests/test_utils/get_model_relation_for_test.sql
@@ -1,4 +1,10 @@
 {% macro get_model_relation_for_test(test_model, test_node) %}
+    {% set depends_on_node_ids = test_node.get("depends_on", {}).get("nodes") %}
+    {% if depends_on_node_ids and depends_on_node_ids | length == 1 %}
+        {% set node = elementary.get_node(depends_on_node_ids[0]) %}
+        {% do return(elementary.get_relation_from_node(node)) %}
+    {% endif %}
+
     {# If the test model is not a string, then return as-is #}
     {% if test_model is not string %}
         {% do return(test_model) %}
