d2afa5ae286d11a13cfa68735467d1a2d7178f41
arbiv <yosef@elementary-data.com>
2025-09-09 18:21:56 +0300

    removed render_pure() when deleting table since quoting is needed

--- macros/edr/tests/test_utils/clean_elementary_test_tables.sql
+++ macros/edr/tests/test_utils/clean_elementary_test_tables.sql
@@ -42,7 +42,7 @@
 {% macro athena__get_clean_elementary_test_tables_queries(test_table_relations) %}
     {% set queries = [] %}
     {% for test_relation in test_table_relations %}
-        {% do queries.append("DROP TABLE IF EXISTS {}".format(test_relation.render_pure())) %}
+        {% do queries.append("DROP TABLE IF EXISTS {}".format(test_relation)) %}
     {% endfor %}
     {% do return(queries) %}
 {% endmacro %}
