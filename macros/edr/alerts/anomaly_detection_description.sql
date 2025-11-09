db1ea972a990567f5389c1bfe379bf784d7cdee5
GuyEshdat <81393741+GuyEshdat@users.noreply.github.com>
2025-11-09 18:32:26 +0200

    made the description of the anomaly test more accurate (#883)
    * made the description of the anomaly test more accurate

--- macros/edr/alerts/anomaly_detection_description.sql
+++ macros/edr/alerts/anomaly_detection_description.sql
@@ -10,7 +10,7 @@
 {% endmacro %}
 
 {% macro freshness_description() %}
-    'Last update was at ' || anomalous_value || ', ' || {{ elementary.edr_cast_as_string('abs(round(' ~ elementary.edr_cast_as_numeric('metric_value/3600') ~ ', 2))') }} || ' hours ago. Usually the table is updated within ' || {{ elementary.edr_cast_as_string('abs(round(' ~ elementary.edr_cast_as_numeric('training_avg/3600') ~ ', 2))') }} || ' hours.'
+    'Last update was at ' || anomalous_value || ', ' || 'which is' || {{ elementary.edr_cast_as_string('abs(round(' ~ elementary.edr_cast_as_numeric('metric_value/3600') ~ ', 2))') }} || ' hours without updates (only full buckets are considered). Usually the table is updated within ' || {{ elementary.edr_cast_as_string('abs(round(' ~ elementary.edr_cast_as_numeric('training_avg/3600') ~ ', 2))') }} || ' hours.'
 {% endmacro %}
 
 {% macro table_metric_description() %}
