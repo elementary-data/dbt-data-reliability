aa958549026635c41cc2acc89d3516200226f537
Itamar Hartstein <haritamar@gmail.com>
2026-01-01 15:23:16 +0200

    remove unique key for test_result_rows, as it is not really unique

--- models/edr/run_results/test_result_rows.sql
+++ models/edr/run_results/test_result_rows.sql
@@ -2,7 +2,6 @@
 {{
   config(
     materialized = 'incremental',
-    unique_key = 'elementary_test_results_id',
     on_schema_change = 'append_new_columns',
     indexes=[{'columns': ['created_at']}, {'columns': ['elementary_test_results_id']}, {'columns': ['detected_at']}] if target.type == "postgres" else [],
     full_refresh=elementary.get_config_var('elementary_full_refresh'),
