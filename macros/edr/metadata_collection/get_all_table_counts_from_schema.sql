f0e611355f78f68507ed671a92d4cf1645784d51
Bindusekhar Gorintla <147714463+BindusekharGorintla@users.noreply.github.com>
2026-02-03 23:24:33 +0530

    Add macro to generate row counts for all tables in a schema
    This macro generates a row count summary for all tables in a given schema. It dynamically queries the information_schema.tables to list all tables, then builds a UNION ALL query that returns the row counts for each table in that schema
    
    Benefits
    Automates row count checks across all tables in a schema.
    Useful for data quality monitoring and schema validation.
    Provides a quick snapshot of table sizes during dbt runs.
    Logs the number of tables processed for transparency.

---
+++ macros/edr/metadata_collection/get_all_table_counts_from_schema.sql
@@ -0,0 +1,28 @@
+{%- macro get_all_table_counts_from_schema(schema_name, catalog_name = target.catalog) -%}
+
+{%- set schema_to_use = target.schema ~ '_' ~ schema_name -%}
+
+{%- set catalog_sql %}
+    SELECT table_catalog, table_schema, table_name
+    FROM {{ catalog_name }}.information_schema.tables t
+    where t.table_schema = '{{schema_to_use}}'
+{%- endset -%}
+
+{%- set results = dbt_utils.get_query_results_as_dict(catalog_sql) -%}
+
+{% for relation in results['table_name'] %}
+
+    {%- if loop.first -%}
+        {% do log(msg='Number of counts to perform: ' ~ loop.length, info=true) %}
+    {%- endif -%}
+
+    select '{{run_started_at}}' as date_time, '{{results['table_catalog'][loop.index-1]}}' as catalog_name, '{{results['table_schema'][loop.index-1]}}' as schema_name, '{{relation}}' as table_name, count(*) as count
+    from {{results['table_catalog'][loop.index-1]}}.{{results['table_schema'][loop.index-1]}}.{{results['table_name'][loop.index-1]}} tab
+
+    {% if not ( loop.last ) -%}
+        union all
+    {% endif %}
+
+{%- endfor -%}
+
+{%- endmacro %}
